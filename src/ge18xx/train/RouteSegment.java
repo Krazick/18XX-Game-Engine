abc0f44d9d7778154e613d2af759ec99b2514b01
Krazick <59795987+Krazick@users.noreply.github.com>
2020-12-07 13:18:54 -0500

    Add Tile Object to the Route Segment

--- src/ge18xx/train/RouteSegment.java
+++ src/ge18xx/train/RouteSegment.java
@@ -3,17 +3,20 @@ package ge18xx.train;
 import ge18xx.map.Location;
 import ge18xx.map.MapCell;
 import ge18xx.tiles.Gauge;
+import ge18xx.tiles.Tile;
 
 public class RouteSegment {
 	static Gauge NORMAL_GAUGE = new Gauge (Gauge.NORMAL_GAUGE);
 	public static RouteSegment NO_ROUTE_SEGMENT = null;
 	MapCell mapCell; 			// Hex ID
+	Tile tile;
 	SegmentInformation start;
 	SegmentInformation end;
 	int cost;					// For Ferry/Tunnel/Bridge Fee
 	
 	public RouteSegment (MapCell aMapCell) {
 		setMapCell (aMapCell);
+		setTile (aMapCell.getTile ());
 		setCost (0);
 		setStartSegment (new Location ());
 		setEndSegment (new Location ());
@@ -60,6 +63,30 @@ public class RouteSegment {
 		cost = aCost;
 	}
 
+	public void setTile (Tile aTile) {
+		tile = aTile;
+	}
+	
+	public Tile getTile () {
+		return tile;
+	}
+	
+	public boolean hasTownOnTile () {
+		boolean tHasTownOnTile = false;
+		
+		if (start.hasRevenueCenter()) {
+			if (tile.hasTown ()) {
+				tHasTownOnTile = true;
+			}
+		}
+		if (end.hasRevenueCenter()) {
+			if (tile.hasTown ()) {
+				tHasTownOnTile = true;
+			}
+		}
+		return tHasTownOnTile;
+	}
+	
 	public boolean hasRevenueCenter() {
 		boolean tHasRevenueCenter = false;
 		
@@ -70,6 +97,32 @@ public class RouteSegment {
 		}
 		
 		return tHasRevenueCenter;
+	}
+//	MapCell mapCell; 			// Hex ID
+//	Tile tile;
+//	SegmentInformation start;
+//	SegmentInformation end;
+//	int cost;					// For Ferry/Tunnel/Bridge Fee
+
+	public void printDetail() {
+		System.out.println ("MapCell " + mapCell.getCellID () + " Tile # " + tile.getNumber() + 
+				" Start " + start.getLocation () + " End " + end.getLocation ());
+		
+	}
+	public int getStartLocation() {
+		return start.getLocation ();
+	}
+
+	public int getEndLocation() {
+		return end.getLocation ();
+	}
+	
+	public void swapStartEndLocations () {
+		SegmentInformation tTempSegmentInformation;
+		
+		tTempSegmentInformation = start;
+		start = end;
+		end = tTempSegmentInformation;
 	}
 }
 
