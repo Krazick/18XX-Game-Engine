ac052bb7ac76cada83c61a46486a576179cc3402
Krazick <59795987+Krazick@users.noreply.github.com>
2020-12-08 10:24:18 -0500

    Add in saving the the RevenueCenter Object in the Segment

--- src/ge18xx/train/SegmentInformation.java
+++ src/ge18xx/train/SegmentInformation.java
@@ -1,5 +1,6 @@
 package ge18xx.train;
 
+import ge18xx.center.RevenueCenter;
 import ge18xx.map.Location;
 import ge18xx.tiles.Gauge;
 
@@ -9,16 +10,18 @@ public class SegmentInformation {
 	boolean corpStation;	//	Operating Corp Station (true or false)
 	boolean openFlow;		//	Open Flow for Train Running Through (true or false)
 	boolean hasRevenueCenter;	// Has a Revenue Center
+	RevenueCenter revenueCenter;	// If a RevenueCenter, it is saved here
 	int revenue;			//	Revenue
 	int bonus;				//	Bonus (for Cattle or Port)
 	Gauge gauge;			//	Track Gauge
 	
 	public SegmentInformation (Location aLocation, boolean aCorpStation, boolean aOpenFlow, boolean aHasRevenueCenter,
-				int aRevenue, int aBonus, Gauge aGauge) {
+				int aRevenue, int aBonus, Gauge aGauge, RevenueCenter aRevenueCenter) {
 		setLocation (aLocation);
 		setCorpStation (aCorpStation);
 		setOpenFlow (aOpenFlow);
 		setHasRevenueCenter (aHasRevenueCenter);
+		setRevenueCenter (aRevenueCenter);
 		setRevenue (aRevenue);
 		setBonus (aBonus);
 		setGauge (aGauge);
@@ -68,8 +71,12 @@ public class SegmentInformation {
 		location = aLocation;
 	}
 
-	public int getLocation () {
+	public int getLocationInt () {
 		return location.getLocation ();
+	}
+	
+	public Location getLocation () {
+		return location;
 	}
 	
 	public boolean isValid () {
@@ -86,6 +93,14 @@ public class SegmentInformation {
 
 	public void setHasRevenueCenter (boolean aHasRevenueCenter) {
 		hasRevenueCenter = aHasRevenueCenter;
+	}
+	
+	public void setRevenueCenter (RevenueCenter aRevenueCenter) {
+		revenueCenter = aRevenueCenter;
+	}
+	
+	public RevenueCenter getRevenueCenter () {
+		return revenueCenter;
 	}
 	
 	public boolean hasRevenueCenter() {
