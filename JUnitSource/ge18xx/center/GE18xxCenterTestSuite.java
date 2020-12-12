df5a5eeb58cf25c94f60226ec8363029217d6d22
Krazick <59795987+Krazick@users.noreply.github.com>
2020-12-11 20:22:34 -0500

    Update Display Name

--- JUnitSource/ge18xx/center/GE18xxCenterTestSuite.java
+++ JUnitSource/ge18xx/center/GE18xxCenterTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
 @RunWith (JUnitPlatform.class)
-@SuiteDisplayName ("Game Engine 18XX Test Suite")
+@SuiteDisplayName ("Game Engine 18XX Center Test Suite")
 @SelectPackages ("ge18xx.center")
 class GE18xxCenterTestSuite {
 
