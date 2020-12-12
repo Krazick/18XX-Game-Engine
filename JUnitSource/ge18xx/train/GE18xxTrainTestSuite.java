1d933e8be5ca142a863f97a2ed0b651dbab68a4b
Krazick <59795987+Krazick@users.noreply.github.com>
2020-12-11 20:22:55 -0500

    Setup New Train Test Suite

---
+++ JUnitSource/ge18xx/train/GE18xxTrainTestSuite.java
@@ -0,0 +1,14 @@
+package ge18xx.train;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith (JUnitPlatform.class)
+@SuiteDisplayName ("Game Engine 18XX Train Test Suite")
+@SelectPackages ("ge18xx.train")
+class GE18xxTrainTestSuite {
+
+
+}
