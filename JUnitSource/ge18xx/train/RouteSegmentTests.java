89aa488b3418ec4ffddcf710eafcad278caa5f68
Krazick <59795987+Krazick@users.noreply.github.com>
2020-12-13 11:39:19 -0500

    Fixed Mocking, added Additional Tests

--- JUnitSource/ge18xx/train/RouteSegmentTests.java
+++ JUnitSource/ge18xx/train/RouteSegmentTests.java
@@ -2,49 +2,222 @@ package ge18xx.train;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import ge18xx.center.City;
+import ge18xx.center.RevenueCenter;
+import ge18xx.center.Town;
 import ge18xx.map.Location;
 import ge18xx.map.MapCell;
 import ge18xx.tiles.Tile;
+import ge18xx.tiles.Track;
 
 @DisplayName ("Route Segment Tests")
 class RouteSegmentTests {
+	MapCell mMapCellWithRCCityTile;
+	MapCell mMapCellWithRCTownTile;
+	MapCell mMapCellWithNoRCTile;
+	Tile mTileWithRCCity;
+	Tile mTileWithRCTown;
+	Tile mTileWithNoRC;
+	RouteSegment routeSegmentWithRCCity;
+	RouteSegment routeSegmentWithRCTown;
+	RouteSegment routeSegmentWithNoRC;
+	NodeInformation mNodeRCCity, mNodeRCTown, mNodeSide1, mNodeSide3;
+	Location mLocationRC, mLocationSide1, mLocationSide3;
+	RevenueCenter mRCCity;
+	RevenueCenter mRCTown;
+	RevenueCenter mNoRevenueCenter;
+	Track mTrackSideToSide, mTrackSideToRC;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp () throws Exception {
+		mMapCellWithRCCityTile = Mockito.mock (MapCell.class);
+		mMapCellWithRCTownTile = Mockito.mock (MapCell.class);
+		mMapCellWithNoRCTile = Mockito.mock (MapCell.class);
+		
+		mRCCity = Mockito.mock (RevenueCenter.class);
+		mRCTown = Mockito.mock (RevenueCenter.class);
+		mNoRevenueCenter = RevenueCenter.NO_CENTER;
+		
+		mTileWithRCCity = Mockito.mock (Tile.class);
+		Mockito.when (mMapCellWithRCCityTile.getTile ()).thenReturn (mTileWithRCCity);
+		Mockito.when (mTileWithRCCity.hasTown ()).thenReturn (false);
+		
+		mTileWithRCTown = Mockito.mock (Tile.class);
+		Mockito.when (mMapCellWithRCTownTile.getTile ()).thenReturn (mTileWithRCTown);
+		Mockito.when (mTileWithRCTown.hasTown ()).thenReturn (true);
+		
+		mTileWithNoRC = Mockito.mock (Tile.class);
+		Mockito.when (mMapCellWithNoRCTile.getTile ()).thenReturn (mTileWithNoRC);
+		Mockito.when (mTileWithNoRC.hasTown ()).thenReturn (false);
+		
+		routeSegmentWithRCCity = new RouteSegment (mMapCellWithRCCityTile);
+		routeSegmentWithRCTown = new RouteSegment (mMapCellWithRCTownTile);
+		routeSegmentWithNoRC = new RouteSegment (mMapCellWithNoRCTile);
+		
+		mLocationRC = Mockito.mock (Location.class);
+		Mockito.when (mLocationRC.getLocation ()).thenReturn (50);
+		Mockito.when (mLocationRC.isSide ()).thenReturn (false);
+		mLocationSide1 = Mockito.mock (Location.class);
+		Mockito.when (mLocationSide1.getLocation ()).thenReturn (1);
+		Mockito.when (mLocationSide1.isSide ()).thenReturn (true);
+		mLocationSide3 = Mockito.mock (Location.class);
+		Mockito.when (mLocationSide3.getLocation ()).thenReturn (3);
+		Mockito.when (mLocationSide3.isSide ()).thenReturn (true);
+		
+		mNodeRCCity = Mockito.mock (NodeInformation.class);
+		Mockito.when (mNodeRCCity.hasRevenueCenter ()).thenReturn (true);
+		Mockito.when (mNodeRCCity.getLocation ()).thenReturn (mLocationRC);
+		Mockito.when (mNodeRCCity.getLocationInt ()).thenReturn (50);
+		Mockito.when (mNodeRCCity.isSide ()).thenReturn (false);
+		Mockito.when (mNodeRCCity.getRevenueCenter ()).thenReturn (mRCCity);
+		
+		mNodeRCTown = Mockito.mock (NodeInformation.class);
+		Mockito.when (mNodeRCTown.hasRevenueCenter ()).thenReturn (true);
+		Mockito.when (mNodeRCTown.getLocation ()).thenReturn (mLocationRC);
+		Mockito.when (mNodeRCTown.getLocationInt ()).thenReturn (26);
+		Mockito.when (mNodeRCTown.isSide ()).thenReturn (false);
+		Mockito.when (mNodeRCTown.getRevenueCenter ()).thenReturn (mRCTown);
+		
+		mNodeSide1 = Mockito.mock (NodeInformation.class);
+		Mockito.when (mNodeSide1.hasRevenueCenter ()).thenReturn (false);
+		Mockito.when (mNodeSide1.getLocation ()).thenReturn (mLocationSide1);
+		Mockito.when (mNodeSide1.getLocationInt ()).thenReturn (1);
+		Mockito.when (mNodeSide1.isSide ()).thenReturn (true);
+		Mockito.when (mNodeSide1.getRevenueCenter ()).thenReturn (mNoRevenueCenter);
+		
+		mNodeSide3 = Mockito.mock (NodeInformation.class);
+		Mockito.when (mNodeSide3.hasRevenueCenter ()).thenReturn (false);
+		Mockito.when (mNodeSide3.getLocation ()).thenReturn (mLocationSide3);
+		Mockito.when (mNodeSide3.getLocationInt ()).thenReturn (3);
+		Mockito.when (mNodeSide3.isSide ()).thenReturn (true);
+		Mockito.when (mNodeSide3.getRevenueCenter ()).thenReturn (mNoRevenueCenter);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown () throws Exception {
+	}
 
 	@Test
 	@DisplayName ("Route Segment Constructor Tests with Mocks")
 	void RouteSegmentConstructorTests () {
 		Location tLocation1, tLocation2, tLocation3;
-//		tMockCoID = 5001;
-//		TokenCompany mCompany = Mockito.mock (TokenCompany.class);
-//		Mockito.when (mCompany.getID ()).thenReturn (tMockCoID);
-		RouteSegment tRouteSegment;
 		
-		MapCell mMapCell = Mockito.mock (MapCell.class);
-		Tile mTile = Mockito.mock (Tile.class);
-		Mockito.when (mMapCell.getTile ()).thenReturn (mTile);
+		assertEquals (routeSegmentWithRCCity.getCost (), 0, "Route Segment Cost is not Zero");
 		
-		tRouteSegment = new RouteSegment (mMapCell);
-		assertEquals (tRouteSegment.getCost (), 0, "Route Segment Cost is not Zero");
+		assertEquals (Location.NO_LOCATION, routeSegmentWithRCCity.getStartLocationInt (), "Route Segment Start is not NO_LOCATTION");
+		assertEquals (Location.NO_LOCATION, routeSegmentWithRCCity.getEndLocationInt (),  "Route Segment End is not NO_LOCATTION");
 		
-		assertEquals (tRouteSegment.getStartLocationInt (), Location.NO_LOCATION, "Route Segment Start is not NO_LOCATTION");
-		assertEquals (tRouteSegment.getEndLocationInt (), Location.NO_LOCATION, "Route Segment End is not NO_LOCATTION");
-		
-		assertFalse (tRouteSegment.validSegment (), "Route Segment before Locations set does have two good Locations");
+		assertFalse (routeSegmentWithRCCity.validSegment (), "Route Segment before Locations set does have two good Locations");
 		tLocation1 = new Location (1);
 		tLocation2 = new Location (50);
-		tRouteSegment.setStartNode (tLocation1);
-		assertFalse (tRouteSegment.validSegment (), "Route Segment after location1 set does have two good Locations");
-		tRouteSegment.setEndNode (tLocation2);
-		assertTrue (tRouteSegment.validSegment (), "Route Segment after location2 set does NOT have two good Locations");
+		routeSegmentWithRCCity.setStartNode (tLocation1);
+		assertFalse (routeSegmentWithRCCity.validSegment (), "Route Segment after location1 set does have two good Locations");
+		routeSegmentWithRCCity.setEndNode (tLocation2);
+		assertTrue (routeSegmentWithRCCity.validSegment (), "Route Segment after location2 set does NOT have two good Locations");
 		
 		tLocation3 = new Location (1);
-		tRouteSegment.setEndNode (tLocation3);
-		assertFalse (tRouteSegment.validSegment (), "Route Segment after location1 set does have two good Different Int Locations");
-		tRouteSegment.setEndNode (tLocation1);
-		assertFalse (tRouteSegment.validSegment (), "Route Segment after location1 set does have two good Different Locations");
+		routeSegmentWithRCCity.setEndNode (tLocation3);
+		assertFalse (routeSegmentWithRCCity.validSegment (), "Route Segment after location1 set does have two good Different Int Locations");
+		routeSegmentWithRCCity.setEndNode (tLocation1);
+		assertFalse (routeSegmentWithRCCity.validSegment (), "Route Segment after location1 set does have two good Different Locations");
 	}
 
+	@Test
+	@DisplayName ("Route Segment Test RevenueCenter presence")
+	void RouteSegmentTestRevenueCenters () {
+		
+		routeSegmentWithNoRC.setStartNode (mNodeSide3);
+		routeSegmentWithNoRC.setEndNode (mNodeSide1);
+		assertFalse (routeSegmentWithNoRC.hasRevenueCenterAtStart (), "Route Segment has RevenueCenter at Start");
+		assertFalse (routeSegmentWithNoRC.hasRevenueCenterAtEnd (), "Route Segment has RevenueCenter at End");
+		assertFalse (routeSegmentWithNoRC.hasRevenueCenter (), "Route Segment has Revenue Centers");
+
+		routeSegmentWithRCCity.setStartNode (mNodeSide1);
+		routeSegmentWithRCCity.setEndNode (mNodeRCCity);
+		assertFalse (routeSegmentWithRCCity.hasRevenueCenterAtStart (), "Route Segment has RevenueCenter at Start");
+		assertTrue (routeSegmentWithRCCity.hasRevenueCenterAtEnd (), "Route Segment has no RevenueCenter at End");
+		assertTrue (routeSegmentWithRCCity.hasRevenueCenter (), "Route Segment has No Revenue Centers");
+		
+		routeSegmentWithRCCity.setStartNode (mNodeRCCity);
+		routeSegmentWithRCCity.setEndNode (mNodeSide1);
+		assertTrue (routeSegmentWithRCCity.hasRevenueCenterAtStart (), "Route Segment has no RevenueCenter at Start");
+		assertFalse (routeSegmentWithRCCity.hasRevenueCenterAtEnd (), "Route Segment has RevenueCenter at End");
+		assertTrue (routeSegmentWithRCCity.hasRevenueCenter (), "Route Segment has No Revenue Centers");
+		assertFalse (routeSegmentWithRCCity.hasTownOnTile (), "Route Segment has Town on Tile - at Start");
+		
+		routeSegmentWithRCCity.swapStartEndLocations ();
+		assertFalse (routeSegmentWithRCCity.hasRevenueCenterAtStart (), "Route Segment has RevenueCenter at Start");
+		assertTrue (routeSegmentWithRCCity.hasRevenueCenterAtEnd (), "Route Segment has no RevenueCenter at End");
+		assertTrue (routeSegmentWithRCCity.hasRevenueCenter (), "Route Segment has No Revenue Centers");
+		assertFalse (routeSegmentWithRCCity.hasTownOnTile (), "Route Segment has Town On Tile - at End");
+
+		routeSegmentWithRCTown.setStartNode (mNodeRCCity);
+		routeSegmentWithRCTown.setEndNode (mNodeSide1);
+		assertTrue (routeSegmentWithRCTown.hasTownOnTile (), "Route Segment has NO Town On Tile - at End");
+		routeSegmentWithRCTown.swapStartEndLocations ();
+		assertTrue (routeSegmentWithRCTown.hasTownOnTile (), "Route Segment has NO Town On Tile - at Start");
+
+	}
+	
+	@Test
+	@DisplayName ("Route Segment Test getting Locations")
+	void RouteSegmentTestGetLocations () {
+		Location tLocation1, tLocation3;
+		Location tStartLocationSide, tEndLocationSide;
+		
+		routeSegmentWithNoRC.setStartNode (mNodeSide1);
+		routeSegmentWithNoRC.setEndNode (mNodeSide3);
+		tLocation1 = routeSegmentWithNoRC.getStartLocation ();
+		tLocation3 = routeSegmentWithNoRC.getEndLocation ();
+		assertEquals (1, tLocation1.getLocation (), "Route Segment Start Location is not 1");
+		assertEquals (3, tLocation3.getLocation (), "Route Segment End Location is not 3");
+		
+		assertEquals (1, routeSegmentWithNoRC.getStartLocationInt (), "Route Segment Start Location Int is not 1");
+		assertEquals (3, routeSegmentWithNoRC.getEndLocationInt (), "Route Segment End Location Int is not 3");
+
+		routeSegmentWithNoRC.setEndNode (mNodeRCCity);
+		assertTrue (routeSegmentWithNoRC.isStartASide (), "Route Segment Is Start NOT A Side");
+		assertFalse (routeSegmentWithNoRC.isEndASide (), "Route Segment Is End A Side ");
+		
+		routeSegmentWithNoRC.swapStartEndLocations ();
+		assertFalse (routeSegmentWithNoRC.isStartASide (), "Route Segment Is Start A Side");
+		assertTrue (routeSegmentWithNoRC.isEndASide (), "Route Segment Is End NOT A Side ");
+		
+		routeSegmentWithNoRC.setStartNode (mNodeSide1);
+		routeSegmentWithNoRC.setEndNode (mNodeSide3);
+		tStartLocationSide = routeSegmentWithNoRC.getStartLocationIsSide ();
+		assertEquals (tLocation1, tStartLocationSide, "Route Segment Start is NOT tLocation1");
+		tEndLocationSide = routeSegmentWithNoRC.getEndLocationIsSide ();
+		assertEquals (tLocation3, tEndLocationSide, "Route Segment End is NOT tLocation3");
+		
+		routeSegmentWithNoRC.setEndNode (mNodeRCCity);
+		tEndLocationSide = routeSegmentWithNoRC.getEndLocationIsSide ();
+		assertEquals (Location.NO_LOC, tEndLocationSide, "Route Segment End is NOT Location.NO_LOC");
+		routeSegmentWithNoRC.swapStartEndLocations ();
+		tStartLocationSide = routeSegmentWithNoRC.getStartLocationIsSide ();
+		assertEquals (Location.NO_LOC, tStartLocationSide, "Route Segment Start is NOT tLocation1");
+	}
+	
+	@Test
+	@DisplayName ("Route Segment Test getting Revenue stuff")
+	void RouteSegmentTestGetRevenueStuff () {
+		RevenueCenter tFoundRevenueCenter;
+		
+		routeSegmentWithRCCity.setStartNode (mNodeSide1);
+		routeSegmentWithRCCity.setStartNode (mNodeRCCity);
+		tFoundRevenueCenter = routeSegmentWithRCCity.getRevenueCenter ();
+		assertEquals (mRCCity, tFoundRevenueCenter, "Expected mRCCity, did not find it");
+	}
 }
