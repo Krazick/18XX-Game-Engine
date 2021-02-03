7d3d5843809191719af21bc62f8a439dc9640556
Krazick <markjsmith60@gmail.com>
2021-02-03 09:32:55 -0500

    Update to Version 0.6.6 Beta

--- src/ge18xx/game/MyResources.java
+++ src/ge18xx/game/MyResources.java
@@ -45,6 +45,6 @@ public class MyResources extends ListResourceBundle {
 			{"showRoundFrameItem", "Show Round Frame"},
 			{"showAuditFrameItem", "Show Audit Frame"},
 			{"frameTitle", "18XX Game Engine"},
-			{"version", "V 0.6.5 Beta"}
+			{"version", "V 0.6.6 Beta"}
 	};
 }
