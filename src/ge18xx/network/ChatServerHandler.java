87719c5fa0cab844a04d14303dd3f56a9e28538f
Krazick <markjsmith60@gmail.com>
2021-02-27 12:35:05 -0500

    Update SendUserReady and SendUserStart Methods

--- src/ge18xx/network/ChatServerHandler.java
+++ src/ge18xx/network/ChatServerHandler.java
@@ -131,13 +131,13 @@ public class ChatServerHandler extends ServerHandler {
 		println ("AFK");
 	}
 
-	public void sendUserReady () {
-		println ("Ready");
+	public void sendUserReady (String aGameID) {
+		println ("Game Support <GS gameID=\"" + aGameID + "\"><Ready></GS>");
 		jClient.appendToChat ("I am ready to play the Game", true);
 	}
 
 	public void sendUserStart () {
-		println ("Start");
+		println ("<GS><Start></GS>");
 		jClient.appendToChat ("I started the Game", true);
 	}
 	
