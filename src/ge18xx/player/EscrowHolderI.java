2d96487662a2e4d37e575caa0b982b5dc83b0a0d
Krazick <markjsmith60@gmail.com>
2021-03-08 15:18:27 -0500

    Add methods addEscdrowInfo and printAllEscrows to Interface

--- src/ge18xx/player/EscrowHolderI.java
+++ src/ge18xx/player/EscrowHolderI.java
@@ -1,9 +1,12 @@
 package ge18xx.player;
 
 import ge18xx.bank.Bank;
+import ge18xx.company.Certificate;
 
 public interface EscrowHolderI extends CashHolderI {
 
 	Bank getBank();
 	void removeEscrow (Escrow escrow, boolean escrowCloseMatch);
+	Escrow addEscrowInfo (Certificate tCertificate, int tCash);
+	void printAllEscrows ();
 }
