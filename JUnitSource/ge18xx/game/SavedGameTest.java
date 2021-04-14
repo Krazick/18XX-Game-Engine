e64516995b957edb10e2e6fdffb7327f1ffad403
Krazick <markjsmith60@gmail.com>
2021-04-14 17:33:55 -0400

    SavedGame Class and JUnit Tests in ge18xx.game Package

---
+++ JUnitSource/ge18xx/game/SavedGameTest.java
@@ -0,0 +1,192 @@
+package ge18xx.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName ("18XX Saved Game Tests")
+class SavedGameTest {
+	SavedGame savedGame;
+	
+	@BeforeEach
+	void setUp() {
+		try {
+			savedGame = new SavedGame (SavedGame.TEST_FILE);
+		} catch (FileNotFoundException tException) {
+			tException.printStackTrace();
+		}
+	}
+
+	@Test
+	@DisplayName ("Saved Game XML Generation Test")
+	void SaveGameXMLGenerationTest  () {
+		savedGame.setGameID ("2021-04-12-1315");
+		savedGame.setGameStatus ("Active");
+		savedGame.setLastActionNumber (101);
+		savedGame.addPlayer ("SGPlayerAlpha");
+		savedGame.addPlayer ("SGPlayerBeta");
+		savedGame.addPlayer ("SGPlayerGamma");
+
+		assertEquals ("<Game gameID=\"2021-04-12-1315\" lastActionNumber=\"101\" players=\"SGPlayerAlpha, SGPlayerBeta, SGPlayerGamma\" status=\"Active\">",
+						savedGame.getSavedGameXML ());
+	}
+	
+	@Nested
+	@DisplayName ("Constructor and Setters Tests")
+	class constuctorTests {
+		@Test
+		@DisplayName ("Basic Constructor Test")
+		void SavedGameBasicConstructorTest () {
+			assertEquals ("", savedGame.getGameID ());
+			assertEquals ("", savedGame.getGameStatus ());
+			assertEquals (0, savedGame.getLastActionNumber ());
+			assertEquals (0, savedGame.getPlayerCount ());
+		}
+	
+		@Test
+		@DisplayName ("No Arguments Constructor Test")
+		void SavedGameNoArgsConstructorTest () {
+			SavedGame tSavedGame;
+			
+			tSavedGame = new SavedGame ();
+			assertEquals ("", tSavedGame.getGameID ());
+			assertEquals ("", tSavedGame.getGameStatus ());
+			assertEquals (0, tSavedGame.getLastActionNumber ());
+			assertEquals (0, tSavedGame.getPlayerCount ());
+		}
+		
+		@Test
+		@DisplayName ("Setup Players from List Test")
+		void SavedGameSetViaPlayersListTest () {
+			String tPlayerNameList = "SGPlayerAlpha,SGPlayerBeta,SGPlayerGamma";
+			
+			savedGame.setPlayers (tPlayerNameList);
+			assertEquals (3, savedGame.getPlayerCount ());
+			assertTrue (savedGame.hasPlayer ("SGPlayerBeta"));
+			assertFalse (savedGame.hasPlayer ("SGPlayerDelta"));
+		}
+		
+		@Test
+		@DisplayName ("Setter Tests")
+		void SavedGameSettersTest () {
+			savedGame.setGameID ("2021-04-12-1313");
+			savedGame.setGameStatus ("Prepared");
+			savedGame.setLastActionNumber (100);
+			
+			assertEquals ("2021-04-12-1313", savedGame.getGameID ());
+			assertEquals ("Prepared", savedGame.getGameStatus ());
+			assertEquals (100, savedGame.getLastActionNumber ());
+			assertEquals (0, savedGame.getPlayerCount ());
+			assertEquals ("", savedGame.getPlayers ());
+		}
+	
+		@Test
+		@DisplayName ("Add Players Tests")
+		void SavedGameAddPlayersTest () {
+			savedGame.setGameID ("2021-04-12-1315");
+			savedGame.setGameStatus ("Active");
+			savedGame.setLastActionNumber (101);
+			savedGame.addPlayer ("SGPlayerAlpha");
+			savedGame.addPlayer ("SGPlayerBeta");
+			savedGame.addPlayer ("SGPlayerGamma");
+			
+			assertEquals ("2021-04-12-1315", savedGame.getGameID ());
+			assertEquals ("Active", savedGame.getGameStatus ());
+			assertEquals (101, savedGame.getLastActionNumber ());
+			assertEquals (3, savedGame.getPlayerCount ());
+			assertEquals ("SGPlayerAlpha, SGPlayerBeta, SGPlayerGamma", savedGame.getPlayers ());
+		}
+	}
+	
+	@Nested
+	@DisplayName ("Parsing Data from input")
+	class parsingDataFromInputLineTests {
+		
+		@Test
+		@DisplayName ("Extracting the Last Action Number")
+		void LastActionNumberExtractionTest () {
+			String tNSG_XML_StringGood = "<NetworkSaveGame gameID=\"2021-04-12-1353\" status=\"Active\" lastActionNumber=\"103\">";
+			String tNSG_XML_StringBad = "<NetworkSaveGame gameID=\"2021-04-12-1353\" status=\"Active\" lastActionNumber=\"ABLE\">";
+			
+			assertEquals (103, savedGame.getLastActionNumberFromLine (tNSG_XML_StringGood));
+			assertEquals (-1, savedGame.getLastActionNumberFromLine (tNSG_XML_StringBad));
+		}
+			
+		@Test
+		@DisplayName ("Extracting the GameID")
+		void GameIDExtractionTest () {
+			String tNSG_XML_StringGood = "<NetworkSaveGame gameID=\"2021-04-12-1353\" status=\"Active\" lastActionNumber=\"103\">";
+			String tNSG_XML_StringBad = "<NetworkSaveGame gameID=\"2021-04-12-1353333\" status=\"Active\" lastActionNumber=\"ABLE\">";
+			
+			assertEquals ("2021-04-12-1353", savedGame.getGameIDFromLine (tNSG_XML_StringGood));
+			assertEquals ("NOID", savedGame.getGameIDFromLine (tNSG_XML_StringBad));
+		}
+		
+		@Test
+		@DisplayName ("Extracting the Game Status")
+		void GameStatusExtractionTest () {
+			String tNSG_XML_StringGood = "<NetworkSaveGame gameID=\"2021-04-12-1353\" status=\"Active\" lastActionNumber=\"103\">";
+			String tNSG_XML_StringBad = "<NetworkSaveGame gameID=\"2021-04-12-1353\" gameStatus=\"Active\" lastActionNumber=\"ABLE\">";
+			
+			assertEquals ("Active", savedGame.getGameStatusFromLine (tNSG_XML_StringGood));
+			assertEquals ("NO_STATUS", savedGame.getGameStatusFromLine (tNSG_XML_StringBad));
+		}
+		
+		@Test
+		@DisplayName ("Extracting the Player Name")
+		void PlayerNameExtractionTest () {
+			String tClient_XML_StringGood = "<Player name=\"SGTesterAlpha\" status=\"Active\">";
+			String tClient_XML_StringBad = "<Player playerName=\"SGTesterBeta\" status=\"Active\">";
+			
+			assertEquals ("SGTesterAlpha", savedGame.getPlayerNameFromLine (tClient_XML_StringGood));
+			assertEquals ("NO_NAME", savedGame.getPlayerNameFromLine (tClient_XML_StringBad));
+		}
+		
+		@Test
+		@DisplayName ("Extracting from XML Data Line")
+		void ParseSavedGameXML () {
+			SavedGame tSavedGame;
+			SavedGame tSavedGameBad;
+			String tSavedGameXML = "<Game gameID=\"2021-04-12-1353\" lastActionNumber=\"103\" players=\"SGPlayerAlpha,SGPlayerBeta,SGPlayerGamma\" status=\"Active\">";
+			String tBadSavedGameXML = "<Game gameID=\"2021-AA-BBBB\" lastActionNumber=\"103\" players=\"SGPlayerAlpha,SGPlayerBeta,SGPlayerGamma\" status=\"Active\">";
+			
+			tSavedGame = savedGame.parseSavedGameXML (tSavedGameXML);
+			assertEquals (3, tSavedGame.getPlayerCount ());
+			assertEquals ("2021-04-12-1353", tSavedGame.getGameID ());
+			assertEquals (103, tSavedGame.getLastActionNumber ());
+			
+			tSavedGameBad = savedGame.parseSavedGameXML(tBadSavedGameXML);
+			assertEquals (0, tSavedGameBad.getPlayerCount ());
+			assertEquals (0, tSavedGameBad.getLastActionNumber ());
+		}
+	}
+	
+	@Nested
+	@DisplayName ("Players in Games")
+	class PlayersInGameTests {
+
+		@Test
+		@DisplayName ("Player is in Game")
+		void PlayerIsInGameTest () {
+			savedGame.addPlayer ("SGPlayerAlpha");
+			savedGame.addPlayer ("SGPlayerBeta");
+			savedGame.addPlayer ("SGPlayerGamma");
+	
+			assertTrue (savedGame.hasPlayer ("SGPlayerBeta"));
+			assertTrue (savedGame.hasPlayer ("SGPlayerAlpha"));
+			assertTrue (savedGame.hasPlayer ("SGPlayerGamma"));
+			assertFalse (savedGame.hasPlayer ("SGPlayerDelta"));
+		}
+
+		@Test
+		@DisplayName ("Player is in Game")
+		void NoPlayersInGameTest () {
+			assertFalse (savedGame.hasPlayer ("SGPlayerBeta"));
+		}
+	}
+}
---
+++ src/ge18xx/game/SavedGame.java
@@ -0,0 +1,239 @@
+package ge18xx.game;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SavedGame {
+	String gameID;
+	String gameStatus;
+	int lastActionNumber;
+	ArrayList<String> players;
+	public ArrayList<String> NO_PLAYERS = null;
+	private final static String GAME_ID = "(\\d\\d\\d\\d-\\d\\d-\\d\\d-\\d\\d\\d\\d)";
+	private final static String NSG_WITH_GAME_ID = "<NetworkSaveGame gameID=\"" + GAME_ID + "\" status=\"(.*)\" lastActionNumber=\"(\\d+)\">";
+	private final static Pattern NSG_WITH_GAME_ID_PATTERN = Pattern.compile (NSG_WITH_GAME_ID);
+	private final static String PLAYER_WITH_NAME = "<Player name=\"(.*)\" status=\"(.*)\">";
+	private final static Pattern PLAYER_WITH_NAME_PATTERN = Pattern.compile (PLAYER_WITH_NAME);
+	private final static String GAME_WITH_GAME_ID = "<Game gameID=\"" + GAME_ID + "\" lastActionNumber=\"(\\d+)\" players=\"(.*)\" status=\\\"(.*)\\\">";
+	private final static Pattern GAME_WITH_GAME_ID_PATTERN = Pattern.compile (GAME_WITH_GAME_ID);
+	public final static String NO_GAME_ID = "NOID";
+	public final static String NO_NAME = "NO_NAME";
+	public final static String NO_STATUS = "NO_STATUS";
+	public final static String TEST_FILE = "JunitTestFile";
+	public final static int BAD_ACTION_NUMBER = -1;
+	
+	public SavedGame (String aFileName) throws FileNotFoundException {
+		setupPlayers ();
+		if (aFileName != null) {
+			readFile (aFileName);
+		} else {
+			throw (new FileNotFoundException ("Null File Name"));
+		}
+	}
+
+	public SavedGame () {
+		setupPlayers ();
+		setupEmptyValues ();
+	}
+	
+	private void setupPlayers () {
+		players = new ArrayList<String> ();
+	}
+
+	private void readFile (String aFileName) {
+		FileReader tFile;
+		BufferedReader tReader;
+		String tLine, tGameID, tPlayerName, tGameStatus;
+		int tLastActionNumber;
+		
+		if (! TEST_FILE.equals (aFileName)) {
+			try {
+				tFile = new FileReader (aFileName);
+				tReader = new BufferedReader (tFile);
+				while ((tLine = tReader.readLine()) != null) {
+					tGameID = getGameIDFromLine (tLine);
+					if (tGameID.equals (NO_GAME_ID)) {
+						tPlayerName = getPlayerNameFromLine (tLine);
+						if (! tPlayerName.equals (NO_NAME)) {
+							addPlayer (tPlayerName);
+						}
+					} else {
+						setGameID (tGameID);
+						tGameStatus = getGameStatusFromLine (tLine);
+						setGameStatus (tGameStatus);
+						tLastActionNumber = getLastActionNumberFromLine (tLine);
+						setLastActionNumber (tLastActionNumber);
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			setupEmptyValues ();
+		}
+	}
+
+	private void setupEmptyValues () {
+		setGameID ("");
+		setLastActionNumber (0);
+		setGameStatus ("");
+	}
+	
+	public void setPlayers (String aPlayerList) {
+		String [] tPlayerNames = aPlayerList.split (",");
+		
+		for (String tPlayerName : tPlayerNames) {
+			addPlayer (tPlayerName);
+		}
+	}
+	
+	public void addPlayer (String tPlayerName) {
+		players.add (tPlayerName);		
+	}
+	
+	public void setGameID (String aGameID) {
+		gameID = aGameID;
+	}
+	
+	public String getGameID () {
+		return gameID;
+	}
+	
+	public void setGameStatus (String aGameStatus) {
+		gameStatus = aGameStatus;
+	}
+	
+	public String getGameStatus () {
+		return gameStatus;
+	}
+	
+	public void setLastActionNumber (int aLastActionNumber) {
+		lastActionNumber = aLastActionNumber;
+	}
+	
+	public int getLastActionNumber () {
+		return lastActionNumber;
+	}
+	
+	public int getPlayerCount () {
+		return players.size ();
+	}
+	
+	public int getLastActionNumberFromLine (String aRequest) {
+		Matcher tMatcher = NSG_WITH_GAME_ID_PATTERN.matcher (aRequest);
+		int tLastActionNumber = BAD_ACTION_NUMBER;
+		String tLANText = "";
+		
+		if (tMatcher.find ()) {
+			tLANText = tMatcher.group (3);
+			tLastActionNumber = Integer.parseInt (tLANText);
+		}
+		
+		return tLastActionNumber;
+	}
+	
+	public String getGameIDFromLine (String aRequest) {
+		Matcher tMatcher = NSG_WITH_GAME_ID_PATTERN.matcher (aRequest);
+		String tGameID = NO_GAME_ID;
+		
+		if (tMatcher.find ()) {
+			tGameID = tMatcher.group (1);
+		}
+		
+		return tGameID;
+	}
+	
+	public String getGameStatusFromLine (String aRequest) {
+		Matcher tMatcher = NSG_WITH_GAME_ID_PATTERN.matcher (aRequest);
+		String tGameStatus = NO_STATUS;
+		
+		if (tMatcher.find ()) {
+			tGameStatus = tMatcher.group (2);
+		}
+		
+		return tGameStatus;
+	}
+	
+	public String getPlayerNameFromLine (String aRequest) {
+		Matcher tMatcher = PLAYER_WITH_NAME_PATTERN.matcher (aRequest);
+		String tPlayerName = NO_NAME;
+		
+		if (tMatcher.find ()) {
+			tPlayerName = tMatcher.group (1);
+		}
+		
+		return tPlayerName;
+	}
+	
+	public String getPlayers () {
+		String tPlayers;
+		
+		tPlayers = "";
+		if (players.size () > 0) {
+			for (String tPlayer : players) {
+				if (tPlayers.length () > 0) {
+					tPlayers += ", ";
+				}
+				tPlayers += tPlayer;
+			}
+		}
+		
+		return tPlayers;
+	}
+	
+	public boolean hasPlayer (String aPlayerName) {
+		boolean tHasPlayer = false;
+		
+		if (players.size () > 0) {
+			for (String tPlayer : players) {
+				if (tPlayer.equals(aPlayerName)) {
+					tHasPlayer = true;
+				}
+			}
+		}
+		
+		return tHasPlayer;
+	}
+
+	public SavedGame parseSavedGameXML (String aSavedGameXML) {
+		Matcher tMatcher = GAME_WITH_GAME_ID_PATTERN.matcher (aSavedGameXML);
+		String tGameID = NO_GAME_ID;
+		String tLastActionNumberString;
+		int tLastActionNumber;
+		String aPlayerList;
+		String tStatus;
+		SavedGame tSavedGame = new SavedGame ();
+		
+		if (tMatcher.find ()) {
+			tGameID = tMatcher.group (1);
+			tLastActionNumberString = tMatcher.group (2);
+			tLastActionNumber = Integer.parseInt (tLastActionNumberString);
+			aPlayerList = tMatcher.group (3);
+			tStatus = tMatcher.group (4);
+			tSavedGame.setGameID (tGameID);
+			tSavedGame.setLastActionNumber (tLastActionNumber);
+			tSavedGame.setPlayers (aPlayerList);
+			tSavedGame.setGameStatus (tStatus);
+		}
+
+		return tSavedGame;
+	}
+	
+	public String getSavedGameXML () {
+		String tSavedGameXML = "";
+		String tPlayers;
+		
+		tPlayers = getPlayers ();
+		tSavedGameXML = "<Game gameID=\"" + gameID + "\" lastActionNumber=\"" + lastActionNumber + 
+				"\" players=\"" + tPlayers + "\" status=\"" + gameStatus + "\">";
+		
+		return tSavedGameXML;
+	}
+}
