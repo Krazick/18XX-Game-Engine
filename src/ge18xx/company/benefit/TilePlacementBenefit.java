4a1c686d8a919ebd784ede044a78881bedb9e37f
Krazick <markjsmith60@gmail.com>
2021-06-06 20:19:10 -0400

    Additional functional to begin applying Benefits

--- src/ge18xx/company/benefit/TilePlacementBenefit.java
+++ src/ge18xx/company/benefit/TilePlacementBenefit.java
@@ -1,10 +1,15 @@
 package ge18xx.company.benefit;
 
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import ge18xx.company.Corporation;
 import ge18xx.company.CorporationFrame;
 import ge18xx.company.PrivateCompany;
+import ge18xx.map.HexMap;
+import ge18xx.map.MapCell;
 import ge18xx.utilities.AttributeName;
 import ge18xx.utilities.XMLNode;
 
@@ -39,13 +44,49 @@ public class TilePlacementBenefit extends MapBenefit {
 		String tOwningCorpAbbrev;
 		JButton tPlaceTileButton;
 		
+		super.configure (aPrivateCompany, aButtonRow);
 		if (shouldConfigure (aPrivateCompany)) {
 			tOwningCorpAbbrev = aPrivateCompany.getOwnerName ();
 			System.out.println ("Should Configure Buttons for " + tOwningCorpAbbrev + 
 						" for Tile Placement with Private " + aPrivateCompany.getAbbrev ());
 			tPlaceTileButton = new JButton (getNewButtonLabel (aPrivateCompany));
+			setButton (tPlaceTileButton);
 			tPlaceTileButton.setActionCommand (CorporationFrame.PLACE_TILE_PRIVATE);
+			tPlaceTileButton.addActionListener (this);
 			aButtonRow.add (tPlaceTileButton);
+		}
+	}
+	
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		String tActionCommand;
+		
+		tActionCommand = aEvent.getActionCommand ();
+		if (CorporationFrame.PLACE_TILE_PRIVATE.equals (tActionCommand)) {
+			handlePlaceTile  ();
+		}
+	}
+
+	private void handlePlaceTile () {
+		Corporation tOwningCompany;
+		String tOwningCorpAbbrev;
+		HexMap tMap;
+		MapCell tMapCell;
+		
+		tOwningCompany = (Corporation) privateCompany.getOwner ();
+		tOwningCorpAbbrev = privateCompany.getOwnerName ();
+		System.out.println ("Place a Tile on " + getMapCellID () + 
+				" for " + tOwningCorpAbbrev +
+				" using Private " + privateCompany.getAbbrev () + " Benefit.");
+		tOwningCompany.handlePlaceTile ();
+		tMap = getMap ();
+		tMap.clearAllSelected ();
+		tMapCell = tMap.getMapCellForID (mapCellID);
+		if (tMapCell == HexMap.NO_MAP_CELL) {
+			System.err.println ("Did not find Map Cell with ID " + mapCellID);
+		} else {
+			System.out.println ("Did find Map Cell with ID " + mapCellID);
+			tMap.toggleSelectedMapCell (tMapCell);
 		}
 	}
 }
--- src/ge18xx/company/benefit/TokenPlacementBenefit.java
+++ src/ge18xx/company/benefit/TokenPlacementBenefit.java
@@ -1,10 +1,16 @@
 package ge18xx.company.benefit;
 
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import ge18xx.company.Corporation;
 import ge18xx.company.CorporationFrame;
 import ge18xx.company.PrivateCompany;
+import ge18xx.map.HexMap;
+import ge18xx.map.MapCell;
+import ge18xx.tiles.Tile;
 import ge18xx.utilities.AttributeName;
 import ge18xx.utilities.XMLNode;
 
@@ -38,11 +44,56 @@ public class TokenPlacementBenefit extends MapBenefit {
 	public void configure (PrivateCompany aPrivateCompany, JPanel aButtonRow) {
 		JButton tPlaceTokenButton;
 		
+		super.configure (aPrivateCompany, aButtonRow);
 		if (shouldConfigure (aPrivateCompany)) {
 			System.out.println ("Should Configure for Token Placement");
 			tPlaceTokenButton = new JButton (getNewButtonLabel (aPrivateCompany));
+			setButton (tPlaceTokenButton);
 			tPlaceTokenButton.setActionCommand (CorporationFrame.PLACE_TOKEN_PRIVATE);
+			tPlaceTokenButton.addActionListener (this);
 			aButtonRow.add (tPlaceTokenButton);
 		}
+	}
+	
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		String tActionCommand;
+		
+		tActionCommand = aEvent.getActionCommand ();
+		if (CorporationFrame.PLACE_TOKEN_PRIVATE.equals (tActionCommand)) {
+			handlePlaceToken  ();
+		}
+	}
+
+	private void handlePlaceToken () {
+		Corporation tOwningCompany;
+		HexMap tMap;
+		MapCell tMapCell;
+		Tile tTile;
+		
+		tOwningCompany = (Corporation) privateCompany.getOwner ();
+		System.out.println ("Place a Token on " + getMapCellID () + 
+				" for " + tOwningCompany.getAbbrev () +				
+				" using Private " + privateCompany.getAbbrev () + " Benefit");
+		tMap = getMap ();
+		tMap.clearAllSelected ();
+		tMapCell = tMap.getMapCellForID (mapCellID);
+		if (tMapCell == HexMap.NO_MAP_CELL) {
+			System.err.println ("Did not find Map Cell with ID " + mapCellID);
+		} else {
+			System.out.println ("Did find Map Cell with ID " + mapCellID);
+			if (tMapCell.isTileOnCell ()) {
+				tTile = tMapCell.getTile ();
+				System.out.println ("Found Tile # " + tTile.getNumber () + " on the MapCell");
+				if (tTile.cityOnTile ()) {
+					System.out.println ("City is on the Tile");
+					
+				}
+				tMap.toggleSelectedMapCell (tMapCell);
+			} else {
+				System.err.println ("No Tile found on MapCell");
+			}
+		}
+		
 	}
 }
