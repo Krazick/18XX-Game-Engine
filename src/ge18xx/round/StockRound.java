2e9c200f2f52476a55dc3736541af40062bd46e4
Krazick <markjsmith60@gmail.com>
2021-06-19 14:43:11 -0400

    Remove dead code

--- src/ge18xx/round/StockRound.java
+++ src/ge18xx/round/StockRound.java
@@ -166,7 +166,6 @@ public class StockRound extends Round {
 		Player tPlayer = Player.NO_PLAYER;
 		ChangeStateAction tChangeStateAction;
 		
-//		tPlayer = playerManager.getCurrentPlayer ();
 		tPlayer = playerManager.getPlayer(aPlayerIndex);
 		if (tPlayer != Player.NO_PLAYER) {
 			tChangeStateAction = new ChangeStateAction (getRoundType (), getID (),tPlayer);
