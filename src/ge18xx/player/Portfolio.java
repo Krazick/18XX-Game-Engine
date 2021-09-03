0b3993dfb9cfa3de07e71fbf0c6fd6a8a21f2c0c
Krazick <markjsmith60@gmail.com>
2021-09-03 19:20:25 -0400

    More constant variable cleanup

--- src/ge18xx/player/Portfolio.java
+++ src/ge18xx/player/Portfolio.java
@@ -54,15 +54,15 @@ public class Portfolio implements CertificateHolderI {
 	public final static AttributeName AN_COAL_INDEX = new AttributeName ("coalIndex");
 	public final static AttributeName AN_MINOR_INDEX = new AttributeName ("minorIndex");
 	public final static AttributeName AN_SHARE_INDEX = new AttributeName ("shareIndex");
-	public static final PortfolioHolderI NO_HOLDER = null;
-	public static final Portfolio NO_PORTFOLIO = null;
-	public static final String NO_PORTFOLIO_LABEL = ">> NO PORTFOLIO <<";
-	public static final String NO_CERTIFICATES_LABEL = ">> NO CERTIFICATES <<";
-	public static final int NO_COMPONENT = -1;
-	public static final String NO_COMPANY_YET = ">> NONE YET <<";
-	public static final String NO_CERTIFICATES = ">> NO CERTIFICATES <<";
-	public static final XMLElement NO_BIDDERS = null;
-	public static final String NO_NAME_STRING = "<NONE>";
+	public final static PortfolioHolderI NO_HOLDER = null;
+	public final static Portfolio NO_PORTFOLIO = null;
+	public final static String NO_PORTFOLIO_LABEL = ">> NO PORTFOLIO <<";
+	public final static String NO_CERTIFICATES_LABEL = ">> NO CERTIFICATES <<";
+	public final static String NO_COMPANY_YET = ">> NONE YET <<";
+	public final static String NO_CERTIFICATES = ">> NO CERTIFICATES <<";
+	public final static String NO_NAME_STRING = "<NONE>";
+	public final static XMLElement NO_BIDDERS = null;
+	public final static int NO_COMPONENT = -1;
 
 	/* These items are set once, no need to save/load */
 	PortfolioHolderI holder;
