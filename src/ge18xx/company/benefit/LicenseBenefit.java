46391238e98ac2694ec63ea847eeaeaa53d60564
Krazick <markjsmith60@gmail.com>
2021-09-03 20:20:18 -0400

    Add Placeholder Benefit Classes - Need to fulfill code

---
+++ src/ge18xx/company/benefit/LicenseBenefit.java
@@ -0,0 +1,23 @@
+package ge18xx.company.benefit;
+
+import ge18xx.utilities.XMLNode;
+
+public class LicenseBenefit extends Benefit {
+	public final static String NAME = "License";
+
+	public LicenseBenefit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LicenseBenefit(XMLNode aXMLNode) {
+		super(aXMLNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/ge18xx/company/benefit/PortPlacementBenefit.java
@@ -0,0 +1,23 @@
+package ge18xx.company.benefit;
+
+import ge18xx.utilities.XMLNode;
+
+public class PortPlacementBenefit extends Benefit {
+	public final static String NAME = "Port Placement";
+
+	public PortPlacementBenefit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PortPlacementBenefit(XMLNode aXMLNode) {
+		super(aXMLNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
