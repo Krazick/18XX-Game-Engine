059d2b62fb79bd6b4518636bcb81d74dee361e66
Krazick <markjsmith60@gmail.com>
2021-09-13 15:37:40 -0400

    Create and UndoLastAction Action

---
+++ src/ge18xx/round/action/UndoLastAction.java
@@ -0,0 +1,50 @@
+package ge18xx.round.action;
+
+import ge18xx.game.GameManager;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI.ActionStates;
+import ge18xx.utilities.XMLNode;
+
+public class UndoLastAction extends Action {
+	public static final String NAME = "Undo Last";
+
+	public UndoLastAction () {
+		super (NAME);
+	}
+
+	public UndoLastAction (String aName) {
+		super (aName);
+	}
+
+	public UndoLastAction (ActionStates aRoundType, String aRoundID, ActorI aActor) {
+		super (aRoundType, aRoundID, aActor);
+		setName (NAME);
+		setChainToPrevious (true);
+	}
+
+	public UndoLastAction (XMLNode aActionNode, GameManager aGameManager) {
+		super (aActionNode, aGameManager);
+		setName (NAME);
+	}
+
+	public boolean applyAction (RoundManager aRoundManager) {
+		boolean tActionApplied;
+		ActionManager tActionManager;
+		
+		tActionApplied = true;
+		tActionManager = aRoundManager.getActionManager ();
+		tActionManager.undoLastAction (aRoundManager);
+		System.out.println ("Applying Undo Last Action - from Remote Client");
+
+		return tActionApplied;
+	}
+	
+	public boolean undoAction (RoundManager aRoundManager) {
+		boolean tActionUndone = true;
+		
+		System.out.println ("Performing Undo Action for UndoAction Action");
+		
+		return tActionUndone;
+	}
+
+}
