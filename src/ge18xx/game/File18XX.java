9908a3e85e7a96eea9e1307772e5f2543a529b9e
Krazick <markjsmith60@gmail.com>
2021-11-22 19:44:15 -0500

    Add Constant Strings for each type of File

--- src/ge18xx/game/File18XX.java
+++ src/ge18xx/game/File18XX.java
@@ -5,13 +5,18 @@ import ge18xx.utilities.ElementName;
 import ge18xx.utilities.XMLNode;
 
 public class File18XX {
+	public static final String CITIES_TYPE = "cities";
+	public static final String COMPANIES_TYPE = "companies";
+	public static final String MAP_TYPE = "map";
+	public static final String MARKET_TYPE = "market";
+	public static final String TILE_SET_TYPE = "tileSet";
 	public static final ElementName EN_FILES = new ElementName ("Files");
 	public static final ElementName EN_FILE = new ElementName ("File");
-	public static final AttributeName AN_CITIES_FILE = new AttributeName ("cities");
-	public static final AttributeName AN_COMPANIES_FILE = new AttributeName ("companies");
-	public static final AttributeName AN_MAP_FILE = new AttributeName ("map");
-	public static final AttributeName AN_MARKET_FILE = new AttributeName ("market");
-	public static final AttributeName AN_TILESET_FILE = new AttributeName ("tileSet");
+	public static final AttributeName AN_CITIES_FILE = new AttributeName (CITIES_TYPE);
+	public static final AttributeName AN_COMPANIES_FILE = new AttributeName (COMPANIES_TYPE);
+	public static final AttributeName AN_MAP_FILE = new AttributeName (MAP_TYPE);
+	public static final AttributeName AN_MARKET_FILE = new AttributeName (MARKET_TYPE);
+	public static final AttributeName AN_TILESET_FILE = new AttributeName (TILE_SET_TYPE);
 	static final AttributeName AN_TYPE = new AttributeName ("type");
 	static final AttributeName AN_NAME = new AttributeName ("name");
 	static final String NO_NAME = "<NO NAME>";
