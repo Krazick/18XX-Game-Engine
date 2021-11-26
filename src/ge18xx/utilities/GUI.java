cc484509d3e889596996d23aa0c103da016588f8
Krazick <markjsmith60@gmail.com>
2021-11-26 09:34:07 -0500

    New GUI Interface with generic Constants for GUI components

---
+++ src/ge18xx/utilities/GUI.java
@@ -0,0 +1,11 @@
+package ge18xx.utilities;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public interface GUI {
+	public final String NO_TOOL_TIP = "";
+	public final JPanel NO_PANEL = null;
+	public static final JLabel NO_LABEL = null;
+
+}
