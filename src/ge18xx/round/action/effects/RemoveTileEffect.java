956fca253219166a8dc6d271d5098ea48fe458be
Krazick <markjsmith60@gmail.com>
2021-12-05 19:13:37 -0500

    Remove Dead Code when calling applyBases, applyTokens directly

--- src/ge18xx/round/action/effects/RemoveTileEffect.java
+++ src/ge18xx/round/action/effects/RemoveTileEffect.java
@@ -1,10 +1,7 @@
 package ge18xx.round.action.effects;
 
-import ge18xx.center.RevenueCenter;
-import ge18xx.company.ShareCompany;
 import ge18xx.game.GameManager;
 import ge18xx.map.HexMap;
-import ge18xx.map.Location;
 import ge18xx.map.MapCell;
 import ge18xx.round.RoundManager;
 import ge18xx.round.action.ActorI;
@@ -17,8 +14,6 @@ public class RemoveTileEffect extends ChangeTileContentEffect {
 	public static final String NAME = "Remove Tile";
 	final static AttributeName AN_PREVIOUS_TOKENS = new AttributeName ("previousTokens");
 	final static AttributeName AN_PREVIOUS_BASES = new AttributeName ("previousBases");
-//	String previousTokens;
-//	String previousBases;
 
 	public RemoveTileEffect () {
 		super ();
@@ -29,89 +24,11 @@ public class RemoveTileEffect extends ChangeTileContentEffect {
 			int aOrientation, String aPreviousTokens, String aPreviousBases) {
 		super (aActor, aMapCell, aTile, aOrientation, aPreviousTokens, aPreviousBases);
 		setName (NAME);
-//		setPreviousTokens (aPreviousTokens);
-//		setPreviousBases (aPreviousBases);
 	}
 
 	public RemoveTileEffect (XMLNode aEffectNode, GameManager aGameManager) {
 		super (aEffectNode, aGameManager);
 		setName (NAME);
-//		String tPreviousTokens, tPreviousBases;
-//		
-//		tPreviousTokens = aEffectNode.getThisAttribute (AN_PREVIOUS_TOKENS);
-//		tPreviousBases = aEffectNode.getThisAttribute (AN_PREVIOUS_BASES);
-//		setPreviousTokens (tPreviousTokens);
-//		setPreviousBases (tPreviousBases);
-	}
-
-	public String getPreviousBases () {
-		return getBases ();
-	}
-	
-	public String getPreviousTokens () {
-		return getTokens ();
-	}
-//	
-//	public void setPreviousBases (String aPreviousBases) {
-//		previousBases = aPreviousBases;
-//	}
-//
-//	public void setPreviousTokens (String aPreviousTokens) {
-//		previousTokens = aPreviousTokens;
-//	}
-	
-	public void restorePreviousBases (RoundManager aRoundManager, Tile aTile, MapCell aMapCell) {
-		String [] tPreviousBases;
-		String [] tBaseInfo;
-		String tAbbrev;
-		int tIndex;
-		ShareCompany tShareCompany;
-		GameManager tGameManager;
-		RevenueCenter tRevenueCenter;
-		Location tLocation;
-		String tOldBases;
-		
-		tOldBases = getBases ();
-		if (! (Tile.NO_BASES.equals (tOldBases))) {
-			tPreviousBases = tOldBases.split (";");
-			// Previous Bases are "CompanyAbbrev,CityIndex"
-			for (String tAPreviousBase : tPreviousBases) {
-				tBaseInfo = tAPreviousBase.split (",");
-				tAbbrev = tBaseInfo [0];
-				tIndex = Integer.parseInt (tBaseInfo [1]);
-				tGameManager = aRoundManager.getGameManager ();
-				tShareCompany = tGameManager.getShareCompany (tAbbrev);
-
-				tRevenueCenter = aTile.getRevenueCenter (tIndex);
-				tLocation = tRevenueCenter.getLocation ();
-				aMapCell.setCorporation (tShareCompany, tLocation);
-			}
-		}
-	}
-	
-	public void restorePreviousTokens (RoundManager aRoundManager, MapCell aMapCell) {
-		String [] tPreviousTokens;
-		String [] tTokenInfo;
-		String tAbbrev;
-		int tStationIndex, tCityIndex;
-		ShareCompany tShareCompany;
-		GameManager tGameManager;
-		String tOldTokens;
-		
-		tOldTokens = getTokens ();
-		if (! (Tile.NO_TOKENS.equals (tOldTokens))) {
-			tPreviousTokens = tOldTokens.split (";");
-			// Previous Tokens are "CompanyAbbrev,StationIndex,CityIndex"
-			for (String tAPreviousToken : tPreviousTokens) {
-				tTokenInfo = tAPreviousToken.split (",");
-				tAbbrev = tTokenInfo [0];
-				tStationIndex = Integer.parseInt (tTokenInfo [1]);
-				tCityIndex = Integer.parseInt (tTokenInfo [2]);
-				tGameManager = aRoundManager.getGameManager ();
-				tShareCompany = tGameManager.getShareCompany (tAbbrev);
-				aMapCell.setStationAt (tShareCompany, tStationIndex, tCityIndex);
-			}
-		}
 	}
 	
 	@Override
@@ -137,9 +54,10 @@ public class RemoveTileEffect extends ChangeTileContentEffect {
 		tEffectApplied = false;
 		tTileSet = aRoundManager.getTileSet ();
 		tGameMap = aRoundManager.getGameMap ();
-		tMapCell = this.getMapCell (tGameMap);
+		tMapCell = getMapCell (tGameMap);
 		tTile = tMapCell.getTile ();
 		tTile.returnTokens ();
+		
 		tMapCell.removeTile ();
 		tMapCell.restoreTile (tTileSet, tTile);
 		tGameMap.redrawMap ();
@@ -162,8 +80,8 @@ public class RemoveTileEffect extends ChangeTileContentEffect {
 		tGameMap = aRoundManager.getGameMap ();
 		tMapCell = this.getMapCell (tGameMap);
 		tMapCell.putTile (tTile, orientation);
-		restorePreviousBases (aRoundManager, tTile, tMapCell);
-		restorePreviousTokens (aRoundManager, tMapCell);
+		applyBases (aRoundManager, tMapCell);
+		applyTokens (aRoundManager, tMapCell);
 		tGameMap.redrawMap ();
 		tEffectUndone = true;
 		
