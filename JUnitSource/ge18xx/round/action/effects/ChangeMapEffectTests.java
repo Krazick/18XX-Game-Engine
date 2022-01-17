9eb9cd19d0d28608b14ba10df5e5122f7ce010c0
Krazick <markjsmith60@gmail.com>
2022-01-16 19:12:12 -0500

    Add MapBenefit tests

--- JUnitSource/ge18xx/round/action/effects/ChangeMapEffectTests.java
+++ JUnitSource/ge18xx/round/action/effects/ChangeMapEffectTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import ge18xx.company.PrivateCompany;
 import ge18xx.company.ShareCompany;
+import ge18xx.company.benefit.MapBenefit;
 import ge18xx.game.GameManager;
 import ge18xx.game.TestFactory;
 import ge18xx.map.HexMap;
@@ -17,8 +19,10 @@ import ge18xx.toplevel.MapFrame;
 class ChangeMapEffectTests {
 	ChangeMapEffect effectAlpha;
 	ChangeMapEffect effectBeta;
+	ChangeMapEffect effectGamma;
 	ShareCompany sharePenn;
 	ShareCompany shareBnO;
+	PrivateCompany privateSV;
 	GameManager gameManager;
 	PlayerManager playerManager;
 	TestFactory testFactory;
@@ -26,7 +30,8 @@ class ChangeMapEffectTests {
 	HexMap mHexMap;
 	MapCell mapCell1;
 	MapCell mapCell2;
-
+	MapBenefit mapBenefit;
+	
 	@BeforeEach
 	void setUp () throws Exception {
 		String tClientName;
@@ -36,11 +41,14 @@ class ChangeMapEffectTests {
 		gameManager =  testFactory.buildGameManager (tClientName);
 		sharePenn = testFactory.buildAShareCompany (1);
 		shareBnO = testFactory.buildAShareCompany (2);
+		privateSV = testFactory.buildAPrivateCompany (1);
 		mapCell1 = testFactory.buildMapCell ();
 		mapCell2 = testFactory.buildMapCell ("T4");
 		mHexMap = testFactory.buildMockHexMap ();
+		mapBenefit = testFactory.buildMapBenefit (privateSV);
 		effectAlpha = new ChangeMapEffect (sharePenn, mapCell1);
 		effectBeta = new ChangeMapEffect (sharePenn, mapCell2);
+		effectGamma = new ChangeMapEffect (shareBnO, mapCell2, mapBenefit);
 	}
 
 	@Test
@@ -51,5 +59,16 @@ class ChangeMapEffectTests {
 		effectBeta.setMapCellID ("B2");
 		assertEquals ("B2", effectBeta.getMapCellID ());
 		assertEquals (mapCell2, effectAlpha.getMapCell (mHexMap));
+	}
+	
+	@Test
+	@DisplayName ("Benefit used by ChangeMapEffect Tests")
+	void benefitMapEffectTests () {
+		assertFalse (effectAlpha.getBenefitUsed ());
+		assertEquals ("", effectAlpha.getBenefitName ());
+		assertEquals ("", effectAlpha.getBenefitPrivateAbbrev ());
+		assertFalse (effectGamma.getBenefitUsed ());
+		assertEquals ("Map", effectGamma.getBenefitName ());
+		assertEquals ("TEST-C&SL", effectGamma.getBenefitPrivateAbbrev ());
 	}
 }
