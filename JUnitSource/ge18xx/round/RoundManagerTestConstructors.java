9b391e33b37d1f0c81c476b3eeee8448ce01f948
Krazick <markjsmith60@gmail.com>
2022-01-17 13:22:29 -0500

    Update and organize test methods

--- JUnitSource/ge18xx/round/RoundManagerTestConstructors.java
+++ JUnitSource/ge18xx/round/RoundManagerTestConstructors.java
@@ -1,19 +1,25 @@
 package ge18xx.round;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.Mockito;
 
 import ge18xx.game.GameManager;
 import ge18xx.game.GameTestFactory;
+import ge18xx.round.action.ActorI;
 import ge18xx.toplevel.PlayerInputFrame;
 
 @DisplayName ("Round Manager Constructor Tests")
 class RoundManagerTestConstructors {
 	GameManager gameManager;
+	GameManager mGameManager;
 	GameTestFactory gameTestFactory;
 	RoundTestFactory roundTestFactory;
 	RoundManager roundManager;
@@ -30,6 +36,7 @@ class RoundManagerTestConstructors {
 		gameManager.setPlayerInputFrame (mPlayerInputFrame);
 		roundTestFactory = new RoundTestFactory ();
 		roundManager = roundTestFactory.buildRoundManager (gameManager);
+		mGameManager = gameTestFactory.buildGameManagerMock ();
 	}
 
 	@AfterEach
@@ -40,7 +47,52 @@ class RoundManagerTestConstructors {
 	@DisplayName ("Constructor with GameManager and PlayerManager NULL Tests")
 	void constructorTwoArgsTest () {
 		assertEquals (gameManager, roundManager.getGameManager ());
-
 	}
 
+	@Test
+	@DisplayName ("Test Clear Methods") 
+	void clearMethodTests () {
+		Mockito.doNothing ().when (mGameManager).clearAllPlayerSelections ();
+		Mockito.doNothing ().when (mGameManager).clearBankSelections ();
+		Mockito.doNothing ().when (mGameManager).clearAllAuctionStates ();
+
+		roundManager.setGameManager (mGameManager);
+		roundManager.clearAllPlayerSelections ();
+		Mockito.verify (mGameManager, times (1)).clearAllPlayerSelections ();
+		roundManager.clearBankSelections ();
+		Mockito.verify (mGameManager, times (1)).clearBankSelections ();
+		roundManager.clearAllAuctionStates ();
+		Mockito.verify (mGameManager, times (1)).clearAllAuctionStates ();
+	}
+	
+	@Test
+	@DisplayName ("Test Boolean Methods")
+	void booleanMethodTests () {
+		Mockito.when (mGameManager.canPayHalfDividend ()).thenReturn (false);
+		Mockito.when (mGameManager.isPlaceTileMode ()).thenReturn (false);
+		Mockito.when (mGameManager.isPlaceTokenMode ()).thenReturn (false);
+
+		roundManager.setGameManager (mGameManager);
+		assertFalse (roundManager.isPlaceTileMode ());
+		assertFalse (roundManager.isPlaceTokenMode ());
+		assertFalse (roundManager.canPayHalfDividend ());
+		assertFalse (roundManager.isAuctionRound ());
+		assertFalse (roundManager.isStockRound ());
+		assertFalse (roundManager.isOperatingRound ());
+	}
+	
+	@Test
+	@DisplayName ("Test Set and Get Methods")
+	void setAndGetMethodTests () {
+		roundManager.setRoundType(ActorI.ActionStates.StockRound);
+		assertEquals ("Stock Round", roundManager.getCurrentRoundType ().toString ());
+		assertTrue (roundManager.isStockRound ());
+		roundManager.setRoundType(ActorI.ActionStates.AuctionRound);
+		assertEquals ("Auction Round", roundManager.getCurrentRoundType ().toString ());
+		assertTrue (roundManager.isAuctionRound ());
+		roundManager.setRoundType(ActorI.ActionStates.OperatingRound);
+		assertEquals ("Operating Round", roundManager.getCurrentRoundType ().toString ());
+		assertTrue (roundManager.isOperatingRound ());
+
+	}
 }
