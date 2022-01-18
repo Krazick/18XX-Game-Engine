fd367a7d8ec1dfaa437cd9d6fa1d656902ba7e2e
Krazick <markjsmith60@gmail.com>
2022-01-17 19:48:21 -0500

    Update import to not use wildcard

--- JUnitSource/ge18xx/utilities/UtilitiesTestCleaner.java
+++ JUnitSource/ge18xx/utilities/UtilitiesTestCleaner.java
@@ -1,6 +1,6 @@
 package ge18xx.utilities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
