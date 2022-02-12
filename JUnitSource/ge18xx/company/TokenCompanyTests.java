2d5a8624c3c6af3111a26d0f7a72c9c8f688869e
Krazick <markjsmith60@gmail.com>
2022-02-11 19:56:23 -0500

    Add TestCanLayToken Method

--- JUnitSource/ge18xx/company/TokenCompanyTests.java
+++ JUnitSource/ge18xx/company/TokenCompanyTests.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import ge18xx.round.action.ActorI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -33,7 +36,6 @@ class TokenCompanyTests {
 	void tearDown () throws Exception {
 	}
 
-
 	@Test
 	@DisplayName ("Test various 'isA<something>' method")
 	void testCorporationIsAMethods () {
@@ -47,5 +49,65 @@ class TokenCompanyTests {
 		assertFalse (tokenCompany.isShareCompany ());
 		assertFalse (tokenCompany.isBank ());
 		assertFalse (tokenCompany.isBankPool ());
+	}
+	
+	@Test
+	@DisplayName ("Valid for CanLayToken Method")
+	void testCanLayToken () {	
+		MapToken tMapToken;
+		
+		tMapToken = new MapToken ();
+		tokenCompany.setAbbrev ("TRC");
+		tokenCompany.setName ("Token Railway Company");
+		
+		tokenCompany.resetStatus (ActorI.ActionStates.StartedOperations);
+		assertFalse (tokenCompany.canLayToken ());
+		
+		tokenCompany.addMapToken (tMapToken);
+		tokenCompany.addMapToken (tMapToken);
+		assertEquals ("Token Count: 2", tokenCompany.getTokenLabel ());
+
+		assertEquals (2, tokenCompany.getTokenCount ());
+		tokenCompany.resetStatus (ActorI.ActionStates.Unowned);
+		assertFalse (tokenCompany.canLayToken ());
+		
+		tokenCompany.resetStatus (ActorI.ActionStates.StartedOperations);
+		assertTrue (tokenCompany.canLayToken ());
+		
+		tokenCompany.resetStatus (ActorI.ActionStates.Operated);
+		assertFalse (tokenCompany.canLayToken ());
+		
+		tokenCompany.resetStatus (ActorI.ActionStates.StationLaid);
+		assertTrue (tokenCompany.canLayToken ());
+		
+		tokenCompany.resetStatus (ActorI.ActionStates.TileLaid);
+		assertTrue (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.Tile2Laid);
+		assertTrue (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.TileUpgraded);
+		assertTrue (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.TileAndStationLaid);
+		assertTrue (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.OperatedTrain);
+		assertFalse (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.HoldDividend);
+		assertFalse (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.HalfDividend);
+		assertFalse (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.FullDividend);
+		assertFalse (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.BoughtTrain);
+		assertFalse (tokenCompany.canLayToken ());
+
+		tokenCompany.resetStatus (ActorI.ActionStates.Closed);
+		assertFalse (tokenCompany.canLayToken ());
 	}
 }
