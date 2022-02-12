b4c2faba1748db6a9c1cf5c9216239050a7538f5
Krazick <markjsmith60@gmail.com>
2022-02-12 18:04:37 -0500

    New MapTokenTest Methods to set MapCell and Location

--- JUnitSource/ge18xx/company/MapTokenTests.java
+++ JUnitSource/ge18xx/company/MapTokenTests.java
@@ -10,6 +10,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import ge18xx.map.Location;
+import ge18xx.map.MapCell;
+import ge18xx.map.MapTestFactory;
+import ge18xx.tiles.Tile;
+import ge18xx.tiles.TilesTestFactory;
+
 class MapTokenTests {
 	MapToken mapToken;
 	MapToken mapToken1;
@@ -18,8 +24,8 @@ class MapTokenTests {
 	void setUp () throws Exception {
 		mapToken = new MapToken ();
 		mapToken1 = new MapToken (mapToken, 20);
-		mapToken1.setConnectedSide(0, true);
-		mapToken1.setConnectedSide(4, true);
+		mapToken1.setConnectedSide (0, true);
+		mapToken1.setConnectedSide (4, true);
 	}
 
 	@Nested
@@ -55,5 +61,40 @@ class MapTokenTests {
 		assertFalse (mapToken1.getConnectedSide (3));
 		assertFalse (mapToken1.getConnectedSide (13));
 		assertTrue (mapToken1.isConnectedToSide (4));
+	}
+	
+	@Test
+	@DisplayName ("Set MapCell and Location for MapToken Tests")
+	void testMapTokenMapCellAndLocationTests () {
+		MapCell tMapCell;
+		Location tLocation;
+		MapTestFactory tMapTestFactory;
+		TilesTestFactory tTilesTestFactory;
+		Tile tTile;
+		
+		tMapTestFactory = new MapTestFactory ();
+		tMapCell = tMapTestFactory.buildMapCell ();
+		tLocation = tMapTestFactory.buildLocation ();
+		tTilesTestFactory = new TilesTestFactory ();
+		tTile = tTilesTestFactory.buildTile ();
+		tMapCell.putTile (tTile, 0);
+		
+		assertFalse (mapToken.tokenPlaced ());
+		mapToken.setConnectedSides (tMapCell, tLocation);
+		assertEquals ("|0|1|3|4|", mapToken.getSides ());
+		mapToken.setMapCell (tMapCell);
+		assertFalse (mapToken.tokenPlaced ());
+		
+		assertFalse (mapToken1.tokenPlaced ());
+		
+		mapToken1.setConnectedSide (0, false);
+		mapToken1.setConnectedSide (4, false);
+		mapToken1.placeToken (tMapCell, tLocation);
+		
+		assertEquals (tLocation, mapToken1.getLocation ());
+		assertEquals (tMapCell, mapToken1.getMapCell ());
+		assertTrue (mapToken1.tokenPlaced ());
+
+		assertEquals ("T1", mapToken1.getMapCellID ());
 	}
 }
