72fbf9163a91e59817372f7319601b4b83fb8a09
Krazick <markjsmith60@gmail.com>
2022-02-16 11:14:20 -0500

    Setup basic PlayerTestFactory

---
+++ JUnitSource/ge18xx/player/PlayerTestFactory.java
@@ -0,0 +1,20 @@
+package ge18xx.player;
+
+import ge18xx.game.GameManager;
+
+public class PlayerTestFactory {
+	GameManager gameManager;
+	
+	public PlayerTestFactory (GameManager aGameManager) {
+		gameManager = aGameManager;
+	}
+	
+	public PlayerManager buildPlayerManager () {
+		PlayerManager tPlayerManager;
+		
+		tPlayerManager = new PlayerManager (gameManager);
+
+		return tPlayerManager;
+	}
+
+}
