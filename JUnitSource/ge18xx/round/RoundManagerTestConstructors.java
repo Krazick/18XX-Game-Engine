da6ccec72a1e773e8396a97f51ea4a11c445e53f
Krazick <markjsmith60@gmail.com>
2022-02-16 11:15:15 -0500

    New test for getActorMethod

--- JUnitSource/ge18xx/round/RoundManagerTestConstructors.java
+++ JUnitSource/ge18xx/round/RoundManagerTestConstructors.java
@@ -2,6 +2,7 @@ package ge18xx.round;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 
@@ -13,6 +14,8 @@ import org.mockito.Mockito;
 
 import ge18xx.game.GameManager;
 import ge18xx.game.GameTestFactory;
+import ge18xx.player.PlayerTestFactory;
+import ge18xx.player.PlayerManager;
 import ge18xx.round.action.ActorI;
 import ge18xx.toplevel.PlayerInputFrame;
 
@@ -93,6 +96,41 @@ class RoundManagerTestConstructors {
 		roundManager.setRoundType(ActorI.ActionStates.OperatingRound);
 		assertEquals ("Operating Round", roundManager.getCurrentRoundType ().toString ());
 		assertTrue (roundManager.isOperatingRound ());
+	}
+	
+	@Test
+	@DisplayName ("Test getActor Method")
+	void getActorMethodTests () {
+		ActorI tActor;
+		PlayerTestFactory tPlayerTestFactory;
+		PlayerManager tPlayerManager;
+		StockRound tStockRound;
+		OperatingRound tOperatingRound;
+		AuctionRound tAuctionRound;
+		
+		tPlayerTestFactory = new PlayerTestFactory (gameManager);
+		tPlayerManager = tPlayerTestFactory.buildPlayerManager ();
+		tStockRound = roundTestFactory.buildStockRound (tPlayerManager, roundManager);
+		roundManager.setStockRound (tStockRound);
+		tAuctionRound = roundTestFactory.buildAuctionRound (roundManager);
+		roundManager.setAuctionRound (tAuctionRound);
+		tOperatingRound = roundTestFactory.buildOperatingRound (roundManager);
+		roundManager.setOperatingRound (tOperatingRound);
+		
+		roundManager.setRoundType (ActorI.ActionStates.StockRound);
+		tActor = roundManager.getActor ("Stock Round");
+		assertTrue (tActor.isAStockRound ());
+		
+		roundManager.setRoundType (ActorI.ActionStates.AuctionRound);
+		tActor = roundManager.getActor ("Auction Round");
+		assertEquals (tAuctionRound, tActor);
+		
+		
+		roundManager.setRoundType (ActorI.ActionStates.OperatingRound);
+		tActor = roundManager.getActor ("Operating Round");
+		assertTrue (tActor.isAOperatingRound ());
 
+		tActor = roundManager.getActor("RoundManager Tester");
+		assertNull (tActor);
 	}
 }
