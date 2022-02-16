00229058cdd73790fb1ab24cbc680a084ab9068d
Krazick <markjsmith60@gmail.com>
2022-02-16 11:14:50 -0500

    Add buildStockRound, buildOperatingRound, buildAuctionRound methods

--- JUnitSource/ge18xx/round/RoundTestFactory.java
+++ JUnitSource/ge18xx/round/RoundTestFactory.java
@@ -2,6 +2,7 @@ package ge18xx.round;
 
 import org.mockito.Mockito;
 
+import ge18xx.company.CorporationList;
 import ge18xx.game.GameManager;
 import ge18xx.game.GameTestFactory;
 import ge18xx.player.PlayerManager;
@@ -38,9 +39,40 @@ public class RoundTestFactory {
 		return new RoundManager (aGameManager, aPlayerManager);
 		
 	}
+	
 	public RoundManager buildRoundManagerMock () {
 		RoundManager mRoundManager = Mockito.mock (RoundManager.class);
 
 		return mRoundManager;
+	}
+	
+	public StockRound buildStockRound (PlayerManager aPlayerManager, RoundManager aRoundManager) {
+		StockRound tStockRound;
+		
+		tStockRound = new StockRound (aPlayerManager, aRoundManager);
+		
+		return tStockRound;
+	}
+	
+	public OperatingRound buildOperatingRound (RoundManager aRoundManager) {
+		OperatingRound tOperatingRound;
+		CorporationList tPrivatesNull, tCoalsNull, tMinorsNull, tSharesNull;
+		
+		tPrivatesNull = CorporationList.NO_CORPORATION_LIST;
+		tCoalsNull = CorporationList.NO_CORPORATION_LIST;
+		tMinorsNull = CorporationList.NO_CORPORATION_LIST;
+		tSharesNull = CorporationList.NO_CORPORATION_LIST;
+		
+		tOperatingRound = new OperatingRound (aRoundManager, tPrivatesNull, tCoalsNull, tMinorsNull, tSharesNull);
+		
+		return tOperatingRound;
+	}
+
+	public AuctionRound buildAuctionRound (RoundManager aRoundManager) {
+		AuctionRound tAuctionRound;
+		
+		tAuctionRound = new AuctionRound (aRoundManager);
+		
+		return tAuctionRound;
 	}
 }
