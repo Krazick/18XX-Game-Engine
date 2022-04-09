a674af4182d2e1fad8013658b2d76fde0f3846f0
Krazick <markjsmith60@gmail.com>
2022-04-09 09:43:01 -0400

    Use super class buildPortfolioJPanel with single argument

--- src/ge18xx/player/StartPacketPortfolio.java
+++ src/ge18xx/player/StartPacketPortfolio.java
@@ -2,11 +2,7 @@ package ge18xx.player;
 
 import ge18xx.game.GameManager;
 
-import java.awt.Component;
 import java.awt.event.ItemListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 public class StartPacketPortfolio extends Portfolio {
@@ -24,14 +20,7 @@ public class StartPacketPortfolio extends Portfolio {
 	public JPanel buildPortfolioJPanel (String aTitle, boolean aPrivates, boolean aCoals, boolean aMinors, 
 			boolean aShares, String aSelectedButtonLabel, ItemListener aItemListener, 
 			GameManager aGameManager) {
-		BoxLayout tLayout;
-		
-		portfolioInfoJPanel = new JPanel ();
-		portfolioInfoJPanel.setBorder (BorderFactory.createTitledBorder (aTitle));
-		tLayout = new BoxLayout (portfolioInfoJPanel, BoxLayout.Y_AXIS);
-		portfolioInfoJPanel.setLayout (tLayout);
-		portfolioInfoJPanel.setAlignmentX (Component.CENTER_ALIGNMENT);
-		addJCAndVGlue (portfolioInfoJPanel, null);
+		super.buildPortfolioJPanel (aTitle);
 		
 		return portfolioInfoJPanel;
 	}
