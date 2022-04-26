6a4a90fe0dccfc043d8b635fa1fd550a03efcc7a
Krazick <markjsmith60@gmail.com>
2022-04-26 10:37:01 -0400

    Update for Basics of License features

--- src/ge18xx/company/benefit/LicenseBenefit.java
+++ src/ge18xx/company/benefit/LicenseBenefit.java
@@ -1,23 +1,140 @@
 package ge18xx.company.benefit;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import ge18xx.company.CorporationFrame;
+import ge18xx.company.PrivateCompany;
+import ge18xx.company.ShareCompany;
+import ge18xx.utilities.AttributeName;
 import ge18xx.utilities.XMLNode;
+
+// license="true" licenseCost="50" mapCell="P17,P19" value="10"/>
 
 public class LicenseBenefit extends Benefit {
 	public final static String NAME = "License";
-
-	public LicenseBenefit() {
-		// TODO Auto-generated constructor stub
-	}
+	final static AttributeName AN_LICENSE = new AttributeName ("license");
+	final static AttributeName AN_LICENSE_COST = new AttributeName ("licenseCost");
+	final static AttributeName AN_MAP_CELL = new AttributeName ("mapCell");
+	final static AttributeName AN_LICENSE_VALUE = new AttributeName ("value");
+	boolean license;
+	int licenseCost;
+	String mapCellIDs;
+	int value;
 
 	public LicenseBenefit(XMLNode aXMLNode) {
-		super(aXMLNode);
-		// TODO Auto-generated constructor stub
+		super (aXMLNode);
+		
+		String tMapCellIDs;
+		boolean tLicense;
+		int tLicenseValue;
+		int tLicenseCost;
+		
+		tLicense = aXMLNode.getThisBooleanAttribute (AN_LICENSE);
+		tLicenseCost = aXMLNode.getThisIntAttribute (AN_LICENSE_COST);
+		tMapCellIDs = aXMLNode.getThisAttribute (AN_MAP_CELL);
+		tLicenseValue = aXMLNode.getThisIntAttribute (AN_LICENSE_VALUE);
+		setLicense (tLicense);
+		setLicenseCost (tLicenseCost);
+		setMapCellIDs (tMapCellIDs);
+		setLicenseValue (tLicenseValue);
+
+		setName (NAME);
+	}
+
+	public void setLicense (boolean aLicense) {
+		license = aLicense;
+	}
+	
+	public void setLicenseCost (int aLicenseCost) {
+		licenseCost = aLicenseCost;
+	}
+	
+	public void setMapCellIDs (String aMapCellIDs) {
+		mapCellIDs = aMapCellIDs;
+	}
+	
+	public void setLicenseValue (int aLicenseValue) {
+		value = aLicenseValue;
+	}
+	
+	public int getLicenseCost () {
+		return licenseCost;
+	}
+	
+	public int getLicenseValue () {
+		return value;
+	}
+	
+	@Override
+	public int getCost () {
+		return licenseCost;
 	}
 
 	@Override
-	public int getCost() {
-		// TODO Auto-generated method stub
-		return 0;
+	public boolean realBenefit () {
+		return true;
+	}
+
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		String tActionCommand;
+
+		tActionCommand = aEvent.getActionCommand ();
+		if (CorporationFrame.BUY_LICENSE.equals (tActionCommand)) {
+			handleBuyLicense  ();
+		}
+	}
+
+	@Override
+	public void configure (PrivateCompany aPrivateCompany, JPanel aButtonRow) {
+		JButton tPlacePortTokenButton;
+
+		super.configure (aPrivateCompany, aButtonRow);
+		if (shouldConfigure ()) {
+			if (! hasButton ()) {
+				tPlacePortTokenButton = new JButton (getNewButtonLabel ());
+				setButton (tPlacePortTokenButton);
+				setButtonPanel (aButtonRow);
+				tPlacePortTokenButton.setActionCommand (CorporationFrame.BUY_LICENSE);
+				tPlacePortTokenButton.addActionListener (this);
+				aButtonRow.add (tPlacePortTokenButton);
+			}
+			updateButton ();
+		}
+	}
+
+	private void handleBuyLicense () {
+		System.out.println ("Ready to Buy License for " + privateCompany.getAbbrev ());
+	}
+	
+	@Override
+	public String getNewButtonLabel () {
+		String tNewButtonText;
+
+		tNewButtonText = "Buy License from " + privateCompany.getAbbrev ();
+
+		return tNewButtonText;
+	}
+	
+	@Override
+	public void updateButton () {
+		ShareCompany tOwningCompany;
+		Benefit tBenefitInUse;
+		String tBenefitInUseName;
+
+		tOwningCompany = getOwningCompany ();
+		tBenefitInUse = tOwningCompany.getBenefitInUse ();
+		tBenefitInUseName = tBenefitInUse.getName ();
+		if ((tBenefitInUse.realBenefit ()) && (! NAME.equals (tBenefitInUseName))) {
+			disableButton ();
+			setToolTip ("Another Benefit is currently in Use");
+		} else {
+			enableButton ();
+			setToolTip ("All Good");
+		}
 	}
 
 }
