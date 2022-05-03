2d8e257c51a5d980ca82c668cd7beb9cf6bcc183
Krazick <markjsmith60@gmail.com>
2022-05-03 10:42:15 -0400

    Formatting

--- src/ge18xx/round/action/ActorI.java
+++ src/ge18xx/round/action/ActorI.java
@@ -40,9 +40,11 @@ public interface ActorI {
 		SoldDone ("Sold and Done"), BoughtSoldDone ("Bought, Sold and Done"), 
 		Bid ("Bid"), BidDone ("Bid and Done"),								
 		Bidder ("Bidder"), AuctionPass ("Auction Passed"), 					// Player Auction States
-		AuctionRaise ("Auction Raised"), NotBidder ("Not a Bidder"),		
+		AuctionRaise ("Auction Raised"), NotBidder ("Not a Bidder"),
+		
 		NoRound ("No Round", "NR"), StockRound ("Stock Round", "SR"), 					// Round States
 		OperatingRound ("Operating Round", "OR"), AuctionRound ("Auction Round", "AR"),	
+		
 		Unowned ("Unowned"), Owned ("Owned"), Closed ("Closed"), 			// Corporation States
 		MayFloat ("May Float"), WillFloat ("Will Float"), NotOperated ("Not Operated"),			
 		StartedOperations ("Started Operating"), TileLaid ("Tile Laid"), 
