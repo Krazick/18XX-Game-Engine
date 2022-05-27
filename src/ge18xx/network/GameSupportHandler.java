18d52933d3110a4c09f7e74a446a0752a77e9bc3
Krazick <markjsmith60@gmail.com>
2022-05-27 09:39:52 -0400

    Use Constant from NetworkGameSupport for WaitTime

--- src/ge18xx/network/GameSupportHandler.java
+++ src/ge18xx/network/GameSupportHandler.java
@@ -3,11 +3,12 @@ package ge18xx.network;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import ge18xx.game.GameManager;
+
 public class GameSupportHandler {
 	private boolean waitingForResponse;
 	private String response;
 	JGameClient jGameClient;
-	public final static int waitTime = 10;
 	public final static String GS_RESPONSE_TAG = "GSResponse";
 	private final static String GAME_ID = "(\\d\\d\\d\\d-\\d\\d-\\d\\d-\\d\\d\\d\\d)";
 	private final static String GS_WITH_GAME_ID = "<GS gameID=\"" + GAME_ID + "\">(.*)</GS>";
@@ -49,7 +50,7 @@ public class GameSupportHandler {
 	public void holdRequestTillReady () {
 		while (waitingForResponse) {
 			try {
-				Thread.sleep (waitTime + 1);
+				Thread.sleep (GameManager.WAIT_TIME + 1);
 			} catch (InterruptedException e) {
 				System.err.println ("Waiting for the Response to Clear - Exception");
 				e.printStackTrace ();
@@ -68,7 +69,7 @@ public class GameSupportHandler {
 		setWaitingForResponse (tWaitForResponse);
 		while (waitingForResponse) {
 			try {
-				Thread.sleep (waitTime);
+				Thread.sleep (GameManager.WAIT_TIME);
 			} catch (InterruptedException e) {
 				e.printStackTrace ();
 			}
