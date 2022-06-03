6d963277f110ce568858bceea9626830296fe367
Krazick <markjsmith60@gmail.com>
2022-06-03 19:00:18 -0400

    Initial Version of PayLoanInterestAction

---
+++ src/ge18xx/round/action/PayLoanInterestAction.java
@@ -0,0 +1,37 @@
+package ge18xx.round.action;
+
+import ge18xx.bank.Bank;
+import ge18xx.game.GameManager;
+import ge18xx.round.action.ActorI.ActionStates;
+import ge18xx.utilities.XMLNode;
+
+public class PayLoanInterestAction extends CashTransferAction {
+	public final static String NAME = "Pay Loan Interest";
+
+	public PayLoanInterestAction () {
+		super (NAME);
+	}
+
+	public PayLoanInterestAction (String aName) {
+		super (aName);
+	}
+
+	public PayLoanInterestAction (ActionStates aRoundType, String aRoundID, ActorI aActor) {
+		super (aRoundType, aRoundID, aActor);
+		setName (NAME);
+	}
+
+	public PayLoanInterestAction (XMLNode aActionNode, GameManager aGameManager) {
+		super (aActionNode, aGameManager);
+		setName (NAME);
+	}
+	
+	@Override
+	public String getSimpleActionReport () {
+		String tSimpleActionReport = "";
+
+		tSimpleActionReport = actor.getName () + NAME + " on Goverment Loan of the amount " + Bank.formatCash (getCashAmount ());
+
+		return tSimpleActionReport;
+	}
+}
