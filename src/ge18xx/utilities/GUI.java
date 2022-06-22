28dd1bd762e6030711c99f89f602dcc151ae856a
Krazick <markjsmith60@gmail.com>
2022-06-22 15:47:57 -0400

    Make NO_PANEL Constant STATIC

--- src/ge18xx/utilities/GUI.java
+++ src/ge18xx/utilities/GUI.java
@@ -9,7 +9,7 @@ public interface GUI {
 	public static final String NO_TOOL_TIP = "";
 	public static final String NULL_STRING = null;
 	public static final String EMPTY_STRING = "";
-	public final JPanel NO_PANEL = null;
+	public static final JPanel NO_PANEL = null;
 	public static final JLabel NO_LABEL = null;
 	public static final JButton NO_BUTTON = null;
 	public static final JCheckBox NO_CHECK_BOX = null;
