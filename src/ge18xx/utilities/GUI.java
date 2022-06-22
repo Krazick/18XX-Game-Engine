e85e707fe858af02342ccc50c295bc2f3a881079
Krazick <markjsmith60@gmail.com>
2022-06-22 15:47:25 -0400

    Make all Constants STATIC

--- src/ge18xx/utilities/GUI.java
+++ src/ge18xx/utilities/GUI.java
@@ -6,9 +6,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public interface GUI {
-	public final String NO_TOOL_TIP = "";
-	public final String NULL_STRING = null;
-	public final String EMPTY_STRING = "";
+	public static final String NO_TOOL_TIP = "";
+	public static final String NULL_STRING = null;
+	public static final String EMPTY_STRING = "";
 	public final JPanel NO_PANEL = null;
 	public static final JLabel NO_LABEL = null;
 	public static final JButton NO_BUTTON = null;
