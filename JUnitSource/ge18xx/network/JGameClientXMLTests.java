31efe4b05d1e1141e23d2524d3c6c8c3f3e3119d
Krazick <markjsmith60@gmail.com>
2022-06-23 16:08:49 -0400

    Add Test Case for Constructing GameActivity

--- JUnitSource/ge18xx/network/JGameClientXMLTests.java
+++ JUnitSource/ge18xx/network/JGameClientXMLTests.java
@@ -26,8 +26,8 @@ public class JGameClientXMLTests {
 	}
 
 	@Test
-	@DisplayName ("Test Game Activity Generation")
-	public void JGameClient_XMLTests1 () {
+	@DisplayName ("Constructing Game Activity 2 Attributes Tests")
+	public void JGameClientGameActivity2XMLTests () {
 		int tSelectedGameIndex = 0;
 		String tBroadcastMessage = "This is a Broadcast Message";
 		String tGameActivity2, tGameActivity3;
@@ -41,4 +41,23 @@ public class JGameClientXMLTests {
 				tSelectedGameIndex, JGameClient.AN_BROADCAST_MESSAGE, tBroadcastMessage);
 		assertEquals (tExpected1, tGameActivity3);
 	}
+	
+	@Test
+	@DisplayName ("Constructing Game XML 1 Attribute Tests")
+	public void JGameClientGameActivity1XMLTests () {
+		int tSelectedGameIndex = 0;
+//		String tBroadcastMessage = "This is a Broadcast Message";
+		String tGameActivity2;
+//		String tGameActivity3;
+		String tExpected1 = "<GameSelection><NetworkGame gameIndex=\"0\"/></GameSelection>";
+
+		tGameActivity2 = jGameClient.constructGameXML (JGameClient.EN_GAME_SELECTION, JGameClient.EN_NETWORK_GAME, JGameClient.AN_GAME_INDEX,
+				tSelectedGameIndex + "");
+		assertEquals (tExpected1, tGameActivity2);
+
+//		tGameActivity3 = jGameClient.constructGameXML (JGameClient.EN_GAME_SELECTION, JGameClient.EN_NETWORK_GAME, JGameClient.AN_GAME_INDEX,
+//				tSelectedGameIndex);
+//		assertEquals (tExpected1, tGameActivity3);
+	}
+	
 }
