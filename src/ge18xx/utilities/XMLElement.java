839c1aa56e6ec2931a3223b57404e45c6e2213f1
Krazick <markjsmith60@gmail.com>
2022-06-23 20:30:45 -0400

    Add methods to set Attributes and get ChildNodes

--- src/ge18xx/utilities/XMLElement.java
+++ src/ge18xx/utilities/XMLElement.java
@@ -2,29 +2,63 @@ package ge18xx.utilities;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class XMLElement {
 	Element element;
 	public static XMLElement NO_XML_ELEMENT = null;
+	public static Element NO_ELEMENT = null;
 
 	public XMLElement (Element aElement) {
-		element = aElement;
+		setElement (aElement);
 	}
 
 	public XMLElement (Node aNode) {
-		element = (Element) aNode;
+		Element tElement;
+		
+		tElement = (Element) aNode;
+		setElement (tElement);
 	}
 	
 	public void appendChild (XMLElement aXMLElement) {
-		element.appendChild (aXMLElement.getElement ());
+		if (validElement ()) {
+			element.appendChild (aXMLElement.getElement ());
+		}
 	}
 
+	public void setElement (Element aElement) {
+		element = aElement;
+	}
+	
 	public Element getElement () {
 		return element;
 	}
 
+	public boolean validElement () {
+		return element != NO_ELEMENT;
+	}
+	
+	public boolean hasChildNodes () {
+		Node tNode;
+		boolean tHasChildNodes;
+		
+		tHasChildNodes = false;
+		if (validElement ()) {
+			tNode = (Node) element;
+			tHasChildNodes = tNode.hasChildNodes ();
+		}
+		
+		return tHasChildNodes;
+	}
+	
+	public NodeList getChildNodes () {
+		return element.getChildNodes ();
+	}
+
 	public void setAttribute (AttributeName aAttributeName, String aValue) {
-		element.setAttribute (aAttributeName.getString (), aValue);
+		if (validElement ()) {
+			element.setAttribute (aAttributeName.getString (), aValue);
+		}
 	}
 
 	public void setAttribute (AttributeName aAttributeName, int aValue) {
@@ -34,4 +68,31 @@ public class XMLElement {
 	public void setAttribute (AttributeName aAttributeName, boolean aValue) {
 		setAttribute (aAttributeName, new Boolean (aValue).toString ());
 	}
+	
+	public XMLElement getElement (ElementName aElementName) {
+		XMLElement tFoundElement;
+		NodeList tNodeList;
+		int tElementIndex;
+		int tElementCount;
+		Element tElement;
+		String tNodeName;
+		String tElementName;
+		
+		tFoundElement = NO_XML_ELEMENT;
+		if (element.hasChildNodes ()) {
+			tNodeList = element.getChildNodes ();
+			tElementCount = tNodeList.getLength ();
+			tElementName = aElementName.getString ();
+			for (tElementIndex = 0; tElementIndex < tElementCount; tElementIndex++) {
+				tElement = (Element) tNodeList.item (tElementIndex);
+				tNodeName = tElement.getNodeName ();
+				if (tElementName.equals (tNodeName)) {
+					tFoundElement = new XMLElement (tElement);
+				}
+			}
+		}
+		
+		return tFoundElement;
+	}
+
 }
