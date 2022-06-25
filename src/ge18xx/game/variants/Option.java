a092270b4f721f202278eba36518194151353b71
Krazick <markjsmith60@gmail.com>
2022-06-25 13:35:59 -0400

    Extract applyOptionEffect method - to be moved to OptionEffect class

---
+++ src/ge18xx/game/variants/Option.java
@@ -0,0 +1,190 @@
+package ge18xx.game.variants;
+
+import org.w3c.dom.NodeList;
+
+import ge18xx.bank.Bank;
+import ge18xx.company.CorporationList;
+import ge18xx.game.GameManager;
+import ge18xx.train.Train;
+import ge18xx.train.TrainInfo;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.ElementName;
+import ge18xx.utilities.XMLDocument;
+import ge18xx.utilities.XMLElement;
+import ge18xx.utilities.XMLNode;
+
+public class Option {
+	static final String NO_TITLE = "<NO TITLE>";
+	public static final OptionEffect NO_OPTION_EFFECT = null;
+	public static final Option NO_OPTION = null;
+	public static final Option [] NO_OPTIONS = null;
+	public static final ElementName EN_OPTION = new ElementName ("Option");
+	public static final ElementName EN_OPTIONS = new ElementName ("Options");
+	static final AttributeName AN_TITLE = new AttributeName ("title");
+	static final AttributeName AN_TYPE = new AttributeName ("type");
+	static final String TYPE_ALL = "ALL";
+	static final String TYPE_CHOOSE_ANY = "Choose Any";
+	static final String TYPE_CHOOSE_1 = "Choose 1";
+	String title;
+	String type;
+	OptionEffect optionEffects [];
+	boolean enabled;
+
+	public Option () {
+		setValues (NO_TITLE, NO_TITLE);
+	}
+
+	public Option (XMLNode aCellNode) {
+		String tTitle;
+		String tType;
+		String tChildName;
+		XMLNode tChildNode;
+		NodeList tChildren;
+		int tIndex, tChildrenCount, tEffectIndex;
+
+		tTitle = aCellNode.getThisAttribute (AN_TITLE);
+		tType = aCellNode.getThisAttribute (AN_TITLE, TYPE_ALL);
+		setValues (tTitle, tType);
+		tChildren = aCellNode.getChildNodes ();
+		tChildrenCount = tChildren.getLength ();
+		tEffectIndex = 0;
+		optionEffects = new OptionEffect [tChildrenCount];
+		for (tIndex = 0; tIndex < tChildrenCount; tIndex++) {
+			tChildNode = new XMLNode (tChildren.item (tIndex));
+			tChildName = tChildNode.getNodeName ();
+			if (OptionEffect.EN_OPTION_EFFECT.equals (tChildName)) {
+				optionEffects [tEffectIndex++] = new OptionEffect (tChildNode);
+			}
+		}
+	}
+
+	public int getEffectCount () {
+		return optionEffects.length;
+	}
+
+	public OptionEffect getEffectIndex (int aIndex) {
+		OptionEffect tEffect;
+
+		if ((aIndex >= 0) && (aIndex < optionEffects.length)) {
+			tEffect = optionEffects [aIndex];
+		} else {
+			tEffect = NO_OPTION_EFFECT;
+		}
+
+		return tEffect;
+	}
+
+	public XMLElement getOptionElement (XMLDocument aXMLDocument) {
+		XMLElement tXMLElement, tOptionEffectElements, tOptionEffectElement;
+
+		tXMLElement = aXMLDocument.createElement (EN_OPTION);
+		tXMLElement.setAttribute (AN_TITLE, title);
+		tOptionEffectElements = aXMLDocument.createElement (OptionEffect.EN_OPTION_EFFECTS);
+		for (OptionEffect tEffect : optionEffects) {
+			if (tEffect != OptionEffect.NO_OPTION_EFFECT) {
+				tOptionEffectElement = tEffect.getEffectElement (aXMLDocument);
+				tOptionEffectElements.appendChild (tOptionEffectElement);
+			}
+		}
+		tXMLElement.appendChild (tOptionEffectElements);
+
+		return tXMLElement;
+	}
+
+	public String getTitle () {
+		return title;
+	}
+
+	public String getType () {
+		return type;
+	}
+	
+	public boolean isTypeAll () {
+		return (type.equals (TYPE_ALL));
+	}
+	
+	public boolean isTypeChoose1 () {
+		return (type.equals (TYPE_CHOOSE_1));
+	}
+	
+	public boolean isTypeChooseAny () {
+		return (type.equals (TYPE_CHOOSE_ANY));
+	}
+	
+	public boolean isEnabled () {
+		return enabled;
+	}
+
+	public void setEnabled (boolean aEnabled) {
+		enabled = aEnabled;
+	}
+
+	private void setValues (String aTitle, String aType) {
+		title = aTitle;
+		type = aType;
+		setEnabled (false);
+	}
+	
+	// TODO: Build out a set of OptionEffect sub-classes for each different Option
+	// Each Option
+	public void applyOptionEffects (GameManager aGameManager) {
+		int tEffectCount, tEffectIndex;
+		OptionEffect tEffect;
+		
+		tEffectCount = getEffectCount ();
+		for (tEffectIndex = 0; tEffectIndex < tEffectCount; tEffectIndex++) {
+			tEffect = getEffectIndex (tEffectIndex);
+			if (tEffect != OptionEffect.NO_OPTION_EFFECT) {
+				applyOptionEffect (aGameManager, tEffect);
+			}
+		}
+	}
+
+	public void applyOptionEffect (GameManager aGameManager, OptionEffect aEffect) {
+		Train tTrain;
+		Train tNewTrain;
+		String tTrainName;
+		String tEffectAction;
+		int tQuantity;
+		int tFoundQuantity;
+		int tAddThisMany;
+		int tRemoveThisMany;
+		Bank tBank;
+		CorporationList tCorporationList;
+		int tTrainIndex;
+
+		tEffectAction = aEffect.getAction ();
+		if (OptionEffect.MUST_BUY_TRAIN.equals (tEffectAction)) {
+			tCorporationList = aGameManager.getShareCompanies ();
+			System.out.println ("Setting all Companies to MUST BUY TRAIN");
+			tCorporationList.setAllMustBuyTrain ();
+		} else if (OptionEffect.ADD_TO_BANK.equals (tEffectAction)) {
+			tBank = aGameManager.getBank ();
+			tBank.addCash (aEffect.getQuantity ());
+		} else if (OptionEffect.SET_TRAIN_QUANTITY.equals (tEffectAction)) {
+			tTrainName = aEffect.getTrainName ();
+			tQuantity = aEffect.getQuantity ();
+			tBank = aGameManager.getBank ();
+			tTrain = tBank.getTrain (tTrainName);
+			if (tQuantity == TrainInfo.UNLIMITED_TRAINS) {
+				tTrain.setUnlimitedQuantity ();
+			} else {
+				tFoundQuantity = tBank.getTrainQuantity (tTrainName);
+				if (tQuantity > tFoundQuantity) {
+					tAddThisMany = tQuantity - tFoundQuantity;
+					for (tTrainIndex = 0; tTrainIndex < tAddThisMany; tTrainIndex++) {
+						System.out.println ("Train " + tTrain.getName () + " adding " + tAddThisMany);
+						tNewTrain = new Train (tTrain);
+						tBank.addTrain (tNewTrain);
+					}
+				} else if (tQuantity < tFoundQuantity) {
+					tRemoveThisMany = tFoundQuantity - tQuantity;
+					for (tTrainIndex = 0; tTrainIndex < tRemoveThisMany; tTrainIndex++) {
+						tBank.removeTrain (tTrainName);
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ src/ge18xx/game/variants/OptionEffect.java
@@ -0,0 +1,168 @@
+package ge18xx.game.variants;
+
+import org.w3c.dom.NodeList;
+
+import ge18xx.train.Train;
+import ge18xx.train.TrainInfo;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.ElementName;
+import ge18xx.utilities.XMLDocument;
+import ge18xx.utilities.XMLElement;
+import ge18xx.utilities.XMLNode;
+
+public class OptionEffect {
+	static final String NO_NAME = "<NO NAME>";
+	static final int NO_QUANTITY = -1;
+	static final AttributeName AN_TRAIN_NAME = new AttributeName ("trainName");
+	static final AttributeName AN_QUANTITY = new AttributeName ("quantity");
+	static final AttributeName AN_ACTION = new AttributeName ("action");
+	static final AttributeName AN_DEFAULT_EFFECT = new AttributeName ("defaultEffect");
+	static final AttributeName AN_NAME = new AttributeName ("name");
+	static final AttributeName AN_PHASE_NAME = new AttributeName ("phaseName");
+	static final AttributeName AN_CELL_NAME = new AttributeName ("cellName");
+	static final AttributeName AN_MUST_BUY_TRAIN = new AttributeName ("mustBuyTrain");
+	public static final ElementName EN_OPTION_EFFECT = new ElementName ("OptionEffect");
+	public static final ElementName EN_OPTION_EFFECTS = new ElementName ("OptionEffects");
+	public static final OptionEffect NO_OPTION_EFFECT = null;
+	public static final String SET_TRAIN_QUANTITY = "Set Train Quantity";
+	public static final String MUST_BUY_TRAIN = "Must Buy Train";
+	public static final String ADD_TO_BANK = "Add To Bank";
+	public static final String REMOVE_PHASE = "Remove Phase";
+	public static final String ADD_TRAIN = "Add Train";
+	public static final String END_GAME_ON_STOCK_CELL = "End Game on Stock Cell";
+	String name;
+	String action;
+	String actorName;
+	String phaseName;
+	String cellName;
+	TrainInfo trainInfo;
+	int quantity;
+	boolean defaultEffect;
+	boolean state;
+
+	public OptionEffect () {
+		setValue (NO_NAME, NO_NAME, NO_NAME, NO_QUANTITY, NO_NAME, NO_NAME);
+	}
+
+	public OptionEffect (XMLNode aCellNode) {
+		String tAction;
+		String tTrainName;
+		String tPhaseName;
+		String tCellName;
+		String tName;
+		int tQuantity, tTrainCount, tIndex;
+		NodeList tTrainChildren;
+		XMLNode tTrainNode;
+		String tTrainChildName;
+
+		tName = aCellNode.getThisAttribute (AN_NAME);
+		tAction = aCellNode.getThisAttribute (AN_ACTION);
+		if (tAction.equals (SET_TRAIN_QUANTITY)) {
+			tTrainName = aCellNode.getThisAttribute (AN_TRAIN_NAME);
+			tQuantity = aCellNode.getThisIntAttribute (AN_QUANTITY);
+			setValue (tName, tAction, tTrainName, tQuantity, NO_NAME, NO_NAME);
+		} else if (tAction.equals (ADD_TO_BANK)) {
+			tQuantity = aCellNode.getThisIntAttribute (AN_QUANTITY);
+			setValue (tName, tAction, "BANK", tQuantity, NO_NAME, NO_NAME);
+		} else if (tAction.equals (REMOVE_PHASE)) {
+			tPhaseName = aCellNode.getThisAttribute (AN_PHASE_NAME);
+			setValue (tName, tAction, "GAME", NO_QUANTITY, tPhaseName, NO_NAME);
+		} else if (tAction.equals (END_GAME_ON_STOCK_CELL)) {
+			tCellName = aCellNode.getThisAttribute (AN_CELL_NAME);
+			setValue (tName, tAction, "GAME", NO_QUANTITY, NO_NAME, tCellName);
+		} else if (tAction.equals (ADD_TRAIN)) {
+			setValue (tName, tAction, "GAME", NO_QUANTITY, NO_NAME, NO_NAME);
+			tTrainChildren = aCellNode.getChildNodes ();
+			tTrainCount = tTrainChildren.getLength ();
+			for (tIndex = 0; tIndex < tTrainCount; tIndex++) {
+				tTrainNode = new XMLNode (tTrainChildren.item (tIndex));
+				tTrainChildName = tTrainNode.getNodeName ();
+				if (Train.TYPE_NAME.equals (tTrainChildName)) {
+					trainInfo = new TrainInfo (tTrainNode);
+				}
+			}
+		} else if (tAction.equals (MUST_BUY_TRAIN)) {
+			setValue (tName, tAction, "GAME", true);
+		} else {
+			setValue (tName, tAction, NO_NAME, NO_QUANTITY, NO_NAME, NO_NAME);
+		}
+	}
+
+	public String getCellName () {
+		return cellName;
+	}
+
+	public boolean getState () {
+		return state;
+	}
+
+	public XMLElement getEffectElement (XMLDocument aXMLDocument) {
+		XMLElement tXMLElement, tTrainElement;
+
+		tXMLElement = aXMLDocument.createElement (EN_OPTION_EFFECT);
+		tXMLElement.setAttribute (AN_NAME, name);
+		if (actorName != null) {
+			if (!(actorName.equals (NO_NAME))) {
+				tXMLElement.setAttribute (AN_TRAIN_NAME, actorName);
+			}
+		}
+		if (quantity != NO_QUANTITY) {
+			tXMLElement.setAttribute (AN_QUANTITY, quantity);
+		}
+		if (phaseName != NO_NAME) {
+			tXMLElement.setAttribute (AN_PHASE_NAME, phaseName);
+		}
+		if (trainInfo != TrainInfo.NO_TRAIN_INFO) {
+			tTrainElement = trainInfo.getTrainInfoElement (aXMLDocument);
+			tXMLElement.appendChild (tTrainElement);
+		}
+		if (cellName != NO_NAME) {
+			tXMLElement.setAttribute (AN_CELL_NAME, cellName);
+		}
+		if (name.equals (MUST_BUY_TRAIN)) {
+			tXMLElement.setAttribute (AN_MUST_BUY_TRAIN, state);
+		}
+
+		return tXMLElement;
+	}
+
+	public String getAction () {
+		return action;
+	}
+	
+	public String getName () {
+		return name;
+	}
+
+	public String getPhaseName () {
+		return phaseName;
+	}
+
+	public int getQuantity () {
+		return quantity;
+	}
+
+	public TrainInfo getTrainInfo () {
+		return trainInfo;
+	}
+
+	public String getTrainName () {
+		return actorName;
+	}
+
+	private void setValue (String aName, String aAction, String aActorName, boolean aState) {
+		name = aName;
+		action = aAction;
+		actorName = aActorName;
+		state = aState;
+	}
+
+	private void setValue (String aName, String aAction, String aActorName, int aQuantity, String aPhaseName, String aCellName) {
+		name = aName;
+		action = aAction;
+		actorName = aActorName;
+		quantity = aQuantity;
+		phaseName = aPhaseName;
+		cellName = aCellName;
+	}
+}
