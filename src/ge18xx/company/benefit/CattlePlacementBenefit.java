2366f2b2142f6e4f9829131ad0032678d78009d1
Krazick <markjsmith60@gmail.com>
2022-06-26 18:53:47 -0400

    Initial Versions of these Benefits to be expanded

---
+++ src/ge18xx/company/benefit/CattlePlacementBenefit.java
@@ -0,0 +1,22 @@
+package ge18xx.company.benefit;
+
+import ge18xx.utilities.XMLNode;
+
+public class CattlePlacementBenefit extends Benefit {
+
+	public CattlePlacementBenefit () {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CattlePlacementBenefit (XMLNode aXMLNode) {
+		super (aXMLNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCost () {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/ge18xx/company/benefit/FirstORBenefit.java
@@ -0,0 +1,22 @@
+package ge18xx.company.benefit;
+
+import ge18xx.utilities.XMLNode;
+
+public class FirstORBenefit extends Benefit {
+
+	public FirstORBenefit () {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FirstORBenefit (XMLNode aXMLNode) {
+		super (aXMLNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCost () {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
