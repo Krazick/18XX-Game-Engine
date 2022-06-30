ffbfae09573b5d9d3b93cb0b873a1f3a08be8e10
Krazick <markjsmith60@gmail.com>
2022-06-30 14:46:17 -0400

    Add Constant NO_SCROLL_PANE

--- src/ge18xx/utilities/GUI.java
+++ src/ge18xx/utilities/GUI.java
@@ -6,6 +6,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public interface GUI {
 	public static final String NO_TOOL_TIP = "";
@@ -17,4 +18,5 @@ public interface GUI {
 	public static final JCheckBox NO_CHECK_BOX = null;
 	public static final JComponent NO_JCOMPONENT = null;
 	public static final ButtonGroup NO_BUTTON_GROUP = null;
+	public static final JScrollPane NO_SCROLL_PANE = null;
 }
