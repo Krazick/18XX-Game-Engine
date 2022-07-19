044030f25bc4617c93fb9613a78ce62941580ff8
Krazick <markjsmith60@gmail.com>
2022-07-18 21:10:53 -0400

    Move SetWaitStateEffect to WinAuctionAction

--- src/ge18xx/round/action/AuctionPassAction.java
+++ src/ge18xx/round/action/AuctionPassAction.java
@@ -10,20 +10,6 @@ import ge18xx.utilities.XMLNode;
 public class AuctionPassAction extends ChangeStateAction {
 	public final static String NAME = "Auction Pass";
 
-	// When the AuctionPassAction ends the Auction need to have:
-	// TODO: Need to add a Refund Escrow Effect so that the Escrow can be restored
-	// on an UNDO
-	// (One or more, with Player Name, Certificate Name, and Last Bid/Escrow Value)
-	// TODO: Need to add a Remove Bidder Effect so that can record Bidder can be
-	// restored on UNDO
-	// (One or more, with Player Name, Certificate Name, and Last Bid/Escrow Value)
-	// TODO: Remove Bidder for Winning Bidder
-
-	public AuctionPassAction () {
-		super ();
-		setName (NAME);
-	}
-
 	public AuctionPassAction (ActorI.ActionStates aRoundType, String aRoundID, ActorI aActor) {
 		super (aRoundType, aRoundID, aActor);
 		setName (NAME);
