53b7c7dd56af6d92b1bd9d23d73842da79342bcf
Krazick <markjsmith60@gmail.com>
2022-07-23 15:01:03 -0400

    Add methods buildsListCertificatesMock and buildLoadedCertificateMock

--- JUnitSource/ge18xx/company/CertificateTestFactory.java
+++ JUnitSource/ge18xx/company/CertificateTestFactory.java
@@ -1,5 +1,8 @@
 package ge18xx.company;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.mockito.Mockito;
 
 public class CertificateTestFactory {
@@ -8,9 +11,31 @@ public class CertificateTestFactory {
 	}
 
 	public Certificate buildCertificateMock () {
-		Certificate mCertificate = Mockito.mock (Certificate.class);
+		Certificate mCertificate;
 
+		mCertificate = Mockito.mock (Certificate.class);
+		
 		return mCertificate;
 	}
 
+	public List<Certificate> buildListCertificatesMock () {
+		List<Certificate> mCertificatesList;
+		Certificate mCertificate = Mockito.mock (Certificate.class);
+
+		mCertificatesList = new LinkedList<Certificate> ();
+		mCertificate = buildCertificateMock ();
+		mCertificatesList.add (mCertificate);
+		mCertificate = buildCertificateMock ();
+		mCertificatesList.add (mCertificate);
+		
+		return mCertificatesList;
+	}
+
+	public LoadedCertificate buildLoadedCertificateMock () {
+		LoadedCertificate mLoadedCertificate;
+		
+		mLoadedCertificate = Mockito.mock (LoadedCertificate.class);
+		
+		return mLoadedCertificate;
+	}
 }
