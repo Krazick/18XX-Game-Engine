e5401b7e431136af501406e8153cf10a14914b29
Krazick <markjsmith60@gmail.com>
2022-07-24 20:20:52 -0400

    Remove default methods handled by ActorI

--- src/ge18xx/player/Bidder.java
+++ src/ge18xx/player/Bidder.java
@@ -79,56 +79,7 @@ public class Bidder implements ActorI {
 	}
 
 	@Override
-	public boolean isAPlayer () {
-		return false;
-	}
-
-	@Override
-	public boolean isAStockRound () {
-		return false;
-	}
-
-	@Override
-	public boolean isAOperatingRound () {
-		return false;
-	}
-
-	@Override
-	public boolean isABank () {
-		return false;
-	}
-
-	@Override
-	public boolean isABankPool () {
-		return false;
-	}
-
-	@Override
-	public boolean isACorporation () {
-		return false;
-	}
-
-	@Override
-	public boolean isAPrivateCompany () {
-		return false;
-	}
-
-	@Override
-	public boolean isATrainCompany () {
-		return false;
-	}
-
-	@Override
-	public boolean isAShareCompany () {
-		return false;
-	}
-
-	@Override
 	public String getAbbrev () {
 		return getName ();
-	}
-
-	@Override
-	public void completeBenefitInUse () {
 	}
 }
