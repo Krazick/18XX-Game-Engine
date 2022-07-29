f95e7dfbbf8ee1899fb7b72e4eb5a3772552e68b
Krazick <markjsmith60@gmail.com>
2022-07-29 12:29:17 -0400

    Initial Version

---
+++ JUnitSource/ge18xx/train/TrainTestFactory.java
@@ -0,0 +1,27 @@
+package ge18xx.train;
+
+import org.mockito.Mockito;
+
+public class TrainTestFactory {
+
+	public TrainTestFactory () {
+	}
+
+
+	public Train buildTrain () {
+		Train tTrain;
+		
+		tTrain = new Train ();
+		
+		return tTrain;
+	}
+	
+	public Train buildTrainMock () {
+		Train mTrain;
+
+		mTrain = Mockito.mock (Train.class);
+		Mockito.when (mTrain.getCityCount ()).thenReturn (3);
+
+		return mTrain;
+	}
+}
