3fd92be4e0d99b35e26be139274f3764eb117ab8
Krazick <markjsmith60@gmail.com>
2022-07-31 16:57:22 -0400

    New Variant Type to Enable/Disable Benefit on Privates

---
+++ src/ge18xx/game/variants/EnableBenefitVEffect.java
@@ -0,0 +1,94 @@
+package ge18xx.game.variants;
+
+import ge18xx.company.Corporation;
+import ge18xx.company.CorporationList;
+import ge18xx.company.PrivateCompany;
+import ge18xx.game.GameManager;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.XMLDocument;
+import ge18xx.utilities.XMLElement;
+import ge18xx.utilities.XMLNode;
+
+
+public class EnableBenefitVEffect extends VariantEffect {
+	static final String NAME = "Enable Benefit";
+	static final AttributeName AN_COMPANY_ID = new AttributeName ("companyID");
+	static final AttributeName AN_BENEFIT_NAME = new AttributeName ("benefitName");
+
+	String benefitName;
+	int companyID;
+
+	public EnableBenefitVEffect () {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EnableBenefitVEffect (XMLNode aXMLNode) {
+		super (aXMLNode);
+		
+		int tCompanyID;
+		String tBenefitName;
+
+		tCompanyID = aXMLNode.getThisIntAttribute (AN_COMPANY_ID);
+		setCompanyID (tCompanyID);
+		tBenefitName = aXMLNode.getThisAttribute (AN_BENEFIT_NAME);
+		setBenefitName (tBenefitName);
+	}
+	
+	public int getCompanyID () {
+		return companyID;
+	}
+	
+	public void setCompanyID (int aCompanyID) {
+		companyID = aCompanyID;
+	}
+	
+	public String getBenefitName () {
+		return benefitName;
+	}
+	
+	public void setBenefitName (String aBenefitName) {
+		benefitName = aBenefitName;
+	}
+	
+	/**
+	 * Given an XMLDocument, this will create the XMLElement by using the super-class and then stores 
+	 * the CompanyID and the VariantEffect Class
+	 * 
+	 * @param aXMLDocument The XMLDocumdnt to use to create the XMLElement
+	 * 
+	 * @return the filled out XMLElement
+	 * 
+	 */
+	@Override
+	public XMLElement getEffectElement (XMLDocument aXMLDocument) {
+		XMLElement tXMLElement;
+		
+		tXMLElement = super.getEffectElement (aXMLDocument);
+		tXMLElement.setAttribute (AN_COMPANY_ID, companyID);
+		tXMLElement.setAttribute (AN_BENEFIT_NAME, benefitName);
+		tXMLElement.setAttribute (AN_CLASS, getClass ().getName ());
+		
+		return tXMLElement;
+	}
+	
+	/**
+	 * Apply the Variant Effect using the Game Manager as needed.
+	 * 
+	 * @param aGameManager The current GameManager to have the Variant Effect applied to.
+	 * 
+	 */
+	@Override
+	public void applyVariantEffect (GameManager aGameManager) {
+		CorporationList tCorporationList;
+		PrivateCompany tPrivate;
+		Corporation tCorporation;
+		
+		System.out.println ("Activate Company " + companyID + " Query Exchange Benefit");
+		tCorporationList = aGameManager.getPrivates ();
+		tCorporation = tCorporationList.getCorporationByID (companyID);
+		if (tCorporation != Corporation.NO_CORPORATION) {
+			tPrivate = (PrivateCompany) tCorporation;
+			tPrivate.enableBenefit (benefitName);
+		}
+	}
+}
