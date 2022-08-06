b07cc479a884e22a644aa26e9ecee96314cb4ff5
Krazick <markjsmith60@gmail.com>
2022-08-06 10:16:36 -0400

    Extract out alternate sub-class 'PurchasePrivateOffer'

--- src/ge18xx/company/QueryOffer.java
+++ src/ge18xx/company/QueryOffer.java
@@ -4,7 +4,6 @@ import org.w3c.dom.NodeList;
 
 import ge18xx.round.action.ActorI;
 import ge18xx.round.action.GenericActor;
-import ge18xx.train.Train;
 import ge18xx.utilities.AttributeName;
 import ge18xx.utilities.ElementName;
 import ge18xx.utilities.XMLDocument;
@@ -18,11 +17,8 @@ public class QueryOffer {
 	public static final AttributeName AN_ITEM_TYPE = new AttributeName ("itemType");
 	public static final AttributeName AN_FROM_ACTOR_NAME = new AttributeName ("fromActorName");
 	public static final AttributeName AN_TO_ACTOR_NAME = new AttributeName ("toActorName");
-	public static final AttributeName AN_PRIVATE_ABBREV = new AttributeName ("privateAbbrev");
 	public static final AttributeName AN_OLD_STATUS = new AttributeName ("oldStatus");
 	public static final AttributeName AN_STATUS = new AttributeName ("status");
-	public static final String TRAIN_TYPE = Train.TYPE_NAME;
-	public static final String PRIVATE_TYPE = Corporation.PRIVATE_COMPANY;
 	public static final String NONE = "None";
 	public static final String ACCEPTED = "Accepted";
 	public static final String PENDING = "Pending";
@@ -32,19 +28,13 @@ public class QueryOffer {
 	protected String itemType;
 	String fromActorName;
 	String toActorName;
-	PrivateCompany privateCompany;
 	ActorI.ActionStates oldStatus;
 	// TODO Convert to an ENUM with options on Status.
 	String status;
 
-	public QueryOffer (String aItemName, String aItemType, PrivateCompany aPrivateCompany,
+	public QueryOffer (String aItemName, String aItemType,
 			String aFromActorName, String aToName, ActorI.ActionStates aOldState) {
 
-		if (PRIVATE_TYPE.equals (aItemType)) {
-			setPrivateCompany (aPrivateCompany);
-		} else {
-			System.err.println ("The Type " + aItemType + " is not a " + TRAIN_TYPE + " or a " + PRIVATE_TYPE);
-		}
 		setItemName (aItemName);
 		setItemType (aItemType);
 		setFromActorName (aFromActorName);
@@ -66,7 +56,6 @@ public class QueryOffer {
 		ActorI.ActionStates tOldState;	
 		GenericActor tGenericActor;
 		String tStatus;
-		String tPrivateAbbrev;
 
 		tPurchaseOfferList = aChildNode.getChildNodes ();
 		tPOCount = tPurchaseOfferList.getLength ();
@@ -78,7 +67,6 @@ public class QueryOffer {
 			tToActorName = tPONode.getThisAttribute (AN_TO_ACTOR_NAME);
 			tOldStateName = tPONode.getThisAttribute (AN_OLD_STATUS);
 			tStatus = tPONode.getThisAttribute (AN_STATUS);
-			tPrivateAbbrev = tPONode.getThisAttribute (AN_PRIVATE_ABBREV);
 			setItemName (tItemName);
 			setItemType (tItemType);
 			setFromActorName (tFromActorName);
@@ -88,7 +76,6 @@ public class QueryOffer {
 			tOldState = tGenericActor.getCorporationActionState (tOldStateName);
 			setOldState (tOldState);
 			setStatus (tStatus);
-			System.out.println ("Private Abbrev [" + tPrivateAbbrev + "]");
 		}
 	}
 	
@@ -105,10 +92,6 @@ public class QueryOffer {
 		tXMLElement.setAttribute (AN_ITEM_NAME, fromActorName);
 		tXMLElement.setAttribute (AN_FROM_ACTOR_NAME, fromActorName);
 		tXMLElement.setAttribute (AN_TO_ACTOR_NAME, toActorName);
-		if (privateCompany != PrivateCompany.NO_PRIVATE_COMPANY) {
-			tXMLElement.setAttribute (AN_PRIVATE_ABBREV, privateCompany.getAbbrev ());
-			// TODO -- When loading need to get the Private Company for the given Name, and attach to the Purchase Offer
-		}
 		tXMLElement.setAttribute (AN_OLD_STATUS, oldStatus.toString ());
 		tXMLElement.setAttribute (AN_STATUS, status);
 
@@ -151,10 +134,6 @@ public class QueryOffer {
 		
 		return tIsPending;
 	}
-	
-	private void setPrivateCompany (PrivateCompany aPrivateCompany) {
-		privateCompany = aPrivateCompany;
-	}
 
 	private void setItemName (String aItemName) {
 		itemName = aItemName;
@@ -174,22 +153,6 @@ public class QueryOffer {
 
 	private void setOldState (ActorI.ActionStates aOldState) {
 		oldStatus = aOldState;
-	}
-
-	public boolean isPrivateCompany () {
-		boolean tIsPrivateCompany = false;
-
-		if (PRIVATE_TYPE.equals (itemType)) {
-			tIsPrivateCompany = true;
-		} else {
-			tIsPrivateCompany = false;
-		}
-
-		return tIsPrivateCompany;
-	}
-
-	public PrivateCompany getPrivateCompany () {
-		return privateCompany;
 	}
 
 	public String getItemName () {
