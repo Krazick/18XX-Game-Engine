932effb22ca796287176f27745a1e80f983a50d0
Krazick <markjsmith60@gmail.com>
2022-08-09 08:26:40 -0400

    Handle setState and SetDefaultState in Constructor

--- src/ge18xx/game/variants/VariantToggle.java
+++ src/ge18xx/game/variants/VariantToggle.java
@@ -6,6 +6,16 @@ import ge18xx.utilities.XMLNode;
 public class VariantToggle extends VariantAll {
 	public VariantToggle (XMLNode aXMLNode) {
 		super (aXMLNode);
+		
+		boolean tDefault;
+		
+		tDefault = aXMLNode.getThisBooleanAttribute (AN_DEFAULT);
+		for (VariantEffect tEffect: variantEffects) {
+			if (tEffect != VariantEffect.NO_VARIANT_EFFECT) {
+				tEffect.setState (tDefault);
+				tEffect.setDefaultEffect (tDefault);
+			}
+		}
 	}
 
 	@Override
