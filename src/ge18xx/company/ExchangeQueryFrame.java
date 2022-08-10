51ed6880076561e94e01b97f91bec99ec2ac73e3
Krazick <markjsmith60@gmail.com>
2022-08-10 13:18:37 -0400

    New Subclass of QueryFrame to handle ExchangeQuery

---
+++ src/ge18xx/company/ExchangeQueryFrame.java
@@ -0,0 +1,43 @@
+package ge18xx.company;
+
+import ge18xx.company.benefit.QueryExchangeBenefit;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
+import ge18xx.round.action.ResponseOfferAction;
+import ge18xx.round.action.effects.QueryExchangeBenefitEffect;
+import ge18xx.round.action.effects.ToEffect;
+
+public class ExchangeQueryFrame extends QueryFrame {
+	public final static String NAME = "QUERY EXCHANGE";
+
+	private static final long serialVersionUID = 1L;
+
+	public ExchangeQueryFrame (RoundManager aRoundManager, ToEffect aToEffect) {
+		super (aRoundManager, aToEffect);
+		String tPlayerName;
+
+		tPlayerName = aRoundManager.getClientUserName ();
+		setTitle ("Exchange Query for " + tPlayerName);
+	}
+	
+	@Override
+	protected void setOfferTopPanel () {
+		String tOffer;
+		QueryExchangeBenefitEffect tQueryExchangeBenefitEffect;
+		QueryExchangeBenefit tQueryExchangeBenefit;
+		
+		if (toEffect instanceof QueryExchangeBenefitEffect) {
+			tQueryExchangeBenefitEffect = (QueryExchangeBenefitEffect) toEffect;
+			tQueryExchangeBenefit = tQueryExchangeBenefitEffect.getQueryExchangeBenefit ();
+			
+			tOffer = tQueryExchangeBenefit.buildQueryText ();
+			buildOfferTopPanel (tOffer);
+		}
+	}
+	
+	@Override
+	protected void addResponseOfferEffect (ResponseOfferAction aResponseOfferAction, ActorI aFromActor, 
+			ActorI aToActor, boolean aResponse) {
+		aResponseOfferAction.addResponseOfferEffect (aFromActor, aToActor, aResponse, NAME);
+	}
+}
