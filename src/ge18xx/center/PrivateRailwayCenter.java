0cb771fcea072fd4d1328ddadae11f8a6b3b4608
Krazick <markjsmith60@gmail.com>
2022-08-21 14:12:17 -0400

    Remove test for CityInfo being NO_CITY_INFO

--- src/ge18xx/center/PrivateRailwayCenter.java
+++ src/ge18xx/center/PrivateRailwayCenter.java
@@ -20,9 +20,7 @@ public class PrivateRailwayCenter extends RevenueCenter {
 	@Override
 	public void draw (Graphics g, int Xc, int Yc, int aTileOrient, Hex aHex, boolean onTile,
 			Feature2 aSelectedFeature) {
-		if (cityInfo != CityInfo.NO_CITY_INFO) {
-			cityInfo.drawPrivateRailway (g, Xc, Yc, aHex);
-		}
+		cityInfo.drawPrivateRailway (g, Xc, Yc, aHex);
 	}
 
 	@Override
