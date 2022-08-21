f92d93c522d109276a09c9f7017e56cafff90e4d
Krazick <markjsmith60@gmail.com>
2022-08-21 14:11:48 -0400

    Initial Versions of JUnit Classes

---
+++ JUnitSource/ge18xx/center/CenterTestFactory.java
@@ -0,0 +1,51 @@
+package ge18xx.center;
+
+import java.awt.Graphics;
+
+import org.mockito.Mockito;
+
+import ge18xx.game.GameTestFactory;
+import ge18xx.utilities.UtilitiesTestFactory;
+import ge18xx.utilities.XMLNode;
+
+public class CenterTestFactory {
+	private GameTestFactory gameTestFactory;
+	private UtilitiesTestFactory utilitiesTestFactory;
+
+	/**
+	 * Builds the Center Test Factory by creating the gameTestFactory and get the Utilities Test Factory
+	 * 
+	 */
+	public CenterTestFactory () {
+		gameTestFactory = new GameTestFactory ();
+		utilitiesTestFactory = gameTestFactory.getUtilitiesTestFactory ();
+	}
+
+	public PrivateRailwayCenter buildPrivateRailwayCenter () {
+		PrivateRailwayCenter tPrivateRailwayCenter;
+		String tPrivateRailwayCenterXMLText = "<RevenueCenter id=\"0\" location=\"12\" name=\"CTF Private\" type=\"Private Railway\" />\n";
+		XMLNode tPrivateRailwayCenterNode;
+
+		tPrivateRailwayCenterNode = utilitiesTestFactory.buildXMLNode (tPrivateRailwayCenterXMLText);
+		tPrivateRailwayCenter = (PrivateRailwayCenter) RevenueCenter.NO_CENTER;
+		if (tPrivateRailwayCenterNode != XMLNode.NO_NODE) {
+			tPrivateRailwayCenter = new PrivateRailwayCenter (tPrivateRailwayCenterNode);
+		}
+
+		return tPrivateRailwayCenter;
+	}
+
+	public CityInfo buildCityInfoMock () {
+		CityInfo mCityInfo = Mockito.mock (CityInfo.class);
+
+		Mockito.when (mCityInfo.getName ()).thenReturn ("MCityInfo");
+
+		return mCityInfo;
+	}
+	
+	public Graphics buildGraphicsMock () {
+		Graphics mGraphics = Mockito.mock (Graphics.class);
+
+		return mGraphics;
+	}
+}
---
+++ JUnitSource/ge18xx/center/PrivateRailwayCenterTests.java
@@ -0,0 +1,70 @@
+package ge18xx.center;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+
+import java.awt.Graphics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import ge18xx.map.Hex;
+import ge18xx.tiles.Feature2;
+
+@DisplayName ("Testing PrivateRailwayCenter Class")
+class PrivateRailwayCenterTests {
+	PrivateRailwayCenter privateRailywayCenter;
+	CenterTestFactory centerTestFactory;
+	
+	@BeforeEach
+	void setUp () throws Exception {
+		centerTestFactory = new CenterTestFactory ();
+		privateRailywayCenter = centerTestFactory.buildPrivateRailwayCenter ();
+	}
+
+	@Test
+	@DisplayName ("Private Railway Center Tests")
+	void privateRailwayCenterTests () {
+		assertFalse (privateRailywayCenter.cityOrTown ());
+		assertFalse (privateRailywayCenter.isCity ());
+		assertFalse (privateRailywayCenter.cityHasOpenStation ());
+		assertTrue (privateRailywayCenter.validCityInfo ());
+	}
+
+	@Test
+	@DisplayName ("Private Railway Center CityInfo Test")
+	void privateRailwayCenterCityInfoTest () {
+		CityInfo mCityInfo;
+		
+		Graphics mGraphics;
+		int tX, tY, tOrient;
+		Hex tHex;
+		boolean tOnTile;
+		Feature2 tFeature;
+		
+		mCityInfo = centerTestFactory.buildCityInfoMock ();
+		
+		Mockito.doNothing ().when (mCityInfo).drawPrivateRailway (any (Graphics.class), anyInt (), anyInt (), 
+				any (ge18xx.map.Hex.class));
+		
+		mGraphics = centerTestFactory.buildGraphicsMock ();
+		tX = 10;
+		tY = 10;
+		tOrient = 0;
+		tHex = new Hex ();
+		tOnTile = false;
+		tFeature = new Feature2 ();
+
+		privateRailywayCenter.setCityInfo (mCityInfo);
+
+		privateRailywayCenter.draw (mGraphics, tX, tY, tOrient, tHex, tOnTile, tFeature);
+		Mockito.verify (mCityInfo, times (1)).drawPrivateRailway (any (Graphics.class), anyInt (), anyInt (), 
+				any (ge18xx.map.Hex.class));
+
+	}
+}
