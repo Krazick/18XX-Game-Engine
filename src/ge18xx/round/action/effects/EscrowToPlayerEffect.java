3260ed05513e5c84cd140111879349a10e999b5a
Krazick <markjsmith60@gmail.com>
2022-11-14 14:25:25 -0500

    Store Cash, HolderName and Certificate for reporting purposes

--- src/ge18xx/round/action/effects/EscrowToPlayerEffect.java
+++ src/ge18xx/round/action/effects/EscrowToPlayerEffect.java
@@ -17,7 +17,11 @@ public class EscrowToPlayerEffect extends Effect {
 	public final static String NAME = "Add Escrow to Player";
 	final static AttributeName AN_ESCROW_TO_PLAYER_COMPANY = new AttributeName ("escrowToPlayerCompany");
 	final static AttributeName AN_ESCROW_TO_PLAYER_CASH = new AttributeName ("escrowToPlayerCash");
+	final static AttributeName AN_ESCROW_HOLDER_NAME = new AttributeName ("escrowHolderName");
 	Escrow escrow;
+	int cash;
+	String holderName;
+	Certificate certificate;
 
 	public EscrowToPlayerEffect () {
 		super (NAME, ActorI.NO_ACTOR);
@@ -35,26 +39,30 @@ public class EscrowToPlayerEffect extends Effect {
 		setName (NAME);
 
 		String tCertificateName;
+		String tHolderName;
 		Certificate tCertificate;
 		int tCash;
 
 		tCertificateName = aEffectNode.getThisAttribute (AN_ESCROW_TO_PLAYER_COMPANY);
 		tCertificate = aGameManager.getCertificate (tCertificateName, 100, true);
 		tCash = aEffectNode.getThisIntAttribute (AN_ESCROW_TO_PLAYER_CASH);
+		tHolderName = aEffectNode.getThisAttribute (AN_ESCROW_HOLDER_NAME);
 		escrow = new Escrow (tCertificate, tCash);
+		escrow.setName (tHolderName);
+		setHolderName (tHolderName);
+		setCertificate (tCertificate);
 	}
 
 	@Override
 	public XMLElement getEffectElement (XMLDocument aXMLDocument, AttributeName aActorAN) {
 		XMLElement tEffectElement;
 		String tCompanyAbbrev;
-		int tCash;
 
 		tEffectElement = super.getEffectElement (aXMLDocument, aActorAN);
-		tCompanyAbbrev = escrow.getCompanyAbbrev ();
-		tCash = escrow.getCash ();
+		tCompanyAbbrev = certificate.getCompanyAbbrev ();
 		tEffectElement.setAttribute (AN_ESCROW_TO_PLAYER_COMPANY, tCompanyAbbrev);
-		tEffectElement.setAttribute (AN_ESCROW_TO_PLAYER_CASH, tCash);
+		tEffectElement.setAttribute (AN_ESCROW_TO_PLAYER_CASH, cash);
+		tEffectElement.setAttribute (AN_ESCROW_HOLDER_NAME, holderName);
 
 		return tEffectElement;
 	}
@@ -65,8 +73,39 @@ public class EscrowToPlayerEffect extends Effect {
 
 	public void setEscrow (Escrow aEscrow) {
 		escrow = aEscrow;
+		saveEscrowValues ();
 	}
 
+	public void saveEscrowValues () {
+		setCash (escrow.getCash ());
+		setHolderName (escrow.getName ());
+		setCertificate (escrow.getCertificate ());
+	}
+	
+	public int getCash () {
+		return cash;
+	}
+	
+	public String getHolderName () {
+		return holderName;
+	}
+	
+	public Certificate getCertificate () {
+		return certificate;
+	}
+	
+	public void setCash (int aCash) {
+		cash = aCash;
+	}
+	
+	public void setCertificate (Certificate aCertificate) {
+		certificate = aCertificate;
+	}
+	
+	public void setHolderName (String aHolderName) {
+		holderName = aHolderName;
+	}
+	
 	@Override
 	public String getEffectReport (RoundManager aRoundManager) {
 		return (REPORT_PREFIX + name + " for " + escrow.getCompanyAbbrev () + " made by " + actor.getName () + ".");
