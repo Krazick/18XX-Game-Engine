18d85c8061017d126d4c8dd5ffdaa8901d2dfd71
Krazick <markjsmith60@gmail.com>
2022-11-15 20:05:18 -0500

    Improve display and add resend functionality

--- src/ge18xx/network/ResendLastActionsFrame.java
+++ src/ge18xx/network/ResendLastActionsFrame.java
@@ -1,12 +1,22 @@
 package ge18xx.network;
 
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import ge18xx.game.GameManager;
@@ -14,17 +24,23 @@ import ge18xx.round.action.Action;
 import ge18xx.round.action.ActionManager;
 import ge18xx.toplevel.XMLFrame;
 
-public class ResendLastActionsFrame extends XMLFrame {
+public class ResendLastActionsFrame extends XMLFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
+	private static final String CONFIRM_RESEND = "Confirm Resend Actions";
+	private static final String CANCEL = "Cancel";
+	private final static String NEWLINE = "\n";
 	private ActionManager actionManager;
 	JButton confirmResendButton;
 	JButton cancelButton;
 	JLabel summaryToResendLabel;
 	JLabel networkLastActionLabel;
 	JTextArea listToResendTextArea;
+	JScrollPane resendActionsScrollPane;
+	JPanel resendConfirmPanel;
 	String summary;
 	String networkLastAction;
+	List<Action> resendTheseActions;
 
 	public ResendLastActionsFrame (String aFrameName, ActionManager aActionManager) {
 		super (aFrameName);
@@ -36,12 +52,52 @@ public class ResendLastActionsFrame extends XMLFrame {
 		actionManager = aActionManager;
 	}
 
-	private void buildComponents () {
-		summaryToResendLabel = new JLabel (summary);
-		networkLastActionLabel = new JLabel (networkLastAction);
-		confirmResendButton = new JButton ("Confirm Resend");
+	private void buildResendPanel () {
+		int padding1;
+		Point tRoundFrameOffset;
+		GameManager tGameManager;
 		
+		padding1 = 10;
+		buildComponents ();
+		resendConfirmPanel.setBorder (BorderFactory.createEmptyBorder (padding1, padding1, padding1, padding1));
+		resendConfirmPanel.setLayout (new BoxLayout (resendConfirmPanel, BoxLayout.Y_AXIS));
+		resendConfirmPanel.add (Box.createVerticalGlue ());
+		resendConfirmPanel.add (summaryToResendLabel);
+		resendConfirmPanel.add (Box.createVerticalGlue ());
+		resendConfirmPanel.add (networkLastActionLabel);
+		resendConfirmPanel.add (Box.createVerticalGlue ());
+		resendConfirmPanel.add (resendActionsScrollPane);
+		resendConfirmPanel.add (Box.createVerticalGlue ());
+		resendConfirmPanel.add (confirmResendButton);
+		resendConfirmPanel.add (Box.createVerticalGlue ());
+		resendConfirmPanel.add (cancelButton);
+		resendConfirmPanel.add (Box.createVerticalStrut (10));
+		add (resendConfirmPanel);
+		setPreferredSize (new Dimension (700, 250));
+		tGameManager = actionManager.getGameManager ();
+		tRoundFrameOffset = tGameManager.getOffsetRoundFrame ();
+		setLocation (tRoundFrameOffset);
+		pack ();
+		setVisible (true);
 	}
+
+	private void buildComponents () {
+		resendConfirmPanel = new JPanel ();
+		summaryToResendLabel = new JLabel (summary);
+		summaryToResendLabel.setAlignmentX (Component.CENTER_ALIGNMENT);
+		networkLastActionLabel = new JLabel (networkLastAction);
+		networkLastActionLabel.setAlignmentX (Component.CENTER_ALIGNMENT);
+		confirmResendButton = setupButton (CONFIRM_RESEND, CONFIRM_RESEND, this, Component.CENTER_ALIGNMENT);
+		cancelButton = setupButton (CANCEL, CANCEL, this, Component.CENTER_ALIGNMENT);
+		
+		listToResendTextArea = new JTextArea ();
+		resendActionsScrollPane = new JScrollPane ();
+		resendActionsScrollPane.setAutoscrolls (true);
+		resendActionsScrollPane.setViewportBorder (null);
+		resendActionsScrollPane.setViewportView (listToResendTextArea);
+		resendActionsScrollPane.setPreferredSize (new Dimension (680, 120));
+	}
+	
 	public void resendLastActionsToNet () {
 		Action tAction;
 		int tNetworkLastActionNumber;
@@ -50,10 +106,10 @@ public class ResendLastActionsFrame extends XMLFrame {
 		int tToNumber;
 		String tFromName;
 		String tToName;
-		List<Action> tResendTheseActions;
+		String tFullList;
 		boolean tGetActionsToResend;
-		
-		tResendTheseActions = new LinkedList<Action> ();
+
+		resendTheseActions = new LinkedList<Action> ();
 		tLastActionIndex = actionManager.getLastActionIndex (ActionManager.PREVIOUS_ACTION);
 		tGetActionsToResend = true;
 		tAction = actionManager.getLastAction ();
@@ -61,12 +117,15 @@ public class ResendLastActionsFrame extends XMLFrame {
 		tFromNumber = tToNumber;
 		tToName = tAction.getActorName ();
 		tFromName = tToName;
+		tFullList = "";
 		while (tGetActionsToResend) {
 			tAction = actionManager.getActionAt (tLastActionIndex);
 			if (tAction != Action.NO_ACTION) {
 				tFromNumber = tAction.getNumber ();
 				tFromName = tAction.getActorName ();
-				tResendTheseActions.add (tAction);
+				resendTheseActions.add (0, tAction);
+
+				tFullList = tAction.getBriefActionReport () + NEWLINE + tFullList;
 				tGetActionsToResend = tAction.getChainToPrevious ();	
 				tLastActionIndex--;
 			} else {
@@ -74,9 +133,11 @@ public class ResendLastActionsFrame extends XMLFrame {
 			}
 		}
 		tNetworkLastActionNumber = getNetworkLastActionNumber ();
-		summary = "Ready to resend " + tResendTheseActions.size () + " Actions from " + tFromNumber + 
+		summary = "Ready to resend " + resendTheseActions.size () + " Actions from " + tFromNumber + 
 				" (" + tFromName + ") " + " to " + tToNumber + " (" + tToName + ")";
 		networkLastAction = "Network's Last Action for this game is " + tNetworkLastActionNumber;
+		buildResendPanel ();
+		listToResendTextArea.setText (tFullList);
 	}
 
 	public int getNetworkLastActionNumber () {
@@ -101,4 +162,19 @@ public class ResendLastActionsFrame extends XMLFrame {
 		return tNetworkLastActionNumber;
 	}
 
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		String tEventAction;
+
+		tEventAction = aEvent.getActionCommand ();
+		if (CONFIRM_RESEND.equals (tEventAction)) {
+			System.out.println ("Trying to Resend to Network");
+			for (Action tAction : resendTheseActions ) {
+				actionManager.sendActionToNetwork (tAction);
+			}
+		}
+		if (CANCEL.equals (tEventAction)) {
+			setVisible (false);
+		}
+	}
 }
