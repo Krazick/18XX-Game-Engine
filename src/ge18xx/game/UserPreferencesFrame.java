2b56122a1e6bc951463e375e1ab6fbafd3a17bf8
Krazick <markjsmith60@gmail.com>
2022-11-24 10:06:53 -0500

    Add methods setFrameInfoPanel and setColorsPanel

--- src/ge18xx/game/UserPreferencesFrame.java
+++ src/ge18xx/game/UserPreferencesFrame.java
@@ -14,7 +14,7 @@ public class UserPreferencesFrame extends XMLFrame {
 	JTabbedPane tabbedPane;
 	JPanel userPreferencesPanel;
 	JPanel frameInfoPanel;
-	JPanel p3;
+	JPanel colorsPanel;
 	
 	public UserPreferencesFrame (String aFrameName) {
 		super (aFrameName);
@@ -29,23 +29,33 @@ public class UserPreferencesFrame extends XMLFrame {
 	}
 	
 	private void setupJTabbedPane () {
+		JPanel tColorsPanel;
 		tabbedPane = new JTabbedPane ();
 		userPreferencesPanel = new JPanel ();
 		
 		JTextArea ta = new JTextArea (200, 200);
 
 		userPreferencesPanel.add (ta);
-		frameInfoPanel = new JPanel ();
-		p3 = new JPanel ();
+		
+		tColorsPanel = new JPanel ();
 		tabbedPane.setBounds (50, 50, 200, 200);
 		tabbedPane.setBackground (Color.blue);
 		tabbedPane.setForeground (Color.white);
 		tabbedPane.add ("User Preferences", userPreferencesPanel);
-		tabbedPane.add ("Frame Info", frameInfoPanel);
-		tabbedPane.add ("help", p3);
+		setColorsPanel (tColorsPanel);
 
 		add (tabbedPane);
 		
 		setSize (500, 500);
+	}
+	
+	public void setFrameInfoPanel (JPanel aFrameInfoPanel) {
+		frameInfoPanel = aFrameInfoPanel;
+		tabbedPane.add ("Frame Info", frameInfoPanel);
+	}
+	
+	public void setColorsPanel (JPanel aColorsPanel) {
+		colorsPanel = aColorsPanel;
+		tabbedPane.add ("Colors", colorsPanel);
 	}
 }
