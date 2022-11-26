1c6a79399a8dc68bebc4033efc9ad85622aee4f9
Krazick <markjsmith60@gmail.com>
2022-11-26 09:37:12 -0500

    Add Action Listener to update the Round Frame when Preference changed

--- src/ge18xx/game/PlayerOrderPreference.java
+++ src/ge18xx/game/PlayerOrderPreference.java
@@ -1,5 +1,8 @@
 package ge18xx.game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,14 +10,24 @@ import javax.swing.JRadioButton;
 
 import ge18xx.player.Player;
 import ge18xx.player.PlayerManager;
+import ge18xx.round.StockRound;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.ElementName;
+import ge18xx.utilities.XMLDocument;
+import ge18xx.utilities.XMLElement;
+import ge18xx.utilities.XMLNode;
 
-public class PlayerOrderPreference {
+public class PlayerOrderPreference implements ActionListener {
+	public static final ElementName EN_PLAYER_ORDER = new ElementName ("PlayerOrder");
+	public static final AttributeName AN_ORDER_TYPE = new AttributeName ("orderType");
 
 	ButtonGroup buttonGroup;
 	JRadioButton playerOrderButtons [];
+	GameManager gameManager;
 
-	public PlayerOrderPreference () {
+	public PlayerOrderPreference (GameManager aGameManager) {
 		buttonGroup = new ButtonGroup ();
+		gameManager = aGameManager;
 	}
 
 	public void buildUserPreferences (JPanel aUserPreferencesPanel) {
@@ -25,16 +38,26 @@ public class PlayerOrderPreference {
 		tPlayerOrderLabel = new JLabel ("Player Order in Round Frame");
 		aUserPreferencesPanel.add (tPlayerOrderLabel);
 		playerOrderButtons = new JRadioButton [4];
-		playerOrderButtons [0] = new JRadioButton ("Client Player first");
+		playerOrderButtons [0] = setupRadioButton ("Client Player first", "ClientFirst");
 		playerOrderButtons [0].setSelected (true);
-		playerOrderButtons [1]  = new JRadioButton ("Priority Player at Start of Stock Round First"); 
-		playerOrderButtons [2]  = new JRadioButton ("Priority Player always First"); 
-		playerOrderButtons [3]  = new JRadioButton ("Current Player First"); 
+		playerOrderButtons [1] = setupRadioButton ("Priority Player at Start of Stock Round First", "StockPriorityFirst"); 
+		playerOrderButtons [2] = setupRadioButton ("Priority Player always First", "PriorityFirst"); 
+		playerOrderButtons [3] = setupRadioButton ("Current Player First", "CurrentFirst"); 
 		buttonGroup = new ButtonGroup ();
 		for (tPlayerOrderIndex = 0; tPlayerOrderIndex < 4; tPlayerOrderIndex++) {
 			buttonGroup.add (playerOrderButtons [tPlayerOrderIndex]);
 			aUserPreferencesPanel.add (playerOrderButtons [tPlayerOrderIndex]);
 		}
+	}
+	
+	public JRadioButton setupRadioButton (String aLabel, String aActionCommand) {
+		JRadioButton tRadioButton;
+		
+		tRadioButton = new JRadioButton (aLabel);
+		tRadioButton.setActionCommand (aActionCommand);
+		tRadioButton.addActionListener (this);
+		
+		return tRadioButton;
 	}
 	
 	public int getPlayerOrderPreference () {
@@ -51,36 +74,64 @@ public class PlayerOrderPreference {
 		return tSelectedPlayerOrder;
 	}
 	
-	public String getFirstPlayerName (GameManager aGameManager) {
+	public String getFirstPlayerName () {
 		String tFirstPlayerName;
 		int tPlayerOrderPreference;
 		int tPriorityPlayerIndex;
 		PlayerManager tPlayerManager;
+		StockRound tStockRound;
 		Player tPlayer;
 		
 		tPlayerOrderPreference = getPlayerOrderPreference ();
 		switch (tPlayerOrderPreference) {
 			case 1:
-				tFirstPlayerName = aGameManager.getClientUserName ();
+				tStockRound = gameManager.getStockRound ();
+				tPlayerManager = gameManager.getPlayerManager ();
+				tPriorityPlayerIndex = tStockRound.getStartRoundPriorityIndex ();
+				tPlayer = tPlayerManager.getPlayer (tPriorityPlayerIndex);
+				tFirstPlayerName = tPlayer.getName ();
 				break;
 				
 			case 2:
-				tPlayerManager = aGameManager.getPlayerManager ();
+				tPlayerManager = gameManager.getPlayerManager ();
 				tPriorityPlayerIndex = tPlayerManager.getPriorityPlayerIndex ();
 				tPlayer = tPlayerManager.getPlayer (tPriorityPlayerIndex);
 				tFirstPlayerName = tPlayer.getName ();
 				break;
 				
 			case 3:
-				tPlayer = aGameManager.getCurrentPlayer ();
+				tPlayer = gameManager.getCurrentPlayer ();
 				tFirstPlayerName = tPlayer.getName ();
 				break;
 				
 			default:
-				tFirstPlayerName = aGameManager.getClientUserName ();	
+				tFirstPlayerName = gameManager.getClientUserName ();	
 		}
 		
 		return tFirstPlayerName;
 	}
 
+	public XMLElement createElement (XMLDocument aXMLDocument) {
+		XMLElement tPlayerOrderElement;
+		int tPlayerOrder;
+		
+		tPlayerOrder = getPlayerOrderPreference ();
+		tPlayerOrderElement = aXMLDocument.createElement (EN_PLAYER_ORDER);
+		tPlayerOrderElement.setAttribute (AN_ORDER_TYPE, tPlayerOrder);
+		
+		return tPlayerOrderElement;
+	}
+
+	public void parsePlayerOrder (XMLNode aChildNode) {
+		int tPlayerOrderType;
+		
+		playerOrderButtons [0].setSelected (false);
+		tPlayerOrderType = aChildNode.getThisIntAttribute (AN_ORDER_TYPE);
+		playerOrderButtons [tPlayerOrderType].setSelected (true);
+	}
+
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		gameManager.updateRoundFrame ();
+	}
 }
