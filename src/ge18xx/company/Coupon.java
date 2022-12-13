d5004b2aa9b4a3e368605f89869038278f5bdac2
Krazick <markjsmith60@gmail.com>
2022-12-13 12:18:01 -0500

    Migrate methods up from the Train Class

--- src/ge18xx/company/Coupon.java
+++ src/ge18xx/company/Coupon.java
@@ -1,20 +1,23 @@
 package ge18xx.company;
 
-import javax.swing.JPanel;
+import java.awt.Color;
 
-public class Coupon implements Comparable<Object> {
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import ge18xx.bank.Bank;
+
+public class Coupon {
 	String name;
 	int price;
+	JLabel costLabel;
 	
 	public Coupon (String aName, int aPrice) {
 		setName (aName);
 		setPrice (aPrice);
-	}
-
-	@Override
-	public int compareTo (Object aObject) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	public int getPrice () {
@@ -33,14 +36,69 @@ public class Coupon implements Comparable<Object> {
 		name = aName;
 	}
 
-	public JPanel buildCertificateInfoPanel () {
-		// TODO Auto-generated method stub
-		return null;
+	public JPanel buildCouponInfoPanel () {
+		JPanel tCouponInfoPanel;
+		Border tCouponInfoBorder;
+
+		tCouponInfoPanel = new JPanel ();
+		tCouponInfoPanel.setLayout (new BoxLayout (tCouponInfoPanel, BoxLayout.Y_AXIS));
+		tCouponInfoBorder = setupCouponInfoBorder ();
+		tCouponInfoPanel.setBorder (tCouponInfoBorder);
+		tCouponInfoPanel.add (new JLabel (getFullName ()));
+		setCostLabel (tCouponInfoPanel, getPrice ());
+
+		return tCouponInfoPanel;
 	}
+	
 	public void setSelection () {
 	}
 
 	public void clearSelection () {
+	}
+
+	protected String getFullName () {
+		return getName ();
+	}
+
+	protected void setCostLabel (JPanel aCouponInfoPanel, int aPrice) {
+		String tCostLabel;
+
+		tCostLabel = "Cost " + Bank.formatCash (aPrice);
+		costLabel = new JLabel (tCostLabel);
+		aCouponInfoPanel.add (costLabel);
+	}
+	
+	protected void setCostLabel (String aLabel) {
+		if (validCostLabel ()) {
+			costLabel.setText (aLabel);
+		} else {
+			costLabel = new JLabel (aLabel);
+		}
+	}
+	
+	protected boolean validCostLabel () {
+		boolean tValidCostLabel;
+		
+		if (costLabel != null) {
+			tValidCostLabel = true;
+		} else {
+			tValidCostLabel = false;
+		}
+		
+		return tValidCostLabel;
+	}
+	protected Border setupCouponInfoBorder () {
+		Border tCouponInfoBorder;
+		Border tInnerBorder;
+		Border tOuterBorder;
+		Color tInnerColor;
+	
+		tInnerColor = new Color (237, 237, 237);
+		tInnerBorder = BorderFactory.createLineBorder (tInnerColor, 5);
+		tOuterBorder = BorderFactory.createLineBorder (Color.black, 1);
+		tCouponInfoBorder = BorderFactory.createCompoundBorder (tOuterBorder, tInnerBorder);
+	
+		return tCouponInfoBorder;
 	}
 
 }
