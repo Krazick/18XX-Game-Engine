a07cbc60baeadf56b5e01a38411ef6d32a69e5e5
Krazick <markjsmith60@gmail.com>
2022-12-17 19:39:18 -0500

    Additional Tests for all Get Token Methods

--- JUnitSource/ge18xx/company/TokenInfoTests.java
+++ JUnitSource/ge18xx/company/TokenInfoTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import ge18xx.company.TokenInfo.TokenType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -17,19 +19,20 @@ class TokenInfoTests {
 	TokenInfo tokenInfo;
 	CompanyTestFactory companyTestFactory;
 	Token mToken;
-	MapToken mMapToken;
+	MapToken mMapToken1;
+	MapToken mMapToken2;
 	
 	@BeforeEach
 	void setUp () throws Exception {
 		companyTestFactory = new CompanyTestFactory ();
 		mToken = companyTestFactory.buildTokenMock ();
-		mMapToken = companyTestFactory.buildMapTokenMock ();
-		tokenInfo = new TokenInfo (mToken, TokenInfo.TokenType.MARKET, 0);
+		mMapToken1 = companyTestFactory.buildMapTokenMock ();
+		mMapToken2 = companyTestFactory.buildMapTokenMock ();
 	}
 
 	@Nested
 	@DisplayName ("TokenInfo IS ... TOKEN Tests")
-	class TestTokenInfoCreation {
+	class TokenInfoCreationTests {
 		@Test
 		@DisplayName ("With Mocked Token and MARKET TokenType")
 		void basicConstructorMarketTokenTest () {
@@ -51,7 +54,7 @@ class TokenInfoTests {
 		void basicConstructorHome1TokenTest () {
 			TokenInfo tTokenInfo;
 	
-			tTokenInfo = new TokenInfo (mMapToken, TokenType.HOME1, 0);
+			tTokenInfo = new TokenInfo (mMapToken1, TokenType.HOME1, 0);
 	
 			assertFalse (tTokenInfo.isMarketToken ());
 			assertTrue (tTokenInfo.isHomeToken ());
@@ -67,7 +70,7 @@ class TokenInfoTests {
 		void basicConstructorHome2TokenTest () {
 			TokenInfo tTokenInfo;
 	
-			tTokenInfo = new TokenInfo (mMapToken, TokenType.HOME2, 0);
+			tTokenInfo = new TokenInfo (mMapToken1, TokenType.HOME2, 0);
 	
 			assertFalse (tTokenInfo.isMarketToken ());
 			assertTrue (tTokenInfo.isHomeToken ());
@@ -83,7 +86,7 @@ class TokenInfoTests {
 		void basicConstructorFixedCostdTokenTest () {
 			TokenInfo tTokenInfo;
 	
-			tTokenInfo = new TokenInfo (mMapToken, TokenType.FIXED_COST, 40);
+			tTokenInfo = new TokenInfo (mMapToken1, TokenType.FIXED_COST, 40);
 	
 			assertFalse (tTokenInfo.isMarketToken ());
 			assertFalse (tTokenInfo.isHomeToken ());
@@ -99,7 +102,7 @@ class TokenInfoTests {
 		void basicConstructorRangeCostTokenTest () {
 			TokenInfo tTokenInfo;
 	
-			tTokenInfo = new TokenInfo (mMapToken, TokenType.RANGE_COST, 0);
+			tTokenInfo = new TokenInfo (mMapToken1, TokenType.RANGE_COST, 0);
 	
 			assertFalse (tTokenInfo.isMarketToken ());
 			assertFalse (tTokenInfo.isHomeToken ());
@@ -108,6 +111,93 @@ class TokenInfoTests {
 			assertTrue (tTokenInfo.isMapToken ());
 			assertEquals (0, tTokenInfo.getCost ());
 			assertEquals (TokenType.RANGE_COST, tTokenInfo.getTokenType ());
+		}
+	}
+	
+	@Nested
+	@DisplayName ("TokenInfo to test GetToken Methods")
+	class TokenInfoGetTokenTests {
+		@Test
+		@DisplayName ("GetHomeToken Test")
+		void getHomeTokenTest () {
+			TokenInfo tHome1TokenInfo;
+			TokenInfo tHome2TokenInfo;
+			TokenInfo tMarketTokenInfo;
+			MapToken tMapToken1;
+			MapToken tMapToken2;
+			Token tMarketToken;
+			
+			tHome1TokenInfo = new TokenInfo (mMapToken1, TokenType.HOME1, 0);
+			tMapToken1 = tHome1TokenInfo.getHomeToken ();
+			assertEquals (mMapToken1, tMapToken1);
+			assertNotEquals (mMapToken2, tMapToken1);
+			
+			tMarketToken = tHome1TokenInfo.getMarketToken ();
+			assertNull (tMarketToken);
+
+			tHome2TokenInfo = new TokenInfo (mMapToken2, TokenType.HOME2, 0);
+			tMapToken2 = tHome2TokenInfo.getHomeToken ();
+			assertNotEquals (mMapToken1, tMapToken2);
+			assertEquals (mMapToken2, tMapToken2);
+			
+			tMarketToken = tHome2TokenInfo.getMarketToken ();
+			assertNull (tMarketToken);
+			
+			tMarketTokenInfo = new TokenInfo (mToken, TokenType.MARKET);
+			tMapToken1 = tMarketTokenInfo.getHomeToken ();
+			assertNull (tMapToken1);
+		}
+		
+		@Test
+		@DisplayName ("Get Map Token with a Fixed Cost Token Test")
+		void getFixedCostTokenTest () {
+			TokenInfo tFixedCostMapTokenInfo;
+			TokenInfo tFixedCostTokenInfo;
+			TokenInfo tMarketTokenInfo;
+			MapToken tMapToken;
+			Token tMarketToken;
+			
+			tFixedCostMapTokenInfo = new TokenInfo (mMapToken1, TokenType.FIXED_COST, 0);
+			tMapToken = tFixedCostMapTokenInfo.getMapToken ();
+			assertEquals (mMapToken1, tMapToken);
+			assertNotEquals (mMapToken2, tMapToken);
+			
+			tMarketToken = tFixedCostMapTokenInfo.getMarketToken ();
+			assertNull (tMarketToken);
+
+			tFixedCostTokenInfo = new TokenInfo (mToken, TokenType.FIXED_COST, 0);
+			tMapToken = tFixedCostTokenInfo.getMapToken ();
+			assertNull (tMapToken);
+			
+			tMarketToken = tFixedCostMapTokenInfo.getMarketToken ();
+			assertNull (tMarketToken);
+
+			tMarketTokenInfo = new TokenInfo (mToken, TokenType.MARKET);
+			tMapToken = tMarketTokenInfo.getMapToken ();
+			assertNull (tMapToken);
+		}
+
+		@Test
+		@DisplayName ("Get Token with a Market Token Test")
+		void getMarketTokenTest () {
+			TokenInfo tFixedCostMapTokenInfo;
+			TokenInfo tMarketTokenInfo;
+			Token tMarketToken;
+			
+			tFixedCostMapTokenInfo = new TokenInfo (mMapToken1, TokenType.FIXED_COST, 0);
+			tMarketToken = tFixedCostMapTokenInfo.getMarketToken ();
+			assertNull (tMarketToken);
+			
+			tMarketTokenInfo = new TokenInfo (mToken, TokenType.MARKET);
+			tMarketToken = tMarketTokenInfo.getMarketToken ();
+			assertEquals (mToken, tMarketToken);
+			assertNotEquals (mMapToken2, tMarketToken);
+			
+			assertFalse (tMarketTokenInfo.isUsed ());
+			assertFalse (tMarketTokenInfo.getUsed ());
+			tMarketTokenInfo.setUsed (true);
+			assertTrue (tMarketTokenInfo.isUsed ());
+			assertTrue (tMarketTokenInfo.getUsed ());
 		}
 	}
 }
