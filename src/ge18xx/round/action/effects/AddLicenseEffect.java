a9e00eadee2ef63d42abac9450eb7abbf80a7ee4
Krazick <markjsmith60@gmail.com>
2023-01-20 13:25:02 -0500

    New Class for handling AddLicenseEffect

---
+++ src/ge18xx/round/action/effects/AddLicenseEffect.java
@@ -0,0 +1,100 @@
+package ge18xx.round.action.effects;
+
+import ge18xx.bank.Bank;
+import ge18xx.company.License;
+import ge18xx.company.PortLicense;
+import ge18xx.game.GameManager;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.XMLDocument;
+import ge18xx.utilities.XMLElement;
+import ge18xx.utilities.XMLNode;
+
+public class AddLicenseEffect extends CashTransferEffect {
+	public final static String NAME = "Add License";
+	final static AttributeName AN_CASH = new AttributeName ("License");
+	License license;
+	
+	public AddLicenseEffect () {
+		this (NAME);
+	}
+
+	public AddLicenseEffect (String aName) {
+		super (aName);
+		setCash (NO_CASH);
+		setToActor (ActorI.NO_ACTOR);
+	}
+
+	public AddLicenseEffect (ActorI aFromActor, ActorI aToActor, int aCashAmount, License aLicense) {
+		super (aFromActor, aToActor, aCashAmount);
+		setLicense (aLicense);
+		setName (NAME);
+	}
+
+	public AddLicenseEffect (XMLNode aEffectNode, GameManager aGameManager) {
+		super (aEffectNode, aGameManager);
+		
+		setName (NAME);
+		int tBenefitValue;
+		int tLicensePrice;
+		String tLicenseName;
+		boolean tIsPortLicense;
+		PortLicense tPortLicense;
+		License tOtherLicense;
+		
+		tBenefitValue = aEffectNode.getThisIntAttribute (License.AN_BENEFIT_VALUE);
+		tLicenseName = aEffectNode.getThisAttribute (License.AN_LICENSE_NAME);
+		tIsPortLicense = aEffectNode.getThisBooleanAttribute (PortLicense.AN_PORT_LICENSE);
+		tLicensePrice = aEffectNode.getThisIntAttribute (License.AN_LICENSE_PRICE);
+		if (tIsPortLicense) {
+			tPortLicense = new PortLicense (tLicenseName, tBenefitValue);
+			setLicense (tPortLicense);
+		} else {
+			tOtherLicense = new License (tLicenseName, tLicensePrice, tBenefitValue);
+			setLicense (tOtherLicense);
+		}
+	}
+
+	public void setLicense (License aLicense) {
+		license = aLicense;
+	}
+	
+	public License getLicense () {
+		return license;
+	}
+	
+	@Override
+	public XMLElement getEffectElement (XMLDocument aXMLDocument, AttributeName aActorAN) {
+		XMLElement tEffectElement;
+
+		tEffectElement = super.getEffectElement (aXMLDocument, ActorI.AN_FROM_ACTOR_NAME);
+		tEffectElement.setAttribute (License.AN_BENEFIT_VALUE, license.getBenefitValue ());
+		tEffectElement.setAttribute (License.AN_LICENSE_NAME, license.getName ());
+		tEffectElement.setAttribute (PortLicense.AN_PORT_LICENSE, license.isPortLicense ());
+		tEffectElement.setAttribute (License.AN_LICENSE_PRICE, license.getPrice ());
+
+		return tEffectElement;
+	}
+
+	@Override
+	public String getEffectReport (RoundManager aRoundManager) {
+		String tReport;
+		int tPrice;
+		
+		tReport = REPORT_PREFIX + name + " " + license.getLicenseLabel () + " to " + getToActorName ();
+		tPrice = license.getPrice ();
+		if (tPrice > 0) {
+			tReport += " for " +  Bank.formatCash (license.getPrice ());
+		}
+		tReport += ".";
+		
+		return (tReport);
+	}
+
+	@Override
+	public void printEffectReport (RoundManager aRoundManager) {
+		System.out.println (getEffectReport (aRoundManager));
+	}
+
+}
