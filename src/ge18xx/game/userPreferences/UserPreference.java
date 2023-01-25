eb8d00aad4dd7854e40b4295577ba84a288bdb0a
Krazick <markjsmith60@gmail.com>
2023-01-24 19:22:06 -0500

    New method parseBooleanPreference and appendNewElement

--- src/ge18xx/game/userPreferences/UserPreference.java
+++ src/ge18xx/game/userPreferences/UserPreference.java
@@ -3,14 +3,17 @@ package ge18xx.game.userPreferences;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 import ge18xx.game.GameManager;
+import ge18xx.utilities.AttributeName;
 import ge18xx.utilities.XMLDocument;
 import ge18xx.utilities.XMLElement;
 import ge18xx.utilities.XMLNode;
 
 public abstract class UserPreference implements ActionListener {
+	public static UserPreference NO_USER_PREFERENCE;
 	GameManager gameManager;
 
 	public UserPreference (GameManager aGameManager) {
@@ -26,7 +29,23 @@ public abstract class UserPreference implements ActionListener {
 	public abstract XMLElement createElement (XMLDocument aXMLDocument);
 	
 	public abstract void parsePreference (XMLNode aChildNode);
+	
+	public boolean parseBooleanPreference (XMLNode aChildNode, AttributeName aAttributeName, JCheckBox aCheckBox) {
+		boolean tBooleanAttribute;
+		
+		tBooleanAttribute = aChildNode.getThisBooleanAttribute (aAttributeName);
+		aCheckBox.setSelected (tBooleanAttribute);
+		
+		return tBooleanAttribute;
+	}
 
+	public void appendNewElement (XMLElement aXMLElement, XMLDocument aXMLDocument) {
+		XMLElement tUserPreferenceElement;
+		
+		tUserPreferenceElement = createElement (aXMLDocument);
+		aXMLElement.appendChild (tUserPreferenceElement);
+	}
+	
 	@Override
 	public void actionPerformed (ActionEvent aEvent) {
 		gameManager.updateAllFrames ();
