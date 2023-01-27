aaa953bf68db5ba8bc473e14c14aa7336441f62e
Krazick <markjsmith60@gmail.com>
2023-01-27 12:21:09 -0500

    Initial FreeLicenseBenefit version

---
+++ src/ge18xx/company/benefit/FreeLicenseBenefit.java
@@ -0,0 +1,82 @@
+package ge18xx.company.benefit;
+
+import ge18xx.company.Corporation;
+import ge18xx.company.License;
+import ge18xx.utilities.XMLNode;
+
+public class FreeLicenseBenefit extends Benefit {
+	public final static String NAME = "Free License";
+	int corporationID;
+	int licenseCost;
+	String mapCellIDs;
+	int value;
+
+	public FreeLicenseBenefit (XMLNode aXMLNode) {
+		super (aXMLNode);
+		
+		String tMapCellIDs;
+		int tCorporationID;
+		int tLicenseValue;
+		int tLicenseCost;
+
+		tCorporationID = aXMLNode.getThisIntAttribute (Corporation.AN_ID);
+		tLicenseCost = aXMLNode.getThisIntAttribute (LicenseBenefit.AN_LICENSE_COST);
+		tMapCellIDs = aXMLNode.getThisAttribute (LicenseBenefit.AN_MAP_CELL);
+		tLicenseValue = aXMLNode.getThisIntAttribute (LicenseBenefit.AN_LICENSE_VALUE);
+		setCorporationID (tCorporationID);
+		setLicenseCost (tLicenseCost);
+		setMapCellIDs (tMapCellIDs);
+		setLicenseValue (tLicenseValue);
+
+		setName (NAME);
+	}
+
+	public void setCorporationID (int aCorporationID) {
+		corporationID = aCorporationID;
+	}
+
+	public void setLicenseCost (int aLicenseCost) {
+		licenseCost = aLicenseCost;
+	}
+
+	public void setMapCellIDs (String aMapCellIDs) {
+		mapCellIDs = aMapCellIDs;
+	}
+
+	public void setLicenseValue (int aLicenseValue) {
+		value = aLicenseValue;
+	}
+
+	public int getCorporationID () {
+		return corporationID;
+	}
+	
+	public int getLicenseCost () {
+		return licenseCost;
+	}
+
+	public int getLicenseValue () {
+		return value;
+	}
+
+	@Override
+	public int getCost () {
+		return 0;
+	}
+
+	@Override
+	public String getNewButtonLabel () {
+		return null;
+	}
+
+	public License getLicense () {
+		License tLicense;
+		String tLicenseName;
+		
+		tLicenseName = privateCompany.getAbbrev () + " License";
+		tLicense = new License (tLicenseName, value);
+		tLicense.setMapCellIDs (mapCellIDs);
+		
+		return tLicense;
+	}
+}
