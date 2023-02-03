4033c0bea415784225d681195a0d6c017a25880c
Krazick <markjsmith60@gmail.com>
2023-02-03 16:31:12 -0500

    Rename method to 'setDestinationCapitalizationLevel'

--- src/ge18xx/round/OperatingRound.java
+++ src/ge18xx/round/OperatingRound.java
@@ -221,7 +221,7 @@ public class OperatingRound extends Round {
 			tShareCompany = (ShareCompany) shareCompanies.getCorporation (tNextShareIndexToOperate);
 			if (!tShareCompany.hasFloated ()) {
 				if (tShareCompany.shouldFloat ()) {
-					tShareCompany.setCapitalizationLevel ();
+					tShareCompany.setDestinationCapitalizationLevel ();
 					tStartingTreasury = tShareCompany.calculateStartingTreasury ();
 					tShareCompany.floatCompany (tStartingTreasury);
 				} else {
