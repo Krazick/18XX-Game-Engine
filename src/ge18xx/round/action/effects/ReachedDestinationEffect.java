5c7232a24c43b73e8cc77d99be11b2f64df58062
Krazick <markjsmith60@gmail.com>
2023-02-03 20:48:31 -0500

    Add old & new Capitalization to Effect

--- src/ge18xx/round/action/effects/ReachedDestinationEffect.java
+++ src/ge18xx/round/action/effects/ReachedDestinationEffect.java
@@ -12,7 +12,12 @@ import ge18xx.utilities.XMLNode;
 public class ReachedDestinationEffect extends Effect {
 	public final static String NAME = "Reached Destination";
 	final static AttributeName AN_REACHED_DESTINATION = new AttributeName ("reached");
+	final static AttributeName AN_OLD_CAPITALIZATION= new AttributeName ("oldCapitalization");
+	final static AttributeName AN_NEW_CAPITALIZATION = new AttributeName ("newCapitalization");
 	boolean reached;
+	int oldCapitalizationLevel;
+	int newCapitalizationLevel;
+	
 
 	public ReachedDestinationEffect () {
 		super ();
@@ -27,9 +32,12 @@ public class ReachedDestinationEffect extends Effect {
 		super (aName, aActor);
 	}
 
-	public ReachedDestinationEffect (ActorI aActor, boolean aReached) {
+	public ReachedDestinationEffect (ActorI aActor, boolean aReached, int aOldCapitalizationLevel, 
+			int aNewCapitalizationLevel) {
 		super (NAME, aActor);
 		setReached (aReached);
+		setOldCapitalization (aOldCapitalizationLevel);
+		setNewCapitalization (aNewCapitalizationLevel);
 	}
 
 	public ReachedDestinationEffect (XMLNode aEffectNode, GameManager aGameManager) {
@@ -37,13 +45,27 @@ public class ReachedDestinationEffect extends Effect {
 		setName (NAME);
 
 		boolean tReached;
-
+		int tOldCapitalization;
+		int tNewCapitalization;
+		
 		tReached = aEffectNode.getThisBooleanAttribute (AN_REACHED_DESTINATION);
+		tOldCapitalization = aEffectNode.getThisIntAttribute (AN_OLD_CAPITALIZATION);
+		tNewCapitalization = aEffectNode.getThisIntAttribute (AN_NEW_CAPITALIZATION);
 		setReached (tReached);
+		setOldCapitalization (tOldCapitalization);
+		setNewCapitalization (tNewCapitalization);
 	}
 	
 	public void setReached (boolean aReached) {
 		reached = aReached;
+	}
+	
+	public void setOldCapitalization (int aOldCapitalization) {
+		oldCapitalizationLevel = aOldCapitalization;
+	}
+	
+	public void setNewCapitalization (int aNewCapitalization) {
+		newCapitalizationLevel = aNewCapitalization;
 	}
 
 	@Override
@@ -52,6 +74,8 @@ public class ReachedDestinationEffect extends Effect {
 
 		tEffectElement = super.getEffectElement (aXMLDocument, aActorAN);
 		tEffectElement.setAttribute (AN_REACHED_DESTINATION, reached);
+		tEffectElement.setAttribute (AN_OLD_CAPITALIZATION, oldCapitalizationLevel);
+		tEffectElement.setAttribute (AN_NEW_CAPITALIZATION, newCapitalizationLevel);
 
 		return tEffectElement;
 	}
@@ -59,7 +83,8 @@ public class ReachedDestinationEffect extends Effect {
 	@Override
 	public String getEffectReport (RoundManager aRoundManager) {
 		return (REPORT_PREFIX + name + " for " + actor.getAbbrev () + " "
-				+ " has reached it's Destination.");
+				+ " has reached it's Destination. Old Capitalization Level "+ oldCapitalizationLevel +
+				" New Capitalization Level " + newCapitalizationLevel);
 	}
 
 	@Override
@@ -75,6 +100,7 @@ public class ReachedDestinationEffect extends Effect {
 		if (actor.isAShareCompany ()) {
 			tShareCompany = (ShareCompany) actor;
 			tShareCompany.setReachedDestination (reached);
+			tShareCompany.setDestinationCapitalizationLevel (newCapitalizationLevel);
 			tEffectApplied = true;
 		} else {
 			tEffectApplied = false;
@@ -92,6 +118,7 @@ public class ReachedDestinationEffect extends Effect {
 		if (actor.isAShareCompany ()) {
 			tShareCompany = (ShareCompany) actor;
 			tShareCompany.setReachedDestination (reached);
+			tShareCompany.setDestinationCapitalizationLevel (oldCapitalizationLevel);
 			tEffectUndone = true;
 		} else {
 			tEffectUndone = false;
