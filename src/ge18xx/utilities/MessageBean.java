e561b79640fc5963569d284b48919dea8e1729de
Krazick <markjsmith60@gmail.com>
2023-02-23 17:27:17 -0500

    Update to remove Index (not needed)

--- src/ge18xx/utilities/MessageBean.java
+++ src/ge18xx/utilities/MessageBean.java
@@ -6,16 +6,7 @@ import java.beans.PropertyChangeSupport;
 public class MessageBean {	
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
     private String message; // the bean  property
-    private int index;
-    
-    public MessageBean () {
-    		setIndex (0);
-    }
-    
-    public void setIndex (int aIndex) {
-    		index = aIndex;
-    }
-    
+        
     public void addPropertyChangeListener (PropertyChangeListener aListener) {
         support.addPropertyChangeListener (aListener);
     }
@@ -31,9 +22,8 @@ public class MessageBean {
     public void setMessage (String aNewValue) {
         String tOldValue;
         
-        setIndex (index + 1);
         tOldValue = message;
-        message =  "(" + index + ") " + aNewValue;
+        message = aNewValue;
         // The parameter values of firePropertyChange method
         // constitute the PropertyChangeEvent object
         support.firePropertyChange ("message", tOldValue, aNewValue);
