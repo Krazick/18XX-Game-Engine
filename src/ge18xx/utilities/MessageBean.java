6546e4bbcf0c46fabecfa67e729ac32011718471
Krazick <markjsmith60@gmail.com>
2023-02-25 10:26:36 -0500

    Add method setActive, and test before firingPropertyChange method

--- src/ge18xx/utilities/MessageBean.java
+++ src/ge18xx/utilities/MessageBean.java
@@ -3,29 +3,41 @@ package ge18xx.utilities;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-public class MessageBean {	
-    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
-    private String message; // the bean  property
-        
-    public void addPropertyChangeListener (PropertyChangeListener aListener) {
-        support.addPropertyChangeListener (aListener);
-    }
-
-    public void removePropertyChangeListener (PropertyChangeListener aListener) {
-        support.removePropertyChangeListener (aListener);
-    }
-
-    public String getMessage () {
-        return message;
-    }
-
-    public void setMessage (String aNewValue) {
-        String tOldValue;
-        
-        tOldValue = message;
-        message = aNewValue;
-        // The parameter values of firePropertyChange method
-        // constitute the PropertyChangeEvent object
-        support.firePropertyChange ("message", tOldValue, aNewValue);
-    }
+public class MessageBean {
+	public final static MessageBean NO_BEAN = null;
+	private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+	private String message; // the bean  property
+	private boolean active;
+	
+	public MessageBean () {
+		setActive (false);	
+	}
+	
+	public void setActive (boolean aActive) {
+		active = aActive;
+	}
+	
+	public void addPropertyChangeListener (PropertyChangeListener aListener) {
+	    support.addPropertyChangeListener (aListener);
+	}
+	
+	public void removePropertyChangeListener (PropertyChangeListener aListener) {
+	    support.removePropertyChangeListener (aListener);
+	}
+	
+	public String getMessage () {
+	    return message;
+	}
+	
+	public void setMessage (String aNewValue) {
+	    String tOldValue;
+	    
+	    if (active) {
+		    tOldValue = message;
+		    message = aNewValue;
+		    // The parameter values of firePropertyChange method
+		    // constitute the PropertyChangeEvent object
+		    support.firePropertyChange ("message", tOldValue, aNewValue);
+	    }
+	}
 }
