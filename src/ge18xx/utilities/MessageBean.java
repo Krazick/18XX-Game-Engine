889ea9155f30f7cc467cc056453914dfb016ffa0
Krazick <markjsmith60@gmail.com>
2023-03-03 16:52:34 -0500

    Expanded Message Bean to send multiple Messages in each Bean

--- src/ge18xx/utilities/MessageBean.java
+++ src/ge18xx/utilities/MessageBean.java
@@ -2,19 +2,45 @@ package ge18xx.utilities;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MessageBean {
 	public final static MessageBean NO_BEAN = null;
 	private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+	private String oldMessage;
 	private String message; // the bean  property
+	private List<String> messages;
 	private boolean active;
+	int index;
+	
+	// Add a list of Messages, and an Index.
+	// Add a method 'flushMessages' to clear out the list of Messages
+	// Add a method 'sendMessages' to fire the Property Changed to the 'support' object
+	// Add a method 'processMessage' to indicate the message has been handled.  
+	// 		This would include removing the message
+	// Rename 'setMessage' to addMessage
+	//		Allow 'addMessage' to send messages, if Active
+	
+	// When the Action Manager is ready to apply a Action:
+	//  1. Deactivate all the Message Beans
+	//  2. Allow the Message Bean to add new Messages
+	//  3. When all of the Effects have been applied, Reactive the Message Bean
+	//  4. Call the 'sendMessages' method to send all backlogged messages 
 	
 	public MessageBean () {
-		setActive (false);	
+		setActive (false);
+		messages = new LinkedList<String> ();
+		index = 0;
+		setOldMessage (GUI.EMPTY_STRING);
 	}
 	
 	public void setActive (boolean aActive) {
 		active = aActive;
+	}
+	
+	public void flushMessages () {
+		messages.clear ();
 	}
 	
 	public void addPropertyChangeListener (PropertyChangeListener aListener) {
@@ -29,15 +55,39 @@ public class MessageBean {
 	    return message;
 	}
 	
-	public void setMessage (String aNewValue) {
-	    String tOldValue;
-	    
+	public void setOldMessage (String aOldMessage) {
+		oldMessage = aOldMessage;
+	}
+	
+	public int getMessageCount () {
+		return messages.size ();
+	}
+	
+	public String getMessageAt (int aIndex) {
+		return messages.get (aIndex);
+	}
+	
+	public void addMessage (String aNewValue) {
+	    messages.add (aNewValue);
+	    index++;
 	    if (active) {
-		    tOldValue = message;
-		    message = aNewValue;
-		    // The parameter values of firePropertyChange method
-		    // constitute the PropertyChangeEvent object
-		    support.firePropertyChange ("message", tOldValue, aNewValue);
+	    		sendMessages ();
 	    }
+	}
+	
+	public void sendMessages  () {
+		String tOldMessage;
+		
+		System.out.println ("Send Bean Messages Count " + messages.size ());
+		if (active) {
+			tOldMessage = oldMessage;
+			for (String tNewMessage : messages) {
+				System.out.println ("Message [" + tNewMessage);
+			    // The parameter values of firePropertyChange method
+			    // constitute the PropertyChangeEvent object
+			    support.firePropertyChange ("message " + index, tOldMessage, tNewMessage);
+			}
+			flushMessages ();
+		}
 	}
 }
