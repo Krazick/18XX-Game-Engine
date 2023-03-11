7bb7618f000e5a74f434d8921fa1739fd9e821a7
Krazick <markjsmith60@gmail.com>
2023-03-11 14:52:03 -0500

    Move addAdditonalEffects method to Benefit, add 'AddLicense' method

--- src/ge18xx/company/benefit/PassiveEffectBenefit.java
+++ src/ge18xx/company/benefit/PassiveEffectBenefit.java
@@ -5,12 +5,9 @@ import ge18xx.company.License;
 import ge18xx.company.ShareCompany;
 import ge18xx.round.action.Action;
 import ge18xx.round.action.effects.AddLicenseEffect;
-import ge18xx.round.action.effects.Effect;
 import ge18xx.utilities.XMLNode;
 
 public class PassiveEffectBenefit extends Benefit {
-
-	Effect effect;
 
 	public PassiveEffectBenefit (XMLNode aXMLNode) {
 		super (aXMLNode);
@@ -35,28 +32,11 @@ public class PassiveEffectBenefit extends Benefit {
 
 	public void addLicense (ShareCompany aOwningCompany, License aLicense) {
 		Bank tBank;
+		AddLicenseEffect tAddLicenseEffect;
 		
 		aOwningCompany.addLicense (aLicense);
 		tBank = aOwningCompany.getBank ();
-		effect = new AddLicenseEffect (tBank, aOwningCompany, 0, aLicense);
+		tAddLicenseEffect = new AddLicenseEffect (tBank, aOwningCompany, 0, aLicense);
+		addAdditionalEffect (tAddLicenseEffect);
 	}
-
-	/**
-	 *  Add Any additional Effects to the provided Action generated in the process of applying this Benefit.
-	 *  
-	 * @param aAction The Action to which the Effect needs to be added.
-	 * 
-	 */
-	public void addAdditionalEffects (Action aAction) {
-		aAction.addEffect (effect);
-	}
-
-	public String buildLicenseName () {
-		String tLicenseName;
-		
-		tLicenseName = privateCompany.getAbbrev () + " License";
-	
-		return tLicenseName;
-	}
-
 }
