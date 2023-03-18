378703f134d1761355d7759e272c2c3e13c51657
Krazick <markjsmith60@gmail.com>
2023-03-18 13:24:38 -0400

    Initial Version of HexMapTests

---
+++ JUnitSource/ge18xx/map/HexMapTests.java
@@ -0,0 +1,27 @@
+package ge18xx.map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName ("Hex Map Tests")
+class HexMapTests {
+	MapTestFactory mapTestFactory;
+	HexMap hexMap;
+	
+	@BeforeEach
+	void setUp () throws Exception {
+		mapTestFactory = new MapTestFactory ();
+		hexMap = mapTestFactory.buildHexMap ();
+	}
+
+	@Test
+	@DisplayName ("Test initial flags")
+	void HexMapInitialFlagsTests () {
+		assertTrue (hexMap.isSMCEmpty ());
+		assertFalse (hexMap.wasTilePlaced ());
+	}
+
+}
