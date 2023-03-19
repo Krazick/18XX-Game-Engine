234bfa05e2e054129130346616e3815567104c49
Krazick <markjsmith60@gmail.com>
2023-03-19 12:13:54 -0400

    Correct Imports to be specific

--- JUnitSource/ge18xx/map/HexMapTests.java
+++ JUnitSource/ge18xx/map/HexMapTests.java
@@ -1,6 +1,7 @@
 package ge18xx.map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
