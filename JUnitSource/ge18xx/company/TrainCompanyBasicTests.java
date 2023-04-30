ad18df00ddba648b0e0eca749d6774c05e5f8b03
Krazick <markjsmith60@gmail.com>
2023-04-30 19:08:50 -0400

    Initial Versions of tests, primarily for Port License

---
+++ JUnitSource/ge18xx/company/TrainCompanyBasicTests.java
@@ -0,0 +1,81 @@
+package ge18xx.company;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TrainCompanyBasicTests {
+	CompanyTestFactory companyTestFactory;
+	TrainCompany trainCompany;
+
+	@BeforeEach
+	void setUp () throws Exception {
+		companyTestFactory = new CompanyTestFactory ();
+		trainCompany = companyTestFactory.buildATrainCompany (1);
+	}
+
+	@AfterEach
+	void tearDown () throws Exception {
+	}
+
+	
+	@Test
+	@DisplayName ("Test getting a Port License in this Company")
+	void getPortLicenseTest () {
+		License tPortLicense;
+		License tNewLicense;
+		License tNewPortLicense;
+		int tPrice;
+		int tBenefitValue;
+		
+		tPortLicense = trainCompany.getPortLicense ();
+		assertNull (tPortLicense);
+
+		tPrice = 50;
+		tBenefitValue = 10;
+		tNewLicense = new License (License.LicenseTypes.BRIDGE, tPrice, tBenefitValue);
+		trainCompany.addLicense (tNewLicense);
+		
+		tPortLicense = trainCompany.getPortLicense ();
+		assertNull (tPortLicense);
+		
+		tNewPortLicense = new License (License.LicenseTypes.PORT, tPrice, tBenefitValue);
+		trainCompany.addLicense (tNewPortLicense);
+		
+		tPortLicense = trainCompany.getPortLicense ();
+		assertEquals (tNewPortLicense, tPortLicense);
+	}
+	
+	@Test 
+	@DisplayName ("Test Removing Specific Licenses")
+	void removePortLicenseTest () {
+		License tPortLicense;
+		License tNewLicense;
+		License tNewPortLicense;
+		int tPrice;
+		int tBenefitValue;
+
+		tPrice = 50;
+		tBenefitValue = 10;
+		tNewLicense = new License (License.LicenseTypes.BRIDGE, tPrice, tBenefitValue);
+		trainCompany.addLicense (tNewLicense);
+		tNewPortLicense = new License (License.LicenseTypes.PORT, tPrice, tBenefitValue);
+		trainCompany.addLicense (tNewPortLicense);
+
+		tPortLicense = trainCompany.getPortLicense ();
+		assertEquals (tNewPortLicense, tPortLicense);
+		
+		assertTrue (trainCompany.removeLicense (tNewPortLicense));
+		tPortLicense = trainCompany.getPortLicense ();
+		assertNull (tPortLicense);
+		
+		assertFalse (trainCompany.removeLicense (tNewPortLicense));
+	}
+
+}
