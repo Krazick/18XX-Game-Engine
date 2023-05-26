50eb268b0bb0e16d9a26e9749f2978a230128604
Krazick <markjsmith60@gmail.com>
2023-05-26 09:55:50 -0400

    Separate MapCell Junit from MapCell Integration Tests

--- JUnitSource/ge18xx/map/MapCellTests.java
+++ JUnitSource/ge18xx/map/MapCellTests.java
@@ -3,48 +3,29 @@ package ge18xx.map;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import ge18xx.center.RevenueCenter;
-import ge18xx.company.CompanyTestFactory;
-import ge18xx.company.Corporation;
-import ge18xx.company.ShareCompany;
-import ge18xx.game.GameManager;
 import ge18xx.game.GameTestFactory;
-import ge18xx.tiles.GameTile;
 import ge18xx.tiles.Tile;
-import ge18xx.tiles.TileSet;
 import ge18xx.tiles.TilesTestFactory;
-import ge18xx.tiles.Upgrade;
 
 class MapCellTests {
 	MapCell mapCell;
 	MapTestFactory mapTestFactory;
 	TilesTestFactory tilesTestFactory;
-	CompanyTestFactory companyTestFactory;
 	GameTestFactory gameTestFactory;
-	ShareCompany alphaShareCompany;
-	ShareCompany betaShareCompany;
 	Tile mTile;
-	GameManager mGameManager;
-	TileSet tileSet;
 	
 	@BeforeEach
 	void setUp () throws Exception {
 		gameTestFactory = new GameTestFactory ();
-		mGameManager = gameTestFactory.buildGameManagerMock ();
 		mapTestFactory = new MapTestFactory ();
-		mapCell = mapTestFactory.buildMapCell ();
+		mapCell = mapTestFactory.buildMapCell ("N11");
 		tilesTestFactory = new TilesTestFactory (mapTestFactory);
 		mTile = tilesTestFactory.buildTileMock (7);
-		companyTestFactory = new CompanyTestFactory ();
-		alphaShareCompany = companyTestFactory.buildAShareCompany (1);
-		betaShareCompany = companyTestFactory.buildAShareCompany (2);
 	}
 
 	@Test
@@ -106,93 +87,5 @@ class MapCellTests {
 		
 		mapCell.setAllRotations (true);
 		tilesTestFactory.setMockCanAllTracksExit (mTile, mapCell, 5, false);
-	}
-		
-	@Test
-	@DisplayName ("Test Toronto Tile Upgrade on MapCell")
-	void toronotTileUpgradeTest () {
-		Tile tTile9995;
-		Tile tTile120;
-		Location tCorpLocation;
-		Location tOtherLocation;
-		RevenueCenter tCenterFound;
-		RevenueCenter tOtherCenterFound;
-		int tDestinationCorpID;
-		int tAlpahCorpID;
-		int tBetaCorpID;
-		int tExpectedCount;
-		int tExpectedBaseCorpIDs [];
-		
-		setupTileSet (mGameManager);
-		tTile9995 = addTileAndUpgrade (0);
-		tTile120 = addTileAndUpgrade (2);
-		
-		assertEquals (tTile9995.getNumber (), 9995);
-		assertEquals (tTile120.getNumber (), 120);
-		assertFalse (mapCell.isTileOnCell ());
-		
-		mapCell.putTile (tTile9995, 0);
-		tCorpLocation = new Location (7);
-		tOtherLocation = new Location (10);
-		mapCell.setCorporationHome (alphaShareCompany, tCorpLocation);
-		tAlpahCorpID = alphaShareCompany.getID ();
-		assertTrue (mapCell.isTileOnCell ());
-		tCenterFound = mapCell.getCenterAtLocation (tCorpLocation);
-		assertNotNull (tCenterFound);
-		tOtherCenterFound = mapCell.getCenterAtLocation (tOtherLocation);
-		assertNotNull (tOtherCenterFound);
-		
-		tBetaCorpID = betaShareCompany.getID ();	
-		tDestinationCorpID = mapCell.getDestinationCorpID ();
-		assertEquals (Corporation.NO_ID, tDestinationCorpID);
-		
-		mapCell.setDestinationCorpID (tBetaCorpID);
-		tDestinationCorpID = mapCell.getDestinationCorpID ();
-		assertEquals (tBetaCorpID, tDestinationCorpID);
-		
-		tExpectedCount = 3;
-		tExpectedBaseCorpIDs = new int [tExpectedCount];
-		tExpectedBaseCorpIDs [0] = Corporation.NO_ID;
-		tExpectedBaseCorpIDs [1] = tAlpahCorpID;
-		tExpectedBaseCorpIDs [2] = Corporation.NO_ID;
-		verifyCorporationBasesOnMapCell (tExpectedCount, tExpectedBaseCorpIDs);
-		
-		mapCell.upgradeTile (tileSet, tTile120);
-	}
-
-	void setupTileSet (GameManager mGameManager) {
-		Mockito.when (mGameManager.getActiveGameName ()).thenReturn ("Mock GameManager MapCellTests");
-		tileSet = tilesTestFactory.buildTileSet (mGameManager);
-	}
-	
-	Tile addTileAndUpgrade (int aTileIndex) {
-		Tile tTile;
-		GameTile tGameTile;
-		Upgrade tUpgrade;
-		int tTileNumber;
-
-		tTile = tilesTestFactory.buildTile (aTileIndex);
-		tileSet.addTile (tTile, 1);
-		tUpgrade = tilesTestFactory.buildUpgrade (aTileIndex);
-		tTileNumber = tTile.getNumber ();
-		tGameTile = tileSet.getGameTile (tTileNumber);
-		tGameTile.addUpgrade (tUpgrade);
-		
-		return tTile;
-	}
-	
-	void verifyCorporationBasesOnMapCell (int aExpectedCount, int aCorpIDBases []) {
-		RevenueCenter tCenterFound;
-		int tCenterCount;
-		int tCenterIndex;
-		int tCorpID;
-		
-		tCenterCount = mapCell.getRevenueCenterCount ();
-		assertEquals (aExpectedCount, tCenterCount);
-		for (tCenterIndex = 0; tCenterIndex < tCenterCount; tCenterIndex++) {
-			tCenterFound = mapCell.getRevenueCenter (tCenterIndex);
-			tCorpID = tCenterFound.getHomeCompanyID ();
-			assertEquals (aCorpIDBases [tCenterIndex], tCorpID);
-		}
 	}
 }
