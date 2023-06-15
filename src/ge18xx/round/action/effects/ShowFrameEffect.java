0f960fc02d43afcde1b7d772335e06254f52046d
Krazick <markjsmith60@gmail.com>
2023-06-15 15:15:14 -0400

    Adjust to use XMLFrame, not JFrame, and properly undo the Effect

--- src/ge18xx/round/action/effects/ShowFrameEffect.java
+++ src/ge18xx/round/action/effects/ShowFrameEffect.java
@@ -1,10 +1,9 @@
 package ge18xx.round.action.effects;
 
-import javax.swing.JFrame;
-
 import ge18xx.game.GameManager;
 import ge18xx.round.RoundManager;
 import ge18xx.round.action.ActorI;
+import ge18xx.toplevel.XMLFrame;
 import ge18xx.utilities.AttributeName;
 import ge18xx.utilities.XMLDocument;
 import ge18xx.utilities.XMLElement;
@@ -12,49 +11,63 @@ import ge18xx.utilities.XMLNode;
 
 public class ShowFrameEffect extends Effect {
 	public final static String NAME = "Show Frame";
-	final static AttributeName AN_JFRAME_TITLE = new AttributeName ("jFrameTitle");
-	public final static JFrame NO_JFRAME = null;
-	JFrame jFrame;
+	final static AttributeName AN_XMLFRAME_TITLE = new AttributeName ("xmlFrameTitle");
+	XMLFrame xmlFrame;
+	String xmlFrameTitle;
 
 	public ShowFrameEffect () {
 		this (NAME, ActorI.NO_ACTOR);
 	}
 
 	public ShowFrameEffect (String aName, ActorI aActor) {
-		this (aName, aActor, NO_JFRAME);
+		this (aName, aActor, XMLFrame.NO_XML_FRAME);
 	}
 
-	public ShowFrameEffect (ActorI aActor, JFrame aJFrame) {
-		this (NAME, aActor, aJFrame);
+	public ShowFrameEffect (ActorI aActor, XMLFrame aXMLFrame) {
+		this (NAME, aActor, aXMLFrame);
 	}
 
-	public ShowFrameEffect (String aName, ActorI aActor, JFrame aJFrame) {
+	public ShowFrameEffect (String aName, ActorI aActor, XMLFrame aXMLFrame) {
 		super (aName, aActor);
-		setJFrame (aJFrame);
+		setXMLFrame (aXMLFrame);
+		setXMLFrameTitle (aXMLFrame.getTitle ());
 	}
 
 	public ShowFrameEffect (XMLNode aEffectNode, GameManager aGameManager) {
 		super (aEffectNode, aGameManager);
 		setName (NAME);
+		
+		String tXMLFrameTitle;
+		XMLFrame tXMLFrame;
 
-		String tJFrameTitle = aEffectNode.getThisAttribute (AN_JFRAME_TITLE);
-		JFrame tJFrame = aGameManager.getJFrameName (tJFrameTitle);
-		setJFrame (tJFrame);
+		tXMLFrameTitle = aEffectNode.getThisAttribute (AN_XMLFRAME_TITLE);
+		tXMLFrame = aGameManager.getXMLFrameName (tXMLFrameTitle);
+		setXMLFrame (tXMLFrame);
 	}
 
-	private void setJFrame (JFrame aJFrame) {
-		jFrame = aJFrame;
+	private void setXMLFrame (XMLFrame aXMLFrame) {
+		xmlFrame = aXMLFrame;
 	}
 
-	public JFrame getJFrame () {
-		return jFrame;
+	public void setXMLFrameTitle (String aXMLFrameTitle) {
+		xmlFrameTitle = aXMLFrameTitle;
 	}
-
+	
+	public XMLFrame getXMLFrame () {
+		return xmlFrame;
+	}
+	
+	public String getXMLFrameTitle () {
+		return xmlFrameTitle;
+	}
+	
 	@Override
 	public boolean applyEffect (RoundManager aRoundManager) {
 		boolean tEffectApplied = false;
 
-		jFrame.setVisible (true);
+		if (xmlFrame != XMLFrame.NO_XML_FRAME) {
+			xmlFrame.setVisible (true);
+		}
 		tEffectApplied = true;
 
 		return tEffectApplied;
@@ -65,14 +78,14 @@ public class ShowFrameEffect extends Effect {
 		XMLElement tEffectElement;
 
 		tEffectElement = super.getEffectElement (aXMLDocument, ActorI.AN_FROM_ACTOR_NAME);
-		tEffectElement.setAttribute (AN_JFRAME_TITLE, jFrame.getTitle ());
+		tEffectElement.setAttribute (AN_XMLFRAME_TITLE, xmlFrameTitle);
 
 		return tEffectElement;
 	}
 
 	@Override
 	public String getEffectReport (RoundManager aRoundManager) {
-		return (REPORT_PREFIX + name + " for " + actor.getName () + " Show " + jFrame.getTitle () + ".");
+		return (REPORT_PREFIX + name + " for " + actor.getName () + " Show " + xmlFrameTitle + ".");
 	}
 
 	@Override
@@ -82,6 +95,13 @@ public class ShowFrameEffect extends Effect {
 
 	@Override
 	public boolean undoEffect (RoundManager aRoundManager) {
-		return true;
+		boolean tEffectApplied = false;
+
+		if (xmlFrame != XMLFrame.NO_XML_FRAME) {
+			xmlFrame.setVisible (false);
+		}
+		tEffectApplied = true;
+
+		return tEffectApplied;
 	}
 }
