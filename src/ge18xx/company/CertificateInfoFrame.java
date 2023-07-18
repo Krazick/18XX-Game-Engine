ba8e5781be691f7009b73b15b812d8e1a6451739
Krazick <markjsmith60@gmail.com>
2023-07-18 14:29:21 -0400

    New method setGameManager

--- src/ge18xx/company/CertificateInfoFrame.java
+++ src/ge18xx/company/CertificateInfoFrame.java
@@ -3,20 +3,36 @@ package ge18xx.company;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class CertificateInfoFrame extends JFrame implements ActionListener {
+import ge18xx.bank.Bank;
+import ge18xx.game.GameManager;
+import ge18xx.toplevel.XMLFrame;
+
+public class CertificateInfoFrame extends XMLFrame implements ActionListener {
 	public static final String GET_INFO = "GET INFO";
 	private static final long serialVersionUID = 1L;
 	private Certificate certificate;
+	private JPanel certificateInfoJPanel;
+	int padding1;
 
 	public CertificateInfoFrame () {
 		super ();
+		padding1 = 10;
 	}
 
 	public void setCertificate (Certificate aCertificate) {
 		certificate = aCertificate;
+	}
+	
+	@Override
+	public void setGameManager (String aFrameTitle, GameManager aGameManager) {
+		super.setGameManager (aFrameTitle, aGameManager);
+		System.out.println ("Certificate Info Dialog - Setting Game Manager for  " + certificate.getCompanyName ());
 		fillFrame ();
 	}
 	
@@ -32,11 +48,35 @@ public class CertificateInfoFrame extends JFrame implements ActionListener {
 	}
 
 	public void fillFrame () {
+		Corporation tCorporation;
 		JLabel tTitle;
+		JLabel tNote;
+		JLabel tPrice;
+		JLabel tRevenue;
 		
+		certificateInfoJPanel = new JPanel ();
+		certificateInfoJPanel.setLayout (new BoxLayout (certificateInfoJPanel, BoxLayout.Y_AXIS));
+		certificateInfoJPanel.setBorder (BorderFactory.createEmptyBorder (padding1, padding1, padding1, padding1));
+
+		tCorporation = certificate.getCorporation ();
 		tTitle = new JLabel ("Certificate for " + certificate.getCompanyName () + " (" + 
 							certificate.getCompanyAbbrev () + ")");
-		add (tTitle);
-		this.setSize (500, 500);
+		certificateInfoJPanel.add (tTitle);
+		certificateInfoJPanel.add (Box.createVerticalGlue ());
+		tNote = new JLabel (tCorporation.getNote ());
+		certificateInfoJPanel.add (tNote);
+		certificateInfoJPanel.add (Box.createVerticalGlue ());
+		
+		tPrice = new JLabel ("Price: " + Bank.formatCash (certificate.getValue ()));
+		certificateInfoJPanel.add (tPrice);
+		certificateInfoJPanel.add (Box.createVerticalGlue ());
+		
+		if (tCorporation.isAPrivateCompany ()) {
+			tRevenue = new JLabel ("Revenue: " +  Bank.formatCash (tCorporation.getRevenue ()));
+			certificateInfoJPanel.add (tRevenue);
+			certificateInfoJPanel.add (Box.createVerticalGlue ());
+		}
+		add (certificateInfoJPanel);
+		setSize (500, 500);
 	}
 }
