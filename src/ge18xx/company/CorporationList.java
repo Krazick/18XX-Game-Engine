85c73b5cc6fa518e62daa5f3220bdf523cd0e9fa
Krazick <markjsmith60@gmail.com>
2023-09-04 18:24:53 -0400

    New method findCompanyWithSpecial

--- src/ge18xx/company/CorporationList.java
+++ src/ge18xx/company/CorporationList.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -1281,6 +1282,21 @@ public class CorporationList extends InformationTable implements LoadableXMLI, P
 		return tFoundBenefit;
 	}
 
+	public ShareCompany findCompanyWithSpecial (JButton aJButton) {
+		ShareCompany tFoundShareCompany;
+		ShareCompany tShareCompany;
+		
+		tFoundShareCompany = ShareCompany.NO_SHARE_COMPANY;
+		for (Corporation tCorporation : corporations) {
+			tShareCompany = (ShareCompany) tCorporation;
+			if (tShareCompany.hasSpecialButton (aJButton)) {
+				tFoundShareCompany = tShareCompany;
+			}
+		}
+		
+		return tFoundShareCompany;
+	}
+	
 	// Pass method calls over to Round Manager
 
 	public void addAction (Action aAction) {
