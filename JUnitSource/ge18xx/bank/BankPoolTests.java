a3d30829b656914622ba8b5f7aa7663fe73e119c
Krazick <markjsmith60@gmail.com>
2023-09-06 19:45:22 -0400

    Update tests to support GameInfo Bank Pool name

--- JUnitSource/ge18xx/bank/BankPoolTests.java
+++ JUnitSource/ge18xx/bank/BankPoolTests.java
@@ -6,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import ge18xx.game.GameInfo;
 import ge18xx.game.GameManager;
 import ge18xx.game.GameTestFactory;
 
@@ -15,14 +17,27 @@ class BankPoolTests {
 	private BankTestFactory bankTestFactory;
 	private GameTestFactory gameTestFactory;
 	private GameManager mGameManager;
+	private GameManager mGameManager2;
+	private GameInfo mGameInfo;
+	private GameInfo gameInfo;
 	BankPool bankPool;
+	BankPool bankPool2;
 
 	@BeforeEach
 	void setUp () throws Exception {
 		bankTestFactory = new BankTestFactory ();
 		gameTestFactory = new GameTestFactory ();
 		mGameManager = gameTestFactory.buildGameManagerMock ();
+		mGameInfo = gameTestFactory.buildGameInfoMock ();
+		Mockito.when (mGameInfo.getBankPoolName ()).thenReturn ("Open Market");
+		Mockito.when (mGameManager.getActiveGame ()).thenReturn (mGameInfo);
+
 		bankPool = bankTestFactory.buildBankPool (mGameManager);
+		
+		mGameManager2 = gameTestFactory.buildGameManagerMock ();
+		gameInfo = gameTestFactory.buildGameInfo ();
+		Mockito.when (mGameManager2.getActiveGame ()).thenReturn (gameInfo);
+		bankPool2 = bankTestFactory.buildBankPool (mGameManager2);
 	}
 
 	@Test
@@ -35,7 +50,10 @@ class BankPoolTests {
 	@Test
 	@DisplayName ("Bank Pool Name and Abbrev Test")
 	void NameAndAbbrevTest () {
-		assertEquals ("Bank Pool", bankPool.getName ());
+		assertEquals ("Open Market", bankPool.getName ());
 		assertEquals ("Bank Pool", bankPool.getAbbrev ());
+
+		assertEquals ("Bank Pool", bankPool2.getName ());
+		assertEquals ("Bank Pool", bankPool2.getAbbrev ());
 	}
 }
