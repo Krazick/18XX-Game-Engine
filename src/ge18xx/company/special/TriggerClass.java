f0d18cd4ec8e835684603b8bae94b6008e499613
Krazick <markjsmith60@gmail.com>
2023-09-09 16:58:27 -0400

    Add rebuildSpecialClassPanel method for override

--- src/ge18xx/company/special/TriggerClass.java
+++ src/ge18xx/company/special/TriggerClass.java
@@ -1,8 +1,14 @@
 package ge18xx.company.special;
 
-public class TriggerClass {
+import ge18xx.player.Player;
 
+public class TriggerClass {
+	public static final TriggerClass NO_TRIGGER_CLASS = null;
+	
 	public TriggerClass () {
 	}
 
+	public void rebuildSpecialPanel (Player aActingPlayer) {
+
+	}
 }
