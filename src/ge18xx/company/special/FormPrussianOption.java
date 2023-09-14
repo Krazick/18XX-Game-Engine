3bd75d5fef8d0f19a8fb27cb4f18f352cc64f6ad
Krazick <markjsmith60@gmail.com>
2023-09-14 12:47:50 -0400

    Add Action to constructor

--- src/ge18xx/company/special/FormPrussianOption.java
+++ src/ge18xx/company/special/FormPrussianOption.java
@@ -4,10 +4,6 @@ import ge18xx.game.GameManager;
 import ge18xx.round.action.Action;
 
 public class FormPrussianOption extends TriggerClass {
-
-	public FormPrussianOption () {
-		
-	}
 	
 	public FormPrussianOption (GameManager aGameManager, Action aAction) {
 		System.out.println ("Initiate Form Prussian Option Game: " + aGameManager.getActiveGameName ());
