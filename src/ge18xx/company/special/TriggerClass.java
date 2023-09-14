4febbe293951c01418ae58133ab0342b7a03005b
Krazick <markjsmith60@gmail.com>
2023-09-14 12:52:44 -0400

    Adjust to work

--- src/ge18xx/company/special/TriggerClass.java
+++ src/ge18xx/company/special/TriggerClass.java
@@ -2,16 +2,27 @@ package ge18xx.company.special;
 
 import java.util.List;
 
+import ge18xx.game.GameManager;
 import ge18xx.player.Player;
+import ge18xx.round.action.BuyTrainAction;
 
 public class TriggerClass {
 	public static final TriggerClass NO_TRIGGER_CLASS = null;
 	
 	public TriggerClass () {
+		
+	}
+	
+	public TriggerClass (GameManager aGameManager, BuyTrainAction aBuyTrainAction) {
+		
 	}
 
 	public void rebuildSpecialPanel (Player aActingPlayer) {
-
+	
+	}
+	
+	public void hideSpecialPanel () {
+		
 	}
 	
 	public boolean updateToNextPlayer (List<Player> aPlayers) {
