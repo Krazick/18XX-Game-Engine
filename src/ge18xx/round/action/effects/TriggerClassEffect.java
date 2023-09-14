52f2e06ba82aa54159b13a9ee29b3cd8fd6cfde3
Krazick <markjsmith60@gmail.com>
2023-09-14 12:45:36 -0400

    New Effect to allow undo to hide the Special Panel

---
+++ src/ge18xx/round/action/effects/TriggerClassEffect.java
@@ -0,0 +1,57 @@
+package ge18xx.round.action.effects;
+
+import ge18xx.game.GameManager;
+import ge18xx.player.Player;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
+import ge18xx.utilities.XMLNode;
+
+public class TriggerClassEffect extends SpecialPanelEffect {
+	public final static String NAME = "Trigger Class";
+
+	public TriggerClassEffect () {
+		this (NAME);
+	}
+
+	public TriggerClassEffect (String aName) {
+		super (aName);
+	}
+
+	public TriggerClassEffect (ActorI aActor) {
+		super (NAME, aActor);
+	}
+
+	public TriggerClassEffect (String aName, ActorI aActor) {
+		super (aName, aActor);
+	}
+
+	public TriggerClassEffect (XMLNode aEffectNode, GameManager aGameManager) {
+		super (aEffectNode, aGameManager);
+		setName (NAME);
+	}
+
+	@Override
+	public boolean applyEffect (RoundManager aRoundManager) {
+		boolean tEffectApplied;
+		
+		tEffectApplied = true;
+
+		return tEffectApplied;
+	}
+	
+	@Override
+	public boolean undoEffect (RoundManager aRoundManager) {
+		boolean tEffectUndone;
+		Player tPresident;
+		
+		tEffectUndone = false;
+		if (actor.isAPlayer ()) {
+			tPresident = (Player) actor;
+			tPresident.setRepaymentFinished (false);
+			hideSpecialPanel (aRoundManager);
+			tEffectUndone = true;
+		}
+
+		return tEffectUndone;
+	}
+}
