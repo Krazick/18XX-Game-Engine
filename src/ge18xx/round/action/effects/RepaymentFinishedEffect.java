10fb842cee61511a7e8261a6524b56b0e23e5fb6
Krazick <markjsmith60@gmail.com>
2023-09-18 13:49:18 -0400

    Remove calls to rebuild special panel

--- src/ge18xx/round/action/effects/RepaymentFinishedEffect.java
+++ src/ge18xx/round/action/effects/RepaymentFinishedEffect.java
@@ -42,14 +42,11 @@ public class RepaymentFinishedEffect extends SpecialPanelEffect {
 	public boolean applyEffect (RoundManager aRoundManager) {
 		boolean tEffectApplied;
 		Player tPlayer;
-		int tPlayerIndex;
 		
 		tEffectApplied = false;
 		if (actor.isAPlayer ()) {
 			tPlayer = (Player) actor;
 			tPlayer.setRepaymentFinished (replaymentFinished);
-			tPlayerIndex = getPlayerIndex (aRoundManager, tPlayer);
-			rebuildSpecialPanel (aRoundManager, tPlayerIndex);
 			tEffectApplied = true;
 		}
 
@@ -60,14 +57,11 @@ public class RepaymentFinishedEffect extends SpecialPanelEffect {
 	public boolean undoEffect (RoundManager aRoundManager) {
 		boolean tEffectUndone;
 		Player tPlayer;
-		int tPlayerIndex;
 		
 		tEffectUndone = false;
 		if (actor.isAPlayer ()) {
 			tPlayer = (Player) actor;
 			tPlayer.setRepaymentFinished (false);
-			tPlayerIndex = getPlayerIndex (aRoundManager, tPlayer);
-			rebuildSpecialPanel (aRoundManager, tPlayerIndex);
 			tEffectUndone = true;
 		}
 
