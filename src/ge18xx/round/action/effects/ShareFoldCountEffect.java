98157f477ce6a64dfd6240f1644817889dc7936e
Krazick <markjsmith60@gmail.com>
2023-09-19 12:42:30 -0400

    Initial Version

---
+++ src/ge18xx/round/action/effects/ShareFoldCountEffect.java
@@ -0,0 +1,110 @@
+package ge18xx.round.action.effects;
+
+import ge18xx.company.special.LoanRepayment;
+import ge18xx.company.special.TriggerClass;
+import ge18xx.game.GameManager;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.XMLNode;
+
+public class ShareFoldCountEffect extends Effect {
+	public final static String NAME = "Share Fold Count";
+	final static AttributeName AN_OLD_SHARE_FOLD_COUNT = new AttributeName ("OldShareFoldCount");
+	final static AttributeName AN_NEW_SHARE_FOLD_COUNT = new AttributeName ("NewShareFoldCount");
+	int oldShareFoldCount;
+	int newShareFoldCount;
+
+	public ShareFoldCountEffect () {
+		this (NAME);
+	}
+
+	public ShareFoldCountEffect (String aName) {
+		super (aName);
+	}
+
+	public ShareFoldCountEffect (String aName, ActorI aActor) {
+		super (aName, aActor);
+	}
+
+	public ShareFoldCountEffect (ActorI aActor, int aOldShareFoldCount, int aNewShareFoldCount) {
+		super (NAME, aActor);
+		setOldShareFoldCount (aOldShareFoldCount);
+		setNewShareFoldCount (aNewShareFoldCount);
+	}
+
+	public ShareFoldCountEffect (XMLNode aEffectNode, GameManager aGameManager) {
+		super (aEffectNode, aGameManager);
+		
+		int tOldShareFoldCount;
+		int tNewShareFoldCount;
+		
+		tOldShareFoldCount = aEffectNode.getThisIntAttribute (AN_OLD_SHARE_FOLD_COUNT);
+		setOldShareFoldCount (tOldShareFoldCount);
+		tNewShareFoldCount = aEffectNode.getThisIntAttribute (AN_NEW_SHARE_FOLD_COUNT);
+		setNewShareFoldCount (tNewShareFoldCount);
+	}
+
+	public void setNewShareFoldCount (int aNewShareFoldCount) {
+		newShareFoldCount = aNewShareFoldCount;
+	}
+	
+	public int getNewShareFoldCount () {
+		return newShareFoldCount;
+	}
+
+	public void setOldShareFoldCount (int aOldShareFoldCount) {
+		oldShareFoldCount = aOldShareFoldCount;
+	}
+	
+	public int getOldShareFoldCount () {
+		return oldShareFoldCount;
+	}
+	
+	@Override
+	public String getEffectReport (RoundManager aRoundManager) {
+		return (REPORT_PREFIX + name + " for " + actor.getName () + " from " + getOldShareFoldCount () + " to " + getNewShareFoldCount ());
+	}
+
+	@Override
+	public boolean applyEffect (RoundManager aRoundManager) {
+		boolean tEffectApplied;
+		TriggerClass tTriggerClass;
+		LoanRepayment tLoanRepayment;
+		GameManager tGameManager;
+		
+		tEffectApplied = false;
+		if (actor.isAShareCompany ()) {
+			tGameManager = aRoundManager.getGameManager ();
+			tTriggerClass = tGameManager.getTriggerClass ();
+			if (tTriggerClass instanceof LoanRepayment) {
+				tLoanRepayment = (LoanRepayment) tTriggerClass;
+				tLoanRepayment.setShareFoldCount (newShareFoldCount);
+				tEffectApplied = true;
+			}
+		}
+
+		return tEffectApplied;
+	}
+
+	@Override
+	public boolean undoEffect (RoundManager aRoundManager) {
+		boolean tEffectUndone;
+		TriggerClass tTriggerClass;
+		LoanRepayment tLoanRepayment;
+		GameManager tGameManager;
+		
+		tEffectUndone = false;
+		if (actor.isAShareCompany ()) {
+			tGameManager = aRoundManager.getGameManager ();
+			tTriggerClass = tGameManager.getTriggerClass ();
+			if (tTriggerClass instanceof LoanRepayment) {
+				tLoanRepayment = (LoanRepayment) tTriggerClass;
+				tLoanRepayment.setShareFoldCount (oldShareFoldCount);
+				tEffectUndone = true;
+			}
+		}
+
+		return tEffectUndone;
+	}
+}
