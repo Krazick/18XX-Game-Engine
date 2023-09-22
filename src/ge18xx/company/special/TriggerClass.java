5998646b9d42ff5893ac306f14e5b439ee89f017
Krazick <markjsmith60@gmail.com>
2023-09-22 15:23:42 -0400

    Move buildSpecialButton and getEnabled methods into this class

--- src/ge18xx/company/special/TriggerClass.java
+++ src/ge18xx/company/special/TriggerClass.java
@@ -1,9 +1,13 @@
 package ge18xx.company.special;
 
+import java.awt.event.ActionListener;
 import java.util.List;
+
+import javax.swing.JButton;
 
 import ge18xx.game.GameManager;
 import ge18xx.player.Player;
+import ge18xx.utilities.GUI;
 
 public class TriggerClass {
 	public static final TriggerClass NO_TRIGGER_CLASS = null;
@@ -35,4 +39,31 @@ public class TriggerClass {
 	public void setCurrentPlayerIndex (int aCurrentPlayerIndex) {
 		
 	}
+	
+	public JButton buildSpecialButton (String aTitle, String aActionCommand, String aToolTip, ActionListener aActionListener) {
+		JButton tSpecialButton;
+		boolean tEnabled;
+		
+		tEnabled = getEnabled (aToolTip);
+		tSpecialButton = new JButton (aTitle);
+		tSpecialButton.setActionCommand (aActionCommand);
+		tSpecialButton.setEnabled (tEnabled);
+		tSpecialButton.setToolTipText (aToolTip);
+		tSpecialButton.addActionListener (aActionListener);
+		
+		return tSpecialButton;
+	}
+
+	public boolean getEnabled (String aToolTip) {
+		boolean tEnabled;
+		
+		if (GUI.EMPTY_STRING.equals (aToolTip)) {
+			tEnabled = true;
+		} else {
+			tEnabled = false;
+		}
+		
+		return tEnabled;
+	}
+
 }
