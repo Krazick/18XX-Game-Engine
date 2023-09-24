2b9d1bb5c73b0ac146e2b58181ca76e1a1c1fc14
Krazick <markjsmith60@gmail.com>
2023-09-24 17:12:33 -0400

    Extracted out two Subclasses

---
+++ src/ge18xx/company/special/LoanRepayment.java
@@ -0,0 +1,348 @@
+package ge18xx.company.special;
+
+import java.awt.event.ActionEvent;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import ge18xx.bank.Bank;
+import ge18xx.company.Certificate;
+import ge18xx.company.ShareCompany;
+import ge18xx.game.GameManager;
+import ge18xx.player.Player;
+import ge18xx.player.PlayerManager;
+import ge18xx.player.Portfolio;
+import ge18xx.round.action.ActorI;
+import ge18xx.round.action.RepaymentFinishedAction;
+import ge18xx.round.action.RepaymentHandledAction;
+import ge18xx.round.action.SpecialPanelAction;
+import ge18xx.utilities.GUI;
+
+public class LoanRepayment extends PlayerFormationPhase {
+	public static final String PAY_FROM_TREASURY = "Pay From Treasury";
+	public static final String PAY_FROM_PRESIDENT = "Pay From President";
+	public static final String CONFIRM_REPAYMENT = "Confirm Repayment";
+	public static final String PAY_TREASURY = "PayTreasury";
+	public static final String PAY_PRESIDENT = "PayPresident";
+
+	private static final long serialVersionUID = 1L;
+	boolean sharesExchanged;
+	boolean oneShareToBankPool;
+	JButton exchange;
+	int foldingCompanyCount;
+	int totalExchangeCount;
+
+	public LoanRepayment (GameManager aGameManager, FormationPhase aLoanRepayment, Player aPlayer,
+			Player aActingPresident) {
+		super (aGameManager, aLoanRepayment, aPlayer, aActingPresident);
+		
+	}
+
+	public String confirmRepayment (ShareCompany aShareCompany) {
+		String tToolTip;
+		int tLoanAmount;
+		
+		tToolTip = GUI.EMPTY_STRING;
+		if (aShareCompany.wasRepaymentHandled ()) {
+			tToolTip = "Company [" + aShareCompany.getAbbrev () + "] has already confirmed repayments";		
+		} else if (aShareCompany.hasOutstandingLoans ()) {
+			tLoanAmount = aShareCompany.getLoanAmount ();
+			if (aShareCompany.getCash () >= tLoanAmount) {
+				tToolTip = "Company [" + aShareCompany.getAbbrev () + "] Treasury has enough to pay the Loan Amount " + 
+							Bank.formatCash (tLoanAmount);
+			}
+		}
+	
+		return tToolTip;
+	}
+
+	public String canPayFromTreasury (ShareCompany aShareCompany) {
+		String tToolTip;
+		int tLoanAmount;
+		
+		tToolTip = GUI.EMPTY_STRING;
+		if (aShareCompany.hasOutstandingLoans ()) {
+			tLoanAmount = aShareCompany.getLoanAmount ();
+			if (aShareCompany.getCash () < tLoanAmount) {
+				tToolTip = "Company [" + aShareCompany.getAbbrev () + "] Treasury has less than the Loan Amount " + 
+							Bank.formatCash (tLoanAmount);
+			}
+		} else {
+			tToolTip = "Company [" + aShareCompany.getAbbrev () + "] has no Outstanding Loans";
+		}
+		
+		return tToolTip;
+	}
+
+	public String canPayFromPresident (ShareCompany aShareCompany) {
+		Player tPlayer;
+		int tPlayerCash;
+		int tTotalLoanAmount;
+		int tLoanAmount;
+		int tLoanCount;
+		String tToolTip;
+		
+		tToolTip = GUI.EMPTY_STRING;
+		if (aShareCompany.hasOutstandingLoans ()) {
+			if (! aShareCompany.wasRepaymentHandled ()) {
+				tLoanAmount = aShareCompany.getLoanAmount ();
+				if (aShareCompany.getCash () >= tLoanAmount) {
+					tToolTip = "Company [" + aShareCompany.getAbbrev () + "] Treasury has enough to pay an Outstanding Loan";
+				} else {
+					tPlayer = (Player) aShareCompany.getPresident ();
+					tPlayerCash = tPlayer.getCash ();
+					tLoanCount = aShareCompany.getLoanCount ();
+					tTotalLoanAmount = tLoanAmount * tLoanCount;
+					if (tPlayerCash < tTotalLoanAmount)  {
+						tToolTip = "President " + aShareCompany.getPresidentName () + 
+									" Treasury has less than the Loan Amount " + Bank.formatCash (tLoanAmount);
+					}
+				}
+			} else {
+				tToolTip = "Company [" + aShareCompany.getAbbrev () + "] Outstanding Loans have been confirmed";
+			}
+		} else {
+			tToolTip = "Company [" + aShareCompany.getAbbrev () + "] has no Outstanding Loans";
+		}
+		
+		return tToolTip;
+	}
+
+	public void handleRepayFromTreasury (ShareCompany aShareCompany) {
+		int tLoanCount;
+		int tLoanAmount;
+		int tTotalLoanCount;
+		int tTreasury;
+		int tPresidentContribution;
+		
+		tTreasury = aShareCompany.getTreasury ();
+		tTotalLoanCount = aShareCompany.getLoanCount ();
+		tLoanAmount = aShareCompany.getLoanAmount ();
+		if (tTotalLoanCount > 0) {
+			if (tTreasury >= tLoanAmount) {
+				tLoanCount = Math.min (tTotalLoanCount, tTreasury/tLoanAmount);
+				tPresidentContribution = 0;
+				redeemLoanAndUpdate (aShareCompany, tLoanCount, tPresidentContribution);
+			}
+		}
+	}
+
+	public void handleRepayFromPresident (ShareCompany aShareCompany) {
+		int tLoanAmount;
+		int tTreasury;
+		int tLoanCount;
+		int tTotalLoanCount;
+		int tPresidentContribution;
+		
+		tTotalLoanCount = aShareCompany.getLoanCount ();
+		tLoanAmount = aShareCompany.getLoanAmount ();
+		tTreasury = player.getCash ();
+		tLoanCount = 1;
+		if (tTotalLoanCount > 0) {
+			if (tTreasury >= tLoanAmount) {
+				tPresidentContribution = tLoanAmount;
+				redeemLoanAndUpdate (aShareCompany, tLoanCount, tPresidentContribution);
+			}
+		}
+	}
+
+	public void handleConfirmRepayment (ShareCompany aShareCompany) {
+		RepaymentHandledAction tRepaymentHandledAction;
+		String tOperatingRoundID;
+		String tNotification;
+		String tFoldingCompanyAbbrev;
+		boolean tRepaymentHandled;
+		int tCurrentPlayerIndex;
+		int tShareFoldCount;
+		int tNewShareFoldCount;
+		int tOldShareFoldCount;
+		
+		tOperatingRoundID = aShareCompany.getOperatingRoundID ();
+		tRepaymentHandledAction = new RepaymentHandledAction (ActorI.ActionStates.OperatingRound, 
+								tOperatingRoundID, aShareCompany);
+		tRepaymentHandled = true;
+		aShareCompany.setRepaymentHandled (tRepaymentHandled);
+		tOldShareFoldCount = formationPhase.getShareFoldCount ();
+		if (aShareCompany.willFold ()) {
+			tShareFoldCount = aShareCompany.getShareFoldCount ();
+			formationPhase.addShareFoldCount (tShareFoldCount);
+			
+			tNewShareFoldCount = formationPhase.getShareFoldCount ();
+			
+			tFoldingCompanyAbbrev = aShareCompany.getAbbrev ();
+			tNotification = formationPhase.buildFoldNotification (tFoldingCompanyAbbrev, tShareFoldCount);
+			formationPhase.setNotificationText (tNotification);
+					
+			tRepaymentHandledAction.addShareFoldCountEffect (aShareCompany, tOldShareFoldCount, tNewShareFoldCount);
+		}
+	
+		tRepaymentHandledAction.addSetRepaymentHandledEffect (aShareCompany, tRepaymentHandled);
+	
+		tCurrentPlayerIndex = getCurrentPlayerIndex ();
+	
+		formationPhase.rebuildSpecialPanel (tCurrentPlayerIndex);
+	
+		aShareCompany.addAction (tRepaymentHandledAction);
+	}
+
+	public void buildSpecialButtons (ShareCompany aShareCompany, JPanel aShareCompanyJPanel, boolean aActingPlayer) {
+		JButton tPayFromTreasury;
+		JButton tPayFromPresident;
+		JButton tConfirm;
+		String tToolTip;
+		
+		if (aActingPlayer) {
+			tToolTip = canPayFromTreasury (aShareCompany);
+		} else {
+			tToolTip = NOT_ACTING_PRESIDENT;
+		}
+		tPayFromTreasury = formationPhase.buildSpecialButton (PAY_FROM_TREASURY, PAY_TREASURY, tToolTip, this);
+		aShareCompanyJPanel.add (tPayFromTreasury);
+		aShareCompanyJPanel.add (Box.createHorizontalStrut (10));
+		aShareCompany.addSpecialButton (tPayFromTreasury);
+		
+		if (aActingPlayer) {
+			tToolTip = canPayFromPresident (aShareCompany);
+		} else {
+			tToolTip = NOT_ACTING_PRESIDENT;
+		}
+		tPayFromPresident = formationPhase.buildSpecialButton (PAY_FROM_PRESIDENT, PAY_PRESIDENT, tToolTip, this);
+		aShareCompanyJPanel.add (tPayFromPresident);
+		aShareCompany.addSpecialButton (tPayFromPresident);
+		
+		if (aActingPlayer) {
+			tToolTip = confirmRepayment (aShareCompany);
+		} else {
+			tToolTip = NOT_ACTING_PRESIDENT;
+		}
+		tConfirm = formationPhase.buildSpecialButton (CONFIRM_REPAYMENT, CONFIRM_REPAYMENT, tToolTip, this);
+		aShareCompanyJPanel.add (tConfirm);
+		aShareCompany.addSpecialButton (tConfirm);
+	}
+
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		super.actionPerformed (aEvent);
+		
+		String tActionCommand;
+		ShareCompany tShareCompany;
+		JButton tActivatedButton;
+		
+		tActionCommand = aEvent.getActionCommand ();
+		tActivatedButton = getActivatedButton (aEvent);
+		if (tActivatedButton != GUI.NO_BUTTON) {
+			tShareCompany = findShareCompany (tActivatedButton);
+			if (tShareCompany != ShareCompany.NO_SHARE_COMPANY) {
+				if (tShareCompany != ShareCompany.NO_SHARE_COMPANY) {
+					if (tActionCommand.equals (PAY_TREASURY)) {
+						handleRepayFromTreasury (tShareCompany);
+					} else if (tActionCommand.equals (PAY_PRESIDENT)) {
+						handleRepayFromPresident (tShareCompany);
+					} else if (tActionCommand.equals (CONFIRM_REPAYMENT)) {
+						handleConfirmRepayment (tShareCompany);
+					}
+				}
+			}
+		}
+	}
+	
+	@Override
+	public JPanel buildCompanyJPanel (ShareCompany aShareCompany, boolean aActingPlayer) {
+		JPanel tShareCompanyJPanel;
+		
+		tShareCompanyJPanel = new JPanel ();
+		tShareCompanyJPanel.setLayout (new BoxLayout (tShareCompanyJPanel, BoxLayout.X_AXIS));
+
+		tShareCompanyJPanel = super.buildCompanyJPanel (aShareCompany, aActingPlayer, tShareCompanyJPanel);
+		
+		buildSpecialButtons (aShareCompany, tShareCompanyJPanel, aActingPlayer);
+
+		return tShareCompanyJPanel;
+	}
+	
+	public boolean repaymentFinished () {
+		return player.getRepaymentFinished ();
+	}
+
+	@Override
+	public void updateDoneButton () {
+		String tToolTip;
+		Portfolio tPortfolio;
+		Certificate tCertificate;
+		ShareCompany tShareCompany;
+		int tCertificateCount;
+		int tCertificateIndex;
+		boolean tAllCompaniesHandled;
+
+		if (repaymentFinished ()) {
+			if (formationPhase.getFormationState ().equals ((ActorI.ActionStates.LoanRepayment))) {
+				done.setEnabled (false);
+				tToolTip = "President already completed all loan paybacks";
+				done.setToolTipText (tToolTip);
+			} else {
+				done.setEnabled (false);
+				tToolTip = "Not Ready Yet";
+				done.setToolTipText (tToolTip);
+			}	
+		} else {
+			tPortfolio = player.getPortfolio ();
+			tCertificateCount = tPortfolio.getCertificateTotalCount ();
+			tAllCompaniesHandled = true;
+			for (tCertificateIndex = 0; tCertificateIndex < tCertificateCount; tCertificateIndex++) {
+				tCertificate = tPortfolio.getCertificate (tCertificateIndex);
+				if (tCertificate.isAShareCompany ()) {
+					if (tCertificate.isPresidentShare ()) {
+						tShareCompany = tCertificate.getShareCompany ();
+						if (! tShareCompany.wasRepaymentHandled ()) {
+							tAllCompaniesHandled = false;
+							tToolTip = "Not all Share Companies have confirmed loan repayments";
+							done.setToolTipText (tToolTip);
+						}
+					}
+				}
+			}
+			done.setEnabled (tAllCompaniesHandled);
+		}
+	}
+	
+	@Override
+	public void handlePlayerDone () {
+		List<Player> tPlayers;
+		PlayerManager tPlayerManager;
+		RepaymentFinishedAction tRepaymentFinishedAction;
+		Player tNewPlayer;
+		String tOperatingRoundID;
+		
+		tOperatingRoundID = gameManager.getOperatingRoundID ();
+		
+		tPlayerManager = gameManager.getPlayerManager ();
+		tPlayers = tPlayerManager.getPlayers ();
+		formationPhase.updateToNextPlayer (tPlayers);
+		tNewPlayer = formationPhase.getCurrentPlayer ();
+		
+		player.setRepaymentFinished (true);
+		tRepaymentFinishedAction = new RepaymentFinishedAction (ActorI.ActionStates.OperatingRound, 
+				tOperatingRoundID, player);
+		tRepaymentFinishedAction.addRepaymentFinishedEffect (player, true);
+		tRepaymentFinishedAction.addUpdateToNextPlayerEffect (player, tNewPlayer);
+		gameManager.addAction (tRepaymentFinishedAction);
+	}
+
+	public void redeemLoanAndUpdate (ShareCompany aShareCompany, int tLoanCount, int tPresidentContribution) {
+		int tCurrentPlayerIndex;
+		SpecialPanelAction tSpecialPanelAction;
+		String tOperatingRoundID;
+		
+		tCurrentPlayerIndex = getCurrentPlayerIndex ();
+		aShareCompany.redeemLoans (tLoanCount, tPresidentContribution);
+		formationPhase.rebuildSpecialPanel (tCurrentPlayerIndex);
+		tOperatingRoundID = aShareCompany.getOperatingRoundID ();
+		tSpecialPanelAction = new SpecialPanelAction (ActorI.ActionStates.OperatingRound, 
+								tOperatingRoundID, aShareCompany);
+		tSpecialPanelAction.addShowSpecialPanelEffect (aShareCompany, aShareCompany);
+		aShareCompany.addAction (tSpecialPanelAction);
+	}
+}
---
+++ src/ge18xx/company/special/ShareExchange.java
@@ -0,0 +1,282 @@
+package ge18xx.company.special;
+
+import java.awt.event.ActionEvent;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import ge18xx.bank.Bank;
+import ge18xx.bank.BankPool;
+import ge18xx.company.Certificate;
+import ge18xx.company.Corporation;
+import ge18xx.company.ShareCompany;
+import ge18xx.game.GameManager;
+import ge18xx.phase.PhaseInfo;
+import ge18xx.player.Player;
+import ge18xx.player.Portfolio;
+import ge18xx.player.PortfolioHolderI;
+import ge18xx.round.action.ActorI;
+import ge18xx.round.action.TransferOwnershipAction;
+import ge18xx.utilities.GUI;
+
+public class ShareExchange extends PlayerFormationPhase {
+	public static final String EXCHANGE = "Exchange Shares";
+
+	private static final long serialVersionUID = 1L;
+	boolean sharesExchanged;
+	boolean oneShareToBankPool;
+	JButton exchange;
+	int foldingCompanyCount;
+	int totalExchangeCount;
+
+	public ShareExchange (GameManager aGameManager, FormationPhase aLoanRepayment, Player aPlayer,
+			Player aActingPresident) {
+		super (aGameManager, aLoanRepayment, aPlayer, aActingPresident);
+		setSharesExchanged (false);
+
+	}
+
+	public void setSharesExchanged (boolean aSharesExchanged) {
+		sharesExchanged = aSharesExchanged;
+	}
+	
+	@Override
+	public JPanel buildPlayerCompaniesJPanel (Portfolio aPlayerPortfolio, boolean aActingPlayer) {
+		JPanel tPlayerShareExchangePanel;
+		JLabel tShareExchangeLabel;
+		JLabel tShareExchangeInfo;
+		String tShareExchangeText;
+		String tToolTip;
+		
+		tPlayerShareExchangePanel = new JPanel ();
+		tPlayerShareExchangePanel.setLayout (new BoxLayout (tPlayerShareExchangePanel, BoxLayout.Y_AXIS));
+		tPlayerShareExchangePanel.add (Box.createVerticalStrut (5));
+		
+		tShareExchangeLabel = new JLabel ("Player Share Exchange");
+		tPlayerShareExchangePanel.add (tShareExchangeLabel);
+		tPlayerShareExchangePanel.add (Box.createVerticalStrut (5));
+	
+		tShareExchangeInfo = new JLabel ();
+		tShareExchangeText = buildShareExchangeText (aPlayerPortfolio);
+		tShareExchangeInfo.setText (tShareExchangeText);
+	
+		tPlayerShareExchangePanel.add (tShareExchangeInfo);
+		tPlayerShareExchangePanel.add (Box.createVerticalStrut (5));
+	
+		if (	foldingCompanyCount == 0) {
+			tToolTip = "No Shares till be exchanged for " + formationPhase.getFormingCompanyAbbrev ();;
+		} else if (! aActingPlayer) {
+			tToolTip = NOT_ACTING_PRESIDENT;
+		} else {
+			tToolTip = GUI.EMPTY_STRING;
+		}
+	
+		exchange = formationPhase.buildSpecialButton (EXCHANGE, EXCHANGE, tToolTip, this);
+	
+		tPlayerShareExchangePanel.add (exchange);
+		tPlayerShareExchangePanel.add (Box.createVerticalStrut (5));
+		
+		return tPlayerShareExchangePanel;
+	}
+
+	public String buildShareExchangeText (Portfolio aPlayerPortfolio) {
+		String tShareExchangeText;
+		String tShareExchangePhrase;
+		int tCertificateCount;
+		int tCertificateIndex;
+		int tShareExchangeCount;
+		int tTotalShareCount;
+	
+		int tFoldingCompanyIndex;
+		String tShareCompanyAbbrev;
+		String tFormingCompanyAbbrev;
+		List<String> tShareExchange;
+		ShareCompany tShareCompany;
+		Certificate tCertificate;
+		
+		tCertificateCount = aPlayerPortfolio.getCertificateTotalCount ();
+		tShareExchangeText = GUI.EMPTY_STRING;
+		tTotalShareCount = 0;
+		shareCompaniesHandled = new LinkedList<String> ();
+		tShareExchange = new LinkedList<String> ();
+	
+		for (tCertificateIndex = 0; tCertificateIndex < tCertificateCount; tCertificateIndex++) {
+			tCertificate = aPlayerPortfolio.getCertificate (tCertificateIndex);
+			if (tCertificate.isAShareCompany ()) {
+				tShareCompany = tCertificate.getShareCompany ();
+				tShareCompanyAbbrev = tShareCompany.getAbbrev ();
+				if ( !(shareCompaniesHandled.contains (tShareCompanyAbbrev))) {
+					if (tShareCompany.willFold ()) {
+						tShareExchangeCount = aPlayerPortfolio.getShareCountFor (tShareCompany);
+						tTotalShareCount += tShareExchangeCount;
+						if (tShareExchangeCount == 1) {
+							tShareExchangePhrase = tShareExchangeCount + " Share ";
+						} else {
+							tShareExchangePhrase = tShareExchangeCount + " Shares ";
+						}
+						tShareExchangePhrase += " of " + tShareCompanyAbbrev;
+						tShareExchange.add (tShareExchangePhrase);
+						shareCompaniesHandled.add (tShareCompanyAbbrev);
+					}
+				}
+			}
+		}
+		totalExchangeCount = tTotalShareCount/2;
+		if (tTotalShareCount % 2 != 0) {
+			oneShareToBankPool = true;
+		}
+		foldingCompanyCount = tShareExchange.size ();
+		tFormingCompanyAbbrev = formationPhase.getFormingCompanyAbbrev ();
+		tShareExchangeText = GUI.EMPTY_STRING;
+		if  (tTotalShareCount == 0) {
+			tShareExchangeText = "No Shares till be exchanged for " + tFormingCompanyAbbrev;
+		} else {
+			if (foldingCompanyCount == 1) {
+				tShareExchangeText = tShareExchange.get (0);
+			} else {
+				for (tFoldingCompanyIndex = 0; tFoldingCompanyIndex < foldingCompanyCount; tFoldingCompanyIndex++) {
+					if (tFoldingCompanyIndex == foldingCompanyCount) {
+						tShareExchangeText += " and ";
+					} else {
+						tShareExchangeText += ", ";
+					}
+					tShareExchangeText += tShareExchange.get (tFoldingCompanyIndex);
+				}
+			}
+			tShareExchangeText += " will be exchanged for " + totalExchangeCount + " Shares of " + tFormingCompanyAbbrev;
+		}
+		
+		return tShareExchangeText;
+	}
+
+	public void handleShareExchange () {
+		TransferOwnershipAction tTransferOwnershipAction;
+		BankPool tBankPool;
+		Bank tBank;
+		Portfolio tBankPortfolio;
+		Portfolio tPlayerPortfolio;
+		ShareCompany tShareCompany;
+		Corporation tFormingCompany;
+		Certificate tCertificate;
+		Certificate tFoldedCertificate;
+		int tCertificateIndex;
+		int tCertificateCount;
+		int tFoldingIndex;
+		int tFormingCompanyID;
+		int tPercentage;
+		String tOperatingRoundID;
+		String tFormingAbbrev;
+		
+		tBankPool = gameManager.getBankPool ();
+		tBank = gameManager.getBank ();
+		tBankPortfolio = tBank.getPortfolio ();
+		tPlayerPortfolio = player.getPortfolio ();
+		tCertificateCount = tPlayerPortfolio.getCertificateTotalCount ();
+		tOperatingRoundID = gameManager.getOperatingRoundID ();
+		tTransferOwnershipAction = new TransferOwnershipAction (ActorI.ActionStates.OperatingRound, tOperatingRoundID, player);
+		for (String tCompanyAbbrev : shareCompaniesHandled) {
+			tShareCompany = gameManager.getShareCompany (tCompanyAbbrev);
+			tCertificateCount = tPlayerPortfolio.getCertificateTotalCount ();
+			for (tCertificateIndex = tCertificateCount - 1; tCertificateIndex >= 0; tCertificateIndex--) {
+				tCertificate = tPlayerPortfolio.getCertificate (tCertificateIndex);
+				if (tCertificate.getCompanyAbbrev ().equals (tCompanyAbbrev)) {
+					if ((tCertificate.getPercentage () == PhaseInfo.STANDARD_SHARE_SIZE)  && oneShareToBankPool) {
+						transferShare (player, tBankPool, tCertificate, tTransferOwnershipAction);
+						oneShareToBankPool = false;
+					} else {
+						transferShare (player, tShareCompany, tCertificate, tTransferOwnershipAction);
+					}
+				}
+			}
+		}
+		
+		if (totalExchangeCount > 0) {
+			tFormingCompanyID = gameManager.getFormingCompanyId ();
+			tFormingCompany = gameManager.getCorporationByID (tFormingCompanyID);
+			tFormingAbbrev = tFormingCompany.getAbbrev ();
+			if (formationPhase.getShareFoldCount () > 10) {
+				tPercentage = PhaseInfo.STANDARD_SHARE_SIZE/2;
+			} else {
+				tPercentage = PhaseInfo.STANDARD_SHARE_SIZE;
+			}
+			System.out.println ("All Players Total Exchange Count " +  formationPhase.getShareFoldCount ());
+			for (tFoldingIndex = 0; tFoldingIndex < totalExchangeCount; tFoldingIndex++) {
+				tFoldedCertificate = tBankPortfolio.getCertificate (tFormingAbbrev, tPercentage, false);
+				if (tFoldedCertificate != Certificate.NO_CERTIFICATE) {
+					transferShare (tBank, player, tFoldedCertificate, tTransferOwnershipAction);
+				} else {
+					System.err.println ("No certificate available with All Players Total Exchange Count " + 
+									formationPhase.getShareFoldCount ());
+				}
+			}
+		}
+		if (tTransferOwnershipAction.getEffectCount () > 0) {
+			tTransferOwnershipAction.printActionReport (gameManager.getRoundManager ());
+		} else {
+			System.err.println ("No Effects in the Action");
+		}
+		setSharesExchanged (true);
+		exchange.setEnabled (false);
+		exchange.setToolTipText ("President has not completed all share exchanges");
+		updateDoneButton (true);
+		formationPhase.rebuildSpecialPanel (formationPhase.getCurrentPlayerIndex ());
+	}
+
+	public void transferShare (PortfolioHolderI aFromActor, ActorI aToActor, Certificate aCertificate, 
+					TransferOwnershipAction aTransferOwnershipAction) {
+		Portfolio tPlayerPortfolio;
+		Portfolio tToActorPortfolio;
+		PortfolioHolderI tToHolder;
+	
+		tPlayerPortfolio = aFromActor.getPortfolio ();
+		tToHolder = (PortfolioHolderI) aToActor;
+		tToActorPortfolio = tToHolder.getPortfolio ();
+		tToActorPortfolio.transferOneCertificateOwnership (tPlayerPortfolio, aCertificate);
+		aTransferOwnershipAction.addTransferOwnershipEffect (aFromActor, aCertificate, aToActor);
+	}
+	
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		String tActionCommand;
+		
+		tActionCommand = aEvent.getActionCommand ();
+		if (tActionCommand.equals (EXCHANGE)) {
+			handleShareExchange ();
+		} else {
+			super.actionPerformed (aEvent);
+		}
+	}
+	
+	public boolean sharesExchanged () {
+		return sharesExchanged;
+	}
+	
+	@Override
+	public void updateDoneButton () {
+		String tToolTip;
+
+		if (sharesExchanged ()) {
+			done.setEnabled (true);
+			tToolTip = "President already completed all share exchanges";
+			done.setToolTipText (tToolTip);
+		} else if (foldingCompanyCount == 0) {
+			done.setEnabled (true);
+			tToolTip = "President has no shares to exchanges";
+			done.setToolTipText (tToolTip);
+		} else {
+			done.setEnabled (false);
+			tToolTip = "President has not completed all share exchanges";
+			done.setToolTipText (tToolTip);
+		}
+	}
+	
+	@Override
+	public void handlePlayerDone () {
+		super.handlePlayerDone ();
+	}
+}
