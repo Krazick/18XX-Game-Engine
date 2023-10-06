11dfed6ac08157e4eef52fbf07e1628c14eba491
Krazick <markjsmith60@gmail.com>
2023-10-06 19:04:17 -0400

    New Action for ChangeFormationPhaseState

---
+++ src/ge18xx/round/action/ChangeFormationPhaseStateAction.java
@@ -0,0 +1,52 @@
+package ge18xx.round.action;
+
+import ge18xx.game.GameManager;
+import ge18xx.round.action.effects.SetFormationStateEffect;
+import ge18xx.utilities.XMLNode;
+
+public class ChangeFormationPhaseStateAction extends ChangeStateAction {
+	public final static String NAME = "Change Formation Phase State";
+
+	public ChangeFormationPhaseStateAction () {
+		this (NAME);
+	}
+
+	public ChangeFormationPhaseStateAction (String aName) {
+		super (aName);
+	}
+
+	public ChangeFormationPhaseStateAction (ActorI.ActionStates aRoundType, String aRoundID, ActorI aActor) {
+		super (aRoundType, aRoundID, aActor);
+		setName (NAME);
+	}
+
+	public ChangeFormationPhaseStateAction (XMLNode aActionNode, GameManager aGameManager) {
+		super (aActionNode, aGameManager);
+		setName (NAME);
+	}
+	
+	@Override
+	public String getSimpleActionReport () {
+		String tSimpleActionReport = "";
+		String tOldState, tNewState;
+
+		tOldState = getOldState ();
+		tNewState = getNewState ();
+		if (!tNewState.equals (tOldState)) {
+			tSimpleActionReport = actor.getName () + " changed state of the Formation Phase from  "
+					+ getOldState () + " to " + getNewState () + ".";
+		} else {
+			tSimpleActionReport = actor.getName () + " Formation Phase state remains [" + tOldState + "]";
+		}
+
+		return tSimpleActionReport;
+	}
+
+	public void addSetFormationStateEffect (ActorI aFromActor, ActorI.ActionStates aOldFormationState,
+							ActorI.ActionStates aNewFormationState) {
+		SetFormationStateEffect tSetFormationStateEffect;
+		
+		tSetFormationStateEffect = new SetFormationStateEffect (aFromActor, aOldFormationState, aNewFormationState);
+		addEffect (tSetFormationStateEffect);
+	}
+}
