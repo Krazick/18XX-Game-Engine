7c462c1583192f38e87c65a0b6ca107aaba01f3e
Krazick <markjsmith60@gmail.com>
2023-11-17 16:37:05 -0500

    Initial Version

---
+++ src/ge18xx/round/action/effects/RemoveTokenEffect.java
@@ -0,0 +1,57 @@
+package ge18xx.round.action.effects;
+
+import ge18xx.company.TokenInfo.TokenType;
+import ge18xx.company.benefit.Benefit;
+import ge18xx.game.GameManager;
+import ge18xx.map.MapCell;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
+import ge18xx.tiles.Tile;
+import ge18xx.utilities.XMLNode;
+
+public class RemoveTokenEffect extends LayTokenEffect {
+	public final static String NAME = "Remove Token";
+	int tileNumber;
+	int revenueCenterIndex;
+	int tokenIndex;
+	TokenType tokenType;
+
+	public RemoveTokenEffect () {
+		super ();
+		setName (NAME);
+	}
+
+	public RemoveTokenEffect (ActorI aActor, MapCell aMapCell, Tile aTile, int aRevenueCenterIndex,
+			TokenType aTokenType, int aTokenIndex) {
+		super (aActor, aMapCell, aTile, aRevenueCenterIndex, aTokenType, aTokenIndex, Benefit.NO_BENEFIT);
+		setName (NAME);
+	}
+
+	public RemoveTokenEffect  (XMLNode aEffectNode, GameManager aGameManager) {
+		super (aEffectNode, aGameManager);
+	}
+	
+	@Override
+	public boolean applyEffect (RoundManager aRoundManager) {
+		boolean tEffectApplied;
+		String tActionVerb;
+		
+		tEffectApplied = false;
+		System.out.println ("Ready to Apply " + name);
+		tActionVerb = "Apply";
+		tEffectApplied = removeToken (aRoundManager, tActionVerb);
+		
+		return tEffectApplied;
+	}
+	
+	@Override
+	public boolean undoEffect (RoundManager aRoundManager) {
+		boolean tEffectUndone;
+		String tActionVerb;
+		
+		tActionVerb = "Undone";
+		tEffectUndone = layToken (aRoundManager, tActionVerb);
+		
+		return tEffectUndone;
+	}
+}
