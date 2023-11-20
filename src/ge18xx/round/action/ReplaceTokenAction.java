ed73579a1b938aefe666ed94582e1cc9567c04f6
Krazick <markjsmith60@gmail.com>
2023-11-20 14:48:24 -0500

    New method addClearCorporationEffect

--- src/ge18xx/round/action/ReplaceTokenAction.java
+++ src/ge18xx/round/action/ReplaceTokenAction.java
@@ -3,6 +3,7 @@ package ge18xx.round.action;
 import ge18xx.company.TokenInfo.TokenType;
 import ge18xx.game.GameManager;
 import ge18xx.map.MapCell;
+import ge18xx.round.action.effects.ClearCorporationEffect;
 import ge18xx.round.action.effects.RemoveTokenEffect;
 import ge18xx.tiles.Tile;
 import ge18xx.utilities.XMLNode;
@@ -41,5 +42,12 @@ public class ReplaceTokenAction extends LayTokenAction {
 		tRemoveTokenEffect = new RemoveTokenEffect (aFromActor, aMapCell, aTile, aRevenueCenterIndex,
 				aTokenType, aTokenIndex);
 		addEffect (tRemoveTokenEffect);
+	}
+	
+	public void addClearCorporationEffect (ActorI aFromActor,MapCell aMapCell, Tile aTile, int aRevenueCenterIndex) {
+		ClearCorporationEffect tClearCorporationEffect;
+		
+		tClearCorporationEffect = new ClearCorporationEffect (aFromActor, aMapCell, aTile, aRevenueCenterIndex);
+		addEffect (tClearCorporationEffect);
 	}
 }
