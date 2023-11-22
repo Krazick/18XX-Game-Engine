87e326ae6d021bcd5d9c92369723931821f9dc09
Krazick <markjsmith60@gmail.com>
2023-11-22 14:56:32 -0500

    Update to extend from ChangeBooleanFlagEffect

---
+++ src/ge18xx/round/action/effects/ChangeBooleanFlagEffect.java
@@ -0,0 +1,65 @@
+package ge18xx.round.action.effects;
+
+import ge18xx.game.GameManager;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
+import ge18xx.utilities.AttributeName;
+import ge18xx.utilities.XMLDocument;
+import ge18xx.utilities.XMLElement;
+import ge18xx.utilities.XMLNode;
+
+public class ChangeBooleanFlagEffect extends Effect {
+	public final static String NAME = "Change Boolean Flag";
+	boolean booleanFlag;
+
+	public ChangeBooleanFlagEffect () {
+		super ();
+		setName (NAME);
+	}
+
+	public ChangeBooleanFlagEffect (String aName) {
+		super (aName);
+	}
+
+	public ChangeBooleanFlagEffect (String aName, ActorI aActor) {
+		super (aName, aActor);
+	}
+
+	public ChangeBooleanFlagEffect (String aName, ActorI aActor, boolean aBooleanFlag) {
+		super (aName, aActor);
+		setBooleanFlag (aBooleanFlag);
+	}
+
+	public ChangeBooleanFlagEffect (XMLNode aEffectNode, GameManager aGameManager) {
+		super (aEffectNode, aGameManager);
+		setName (NAME);
+	}
+
+	public XMLElement getEffectElement (XMLDocument aXMLDocument, AttributeName aActorAN, AttributeName aFlagName) {
+		XMLElement tEffectElement;
+
+		tEffectElement = super.getEffectElement (aXMLDocument, aActorAN);
+		tEffectElement.setAttribute (aFlagName, booleanFlag);
+
+		return tEffectElement;
+	}
+
+	public void setBooleanFlag (boolean aBooleanFlag) {
+		booleanFlag = aBooleanFlag;
+	}
+
+	public boolean getBooleanFlag () {
+		return booleanFlag;
+	}
+
+	@Override
+	public String getEffectReport (RoundManager aRoundManager) {
+		return (REPORT_PREFIX + name + " for " + actor.getName () + ".");
+	}
+
+	@Override
+	public void printEffectReport (RoundManager aRoundManager) {
+		System.out.println (getEffectReport (aRoundManager));
+	}
+
+}
