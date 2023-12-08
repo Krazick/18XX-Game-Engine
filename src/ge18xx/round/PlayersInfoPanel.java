3cfa0a204dea80df17b6ad72ef3fd72fc66d22dc
Krazick <markjsmith60@gmail.com>
2023-12-08 17:49:15 -0500

    Formatting

--- src/ge18xx/round/PlayersInfoPanel.java
+++ src/ge18xx/round/PlayersInfoPanel.java
@@ -13,7 +13,7 @@ import ge18xx.player.Portfolio;
 
 import geUtilities.MessageBean;
 
-public class PlayersInfoPanel extends ListenerPanel{
+public class PlayersInfoPanel extends ListenerPanel {
 	private static final String NAME = "Players Info";
 	private static final long serialVersionUID = 1L;
 	private static final String PLAYER_JPANEL_LABEL = "Player Information";
