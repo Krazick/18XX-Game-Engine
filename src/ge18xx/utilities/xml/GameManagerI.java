65050785358fcb03e3ca291b477fe29600ec3338
Krazick <markjsmith60@gmail.com>
2023-12-11 17:14:35 -0500

    Remove dead code

--- src/ge18xx/utilities/xml/GameManagerI.java
+++ src/ge18xx/utilities/xml/GameManagerI.java
@@ -8,7 +8,6 @@ import ge18xx.utilities.GameFrameConfig;
 public interface GameManagerI {
 	public static final String NO_GAME_NAME = "<NONE>";
 	public static final GameManager NO_GAME_MANAGER = null;
-	public static final GameManager_XML NO_GAME_MANAGER_XML = null;
 
 	public String getActiveGameName ();
 
