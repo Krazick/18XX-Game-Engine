5c5220b02caa8be57801e0c313daf6fab5a2251e
Krazick <markjsmith60@gmail.com>
2024-03-11 19:11:12 -0400

    New method addHideFormationPanelEffect

--- src/ge18xx/round/action/StockValueCalculationAction.java
+++ src/ge18xx/round/action/StockValueCalculationAction.java
@@ -5,6 +5,7 @@ import ge18xx.game.GameManager;
 import ge18xx.market.MarketCell;
 import ge18xx.round.RoundManager;
 import ge18xx.round.action.ActorI.ActionStates;
+import ge18xx.round.action.effects.HideFormationPanelEffect;
 import ge18xx.round.action.effects.RemoveTokenFromMarketCellEffect;
 import ge18xx.round.action.effects.SetParValueEffect;
 import geUtilities.XMLNode;
@@ -53,6 +54,13 @@ public class StockValueCalculationAction extends FormationPhaseAction {
 		addEffect (tRemoveTokenFromMarketCellEffect);
 	}
 	
+	public void addHideFormationPanelEffect (ActorI aActor) {
+		HideFormationPanelEffect tHideFormationPanelEffect;
+		
+		tHideFormationPanelEffect = new HideFormationPanelEffect (aActor);
+		addEffect (tHideFormationPanelEffect);
+	}
+
 	@Override
 	public boolean undoAction (RoundManager aRoundManager) {
 		boolean tActionUndone;
