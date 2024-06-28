72f3f55d275e34a5778f72c5895022352c2ba612
Krazick <markjsmith60@gmail.com>
2024-06-28 10:31:56 -0400

    Remove ge18xx.utilities package that have been moved to other project

--- JUnitSource/ge18xx/GE18XXFullTestSuite.java
+++ JUnitSource/ge18xx/GE18XXFullTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @SuiteDisplayName ("Game Engine 18XX Game Package Test Suite")
 @SelectPackages ({ "ge18xx.bank","ge18xx.center", "ge18xx.company", "ge18xx.company.benefit", "ge18xx.game", "ge18xx.map",
 		"ge18xx.market", "ge18xx.network", "ge18xx.player", "ge18xx.round", "ge18xx.round.actions",
-		"ge18xx.round.actions.effects", "ge18xx.tiles", "ge18xx.train", "ge18xx.utilities" })
+		"ge18xx.round.actions.effects", "ge18xx.tiles", "ge18xx.train" })
 
 class GE18XXFullTestSuite {
 
