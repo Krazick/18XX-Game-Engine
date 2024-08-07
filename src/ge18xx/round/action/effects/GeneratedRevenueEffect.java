71e69089e72a24e0c91b51244cd408b0a65dd036
Krazick <markjsmith60@gmail.com>
2024-08-07 10:35:09 -0400

    Add priorRevenue and corporationID, improved report

--- src/ge18xx/round/action/effects/GeneratedRevenueEffect.java
+++ src/ge18xx/round/action/effects/GeneratedRevenueEffect.java
@@ -1,6 +1,7 @@
 package ge18xx.round.action.effects;
 
 import ge18xx.bank.Bank;
+import ge18xx.company.Corporation;
 import ge18xx.company.TrainCompany;
 import ge18xx.game.GameManager;
 import ge18xx.round.RoundManager;
@@ -11,11 +12,14 @@ import geUtilities.XMLElement;
 import geUtilities.XMLNode;
 
 public class GeneratedRevenueEffect extends Effect {
-	public final static String NAME = "Generated Revenue";
+	public final static String NAME = "Set Generated Revenue";
 	final static AttributeName AN_REVENUE = new AttributeName ("revenue");
+	final static AttributeName AN_PRIOR_REVENUE = new AttributeName ("priorRevenue");
 	final static AttributeName AN_TRAIN_COUNT = new AttributeName ("trainCount");
 	int trainCount;
 	int revenue;
+	int priorRevenue;
+	int corporationID;
 
 	public GeneratedRevenueEffect () {
 		this (NAME);
@@ -25,20 +29,37 @@ public class GeneratedRevenueEffect extends Effect {
 		super (aName);
 	}
 
-	public GeneratedRevenueEffect (ActorI aActor, int aRevenue, int aTrainCount) {
+	public GeneratedRevenueEffect (ActorI aActor, int aRevenue, int aTrainCount, int aPriorRevenue) {
 		super (NAME, aActor);
+		
+		int tCorporationID;
+		Corporation tCorporation;
+		
+		if (aActor.isACorporation ()) {
+			tCorporation = (Corporation) aActor;
+			tCorporationID = tCorporation.getID ();
+			setCorporationID (tCorporationID);
+		}
 		setRevenue (aRevenue);
+		setPriorRevenue (aPriorRevenue);
 		setTrainCount (aTrainCount);
 	}
 
 	public GeneratedRevenueEffect (XMLNode aEffectNode, GameManager aGameManager) {
 		super (aEffectNode, aGameManager);
-		int tRevenue, tTrainCount;
+		int tRevenue;
+		int tPriorRevenue;
+		int tTrainCount;
+		int tCorporationID;
 
 		tRevenue = aEffectNode.getThisIntAttribute (AN_REVENUE);
+		tPriorRevenue = aEffectNode.getThisIntAttribute (AN_PRIOR_REVENUE);
 		tTrainCount = aEffectNode.getThisIntAttribute (AN_TRAIN_COUNT);
+		tCorporationID = aEffectNode.getThisIntAttribute (Corporation.AN_ID);
 		setRevenue (tRevenue);
+		setPriorRevenue (tPriorRevenue);
 		setTrainCount (tTrainCount);
+		setCorporationID (tCorporationID);
 	}
 
 	@Override
@@ -47,13 +68,23 @@ public class GeneratedRevenueEffect extends Effect {
 
 		tEffectElement = super.getEffectElement (aXMLDocument, ActorI.AN_FROM_ACTOR_NAME);
 		tEffectElement.setAttribute (AN_REVENUE, getRevenue ());
+		tEffectElement.setAttribute (AN_PRIOR_REVENUE, getPriorRevenue ());
 		tEffectElement.setAttribute (AN_TRAIN_COUNT, getTrainCount ());
+		tEffectElement.setAttribute (Corporation.AN_ID, getCorporationID ());
 
 		return tEffectElement;
+	}
+	
+	public void setCorporationID (int aCorporationID) {
+		corporationID = aCorporationID;
 	}
 
 	public void setRevenue (int aRevenue) {
 		revenue = aRevenue;
+	}
+
+	public void setPriorRevenue (int aPriorRevenue) {
+		priorRevenue = aPriorRevenue;
 	}
 
 	public void setTrainCount (int aTrainCount) {
@@ -63,18 +94,42 @@ public class GeneratedRevenueEffect extends Effect {
 	@Override
 	public String getEffectReport (RoundManager aRoundManager) {
 		String tTrainsUsed;
+		String tFormattedRevenue;
+		String tActorNameAbbrev;
+		Corporation tCorporation;
 
 		tTrainsUsed = trainCount + " train";
-		if (trainCount > 1) {
+		if ((trainCount > 1) || (trainCount == 0)) {
 			tTrainsUsed += "s";
 		}
+			
+		if (actor.isACorporation ()) {
+			tCorporation = (Corporation) getActor ();
+			tActorNameAbbrev = tCorporation.getAbbrev ();
+		} else {
+			tActorNameAbbrev = getActorName ();
+		}
 
-		return (REPORT_PREFIX + name + " of " + Bank.formatCash (revenue) + " with " + tTrainsUsed + " for "
-				+ getActorName () + ".");
+		if (revenue > 0) {
+			tFormattedRevenue = Bank.formatCash (revenue);
+		} else {
+			tFormattedRevenue = "NO REVENUE";
+		}
+		
+		return (REPORT_PREFIX + name + " of " + tFormattedRevenue + " with " + tTrainsUsed + " for "
+				+ tActorNameAbbrev + " (Corp ID: " + getCorporationID () + ").");
 	}
 
+	public int getCorporationID () {
+		return corporationID;
+	}
+	
 	public int getRevenue () {
 		return revenue;
+	}
+
+	public int getPriorRevenue () {
+		return priorRevenue;
 	}
 
 	public int getTrainCount () {
@@ -87,7 +142,7 @@ public class GeneratedRevenueEffect extends Effect {
 		TrainCompany tOperatingCompany;
 
 		tEffectApplied = false;
-		tOperatingCompany = (TrainCompany) aRoundManager.getOperatingCompany ();
+		tOperatingCompany = (TrainCompany) aRoundManager.getCompanyByID (corporationID);
 		tOperatingCompany.setThisRevenue (revenue);
 		tOperatingCompany.closeTrainRevenueFrame ();
 		aRoundManager.updateAllCorporationsBox ();
@@ -98,6 +153,13 @@ public class GeneratedRevenueEffect extends Effect {
 
 	@Override
 	public boolean undoEffect (RoundManager aRoundManager) {
-		return true;
+		boolean tEffectUndone;
+		TrainCompany tOperatingCompany;
+		
+		tEffectUndone = true;
+		tOperatingCompany = (TrainCompany) aRoundManager.getCompanyByID (corporationID);
+		tOperatingCompany.setThisRevenue (priorRevenue);
+		
+		return tEffectUndone;
 	}
 }
