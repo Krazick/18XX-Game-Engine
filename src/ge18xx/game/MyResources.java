1d3a57ad2588f6d9d4b2db9bcaa204eca2af4fa9
Krazick <markjsmith60@gmail.com>
2024-08-19 16:37:30 -0400

    Update to Version 0.7.36 Beta

--- src/ge18xx/game/MyResources.java
+++ src/ge18xx/game/MyResources.java
@@ -48,5 +48,5 @@ public class MyResources extends ListResourceBundle {
 			{ "GameSetXMLFile", "18xx Games.xml" },
 			{ "DataURLBase", "https://krazick.github.io/18XX-Game-Engine-XML/XML/" },
 			{ "GameSetURLFile", "18xx-Games.xml" },
-			{ "version", "0.7.35 Beta" } };
+			{ "version", "0.7.36 Beta" } };
 }
