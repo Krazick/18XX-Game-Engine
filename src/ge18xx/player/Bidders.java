d030c35c0703dc2d55f909e3b5ef5cc1f8636026
Krazick <markjsmith60@gmail.com>
2024-08-30 07:36:50 -0400

    Don't remove each bidder here, it will be done by removing Bidders

--- src/ge18xx/player/Bidders.java
+++ src/ge18xx/player/Bidders.java
@@ -310,7 +310,7 @@ public class Bidders {
 				tBidder = (Player) getCashHolderAt (0);
 				tBid = getBidAt (0);
 				tBidder.refundEscrow (certificate, tBid, aWinAuctionAction);
-				bidders.remove (0);
+//				bidders.remove (0);
 				tBidder.updatePlayerJPanel ();
 			}
 		}
