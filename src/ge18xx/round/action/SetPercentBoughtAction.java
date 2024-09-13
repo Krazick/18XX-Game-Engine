d53fd824f97421460a58573c14d9caa88a1e11b4
Krazick <markjsmith60@gmail.com>
2024-09-13 18:52:02 -0400

    Initial Version

---
+++ src/ge18xx/round/action/SetPercentBoughtAction.java
@@ -0,0 +1,32 @@
+package ge18xx.round.action;
+
+import ge18xx.game.GameManager;
+import ge18xx.round.action.ActorI.ActionStates;
+import ge18xx.round.action.effects.SetPercentBoughtEffect;
+import geUtilities.xml.XMLNode;
+
+public class SetPercentBoughtAction extends Action {
+	public final static String NAME = "Set Percent Bought";
+	
+	public SetPercentBoughtAction (String aName) {
+		super (aName);
+	}
+
+	public SetPercentBoughtAction (ActionStates aRoundType, String aRoundID, ActorI aActor) {
+		super (aRoundType, aRoundID, aActor);
+		setName (NAME);
+	}
+
+	public SetPercentBoughtAction (XMLNode aActionNode, GameManager aGameManager) {
+		super (aActionNode, aGameManager);
+		setName (NAME);
+	}
+
+	public void addSetPercentBoughtEffect (ActorI aPlayer, String aAbbrev, int aPreviousPercent, int aNewPercent) {
+		SetPercentBoughtEffect tSetPercentBoughtEffect;
+		
+		tSetPercentBoughtEffect = new SetPercentBoughtEffect (aPlayer, aAbbrev, aPreviousPercent, aNewPercent);
+		addEffect (tSetPercentBoughtEffect);
+	}
+
+}
