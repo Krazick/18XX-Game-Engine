27e741484c868d37193ca4aae9cc8926724e7cd5
Krazick <markjsmith60@gmail.com>
2024-09-18 20:10:12 -0400

    Specify Exact file to import

--- JUnitSource/ge18xx/round/SetRoundTypeTests.java
+++ JUnitSource/ge18xx/round/SetRoundTypeTests.java
@@ -1,6 +1,6 @@
 package ge18xx.round;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 
 import org.junit.jupiter.api.BeforeEach;
