4e4344d50c528b410bd3da95319e8729cbd493b1
Krazick <markjsmith60@gmail.com>
2024-09-18 20:11:12 -0400

    Initial Version

---
+++ JUnitSource/ge18xx/round/RoundFrameTests.java
@@ -0,0 +1,19 @@
+package ge18xx.round;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RoundFrameTests {
+
+	@BeforeEach
+	void setUp () throws Exception {
+	}
+
+	@Test
+	void test () {
+		fail ("Not yet implemented");
+	}
+
+}
