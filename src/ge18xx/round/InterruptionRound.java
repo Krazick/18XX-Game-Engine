be0da61f8fd958a3ffcd8c95d277e32dc54fa94a
Krazick <markjsmith60@gmail.com>
2024-09-25 07:47:39 -0400

    Formatting

--- src/ge18xx/round/InterruptionRound.java
+++ src/ge18xx/round/InterruptionRound.java
@@ -2,13 +2,27 @@ package ge18xx.round;
 
 public class InterruptionRound extends Round {
 	Round interruptedRound;
+	boolean interruptionStarted;
 	
 	public InterruptionRound (RoundManager aRoundManager) {
 		super (aRoundManager);
+		setInterruptionStarted (false);
 	}
-
+	
 	public void setInterruptedRound (Round aInterruptedRound) {
 		interruptedRound = aInterruptedRound;
+	}
+
+	public void setInterruptionStarted (boolean aInterruptionStarted) {
+		interruptionStarted = aInterruptionStarted;
+	}
+	
+	public boolean interruptionStarted () {
+		return interruptionStarted;
+	}
+	
+	public boolean interruptRound () {
+		return false;
 	}
 	
 	public Round getInterruptedRound () {
