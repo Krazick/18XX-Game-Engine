dd853241c2169ce30ced6d323306b2760eae03df
Krazick <markjsmith60@gmail.com>
2024-11-11 17:35:08 -0500

    Fill out methods for Effect, need to add UndoEffect method

--- src/ge18xx/round/action/effects/SetPreviousRouteInformationEffect.java
+++ src/ge18xx/round/action/effects/SetPreviousRouteInformationEffect.java
@@ -1,34 +1,58 @@
 package ge18xx.round.action.effects;
 
-//import ge18xx.game.GameManager;
-//import ge18xx.round.action.ActorI;
+import ge18xx.game.GameManager;
+import ge18xx.round.RoundManager;
+import ge18xx.round.action.ActorI;
 import ge18xx.train.RouteInformation;
+import ge18xx.train.Train;
 import geUtilities.xml.AttributeName;
-//import geUtilities.xml.XMLNode;
+import geUtilities.xml.XMLNode;
 
 public class SetPreviousRouteInformationEffect extends Effect {
 	public final static String NAME = "Set Previous Route Information";
-	final static AttributeName AN_OLD_PREVIOUS_ROUTE_INFORMATION = new AttributeName ("oldPreviousRouteInformation");
-	final static AttributeName AN_NEW_PREVIOUS_ROUTE_INFORMATION = new AttributeName ("newPreviousRouteInformation");
-	RouteInformation oldPreviousRouteInformation;
-	RouteInformation newPreviousRouteInformation;
+	public final static AttributeName AN_ROUTE_INFORMATION_ID = new AttributeName ("routeInformationID");
+	public final static AttributeName AN_TRAIN_ID = new AttributeName ("trainID");
+	String routeInformationID;
+	int trainID;
+	
+	public SetPreviousRouteInformationEffect (ActorI aActor, int aTrainID, String aRouteInformationID) {
+		super (NAME, aActor);
+		setTrainID (aTrainID);
+		setRouteInformationID (aRouteInformationID);
+	}
+	
+	public SetPreviousRouteInformationEffect (XMLNode aEffectNode, GameManager aGameManager) {
+		super (aEffectNode, aGameManager);
+		
+		String tRouteInformationID;
+		int tTrainID;
+		
+		tTrainID = aEffectNode.getThisIntAttribute (AN_TRAIN_ID);
+		tRouteInformationID = aEffectNode.getThisAttribute (AN_ROUTE_INFORMATION_ID);
 
-//	public SetPreviousRouteInformationEffect (ActorI aActor, RouteInformation aOldPreviousRouteInformation, RouteInformation aNewPreviousRouteInformation) {
-//		super (NAME, aActor);
-//		setOldPreviousRouteInformation (aOldPreviousRouteInformation);
-//		setNewPreviousRouteInformation (aNewPreviousRouteInformation);
-//	}
-//
-//	public SetPreviousRouteInformationEffect (XMLNode aEffectNode, GameManager aGameManager) {
-//		super (aEffectNode, aGameManager);
-//
-//		RouteInformation tOldPreviousRouteInformation;
-//		RouteInformation tNewPreviousRouteInformation;
-//
-//		tOldPreviousRouteInformation = aEffectNode.getThisAttribute (AN_OLD_PREVIOUS_ROUTE_INFORMATION);
-//		tOldPreviousRouteInformation = aEffectNode.getThisAttribute (AN_NEW_PREVIOUS_ROUTE_INFORMATION);
-//		setOldPreviousRouteInformation (tOldPreviousRouteInformation);
-//		setNewPreviousRouteInformation (tNewPreviousRouteInformation);
-//	}
-
+		setTrainID (tTrainID);
+		setRouteInformationID (tRouteInformationID);
+	}
+	
+	public void setTrainID (int aTrainID) {
+		trainID = aTrainID;
+	}
+	
+	public void setRouteInformationID (String aRouteInformationID) {
+		routeInformationID = aRouteInformationID;
+	}
+	
+	@Override
+	public boolean applyEffect (RoundManager aRoundManager) {
+		boolean tEffectApplied;
+		Train tTrain;
+		RouteInformation tRouteInformation;
+		
+		tEffectApplied = true;
+		tTrain = aRoundManager.getTrain (trainID);
+		tRouteInformation = tTrain.getCurrentRouteInformation ();
+		tTrain.setPreviousRouteInformation (tRouteInformation);
+		
+		return tEffectApplied;
+	}
 }
