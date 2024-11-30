03db645c381cb57be4f74280787c96ab39a94128
Krazick <markjsmith60@gmail.com>
2024-11-30 13:20:39 -0500

    Remove unused Test Suites

--- JUnitSource/ge18xx/player/GE18XXPlayerTestSuite.java
+++ JUnitSource/ge18xx/player/GE18XXPlayerTestSuite.java
@@ -1,8 +1,11 @@
 package ge18xx.player;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
+@Disabled
 @Suite
 @SuiteDisplayName ("Game Engine 18XX Player Package Test Suite")
 @SelectPackages ({ "ge18xx.player" })
