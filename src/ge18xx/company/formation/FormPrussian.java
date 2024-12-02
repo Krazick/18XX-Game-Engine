8ad26a9a95386a18b03e5692e4887b322c260d3a
Krazick <markjsmith60@gmail.com>
2024-12-02 16:14:56 -0500

    Add methods to support Prussian to be overridden

--- src/ge18xx/company/formation/FormPrussian.java
+++ src/ge18xx/company/formation/FormPrussian.java
@@ -1,11 +1,36 @@
 package ge18xx.company.formation;
 
 import ge18xx.game.GameManager;
+import ge18xx.round.action.StartFormationAction;
+import geUtilities.xml.XMLFrame;
 
 public class FormPrussian extends TriggerClass {
-	
+	XMLFrame formationFrame;
+	int currentPlayerIndex;
+
 	public FormPrussian (GameManager aGameManager) {
 		System.out.println ("Initiate Form Prussian Option Game: " + aGameManager.getActiveGameName ());
+	}
+	
+	@Override
+	public void prepareFormation (StartFormationAction aStartFormationAction) {
+
+	}
+	
+	@Override
+	public void showFormationFrame () {
+		formationFrame.showFrame ();
+	}
+	
+	@Override
+	public void rebuildFormationPanel () {
+		int tCurrentPlayerIndex;
+		
+		tCurrentPlayerIndex = currentPlayerIndex;
+		if (tCurrentPlayerIndex >= 0) {
+			rebuildFormationPanel (tCurrentPlayerIndex);
+		}
+
 	}
 
 }
