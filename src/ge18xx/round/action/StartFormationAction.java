af0a4b726bc1329f794657d93bcd11260039493a
Krazick <markjsmith60@gmail.com>
2024-12-02 13:25:57 -0500

    Remove references to Triggering Company, saved in TriggeringEffect

--- src/ge18xx/round/action/StartFormationAction.java
+++ src/ge18xx/round/action/StartFormationAction.java
@@ -7,20 +7,11 @@ import ge18xx.round.action.effects.Effect;
 import ge18xx.round.action.effects.SetFormationStateEffect;
 import ge18xx.round.action.effects.ShowFormationPanelEffect;
 import ge18xx.round.action.effects.StartFormationEffect;
-import geUtilities.xml.AttributeName;
 import geUtilities.xml.XMLNode;
 
-/* Currently there is no code that calls this. 
- * This Action is stored as a "Prepared Action" in the 18xx-Games.xml File for when the 
- * CGR Share Company is formed at the Start of Phase 5, that is triggered by the Purchase 
- * of the first 6 Train, and the buy Company Completes the DONE Action.
- * 
- */
 public class StartFormationAction extends ChangeRoundAction {
-	public final static AttributeName AN_TRIGGERING_CORPORATION = new AttributeName ("triggeringCorporation");
 	public final static String NAME = "Start Formation";
 	public final static StartFormationAction NO_START_FORMATION_ACTION = null;
-	ShareCompany triggeringShareCompany;
 
 	public StartFormationAction (ActorI.ActionStates aRoundType, String aRoundID, ActorI aActor) {
 		super (aRoundType, aRoundID, aActor);
@@ -30,27 +21,22 @@ public class StartFormationAction extends ChangeRoundAction {
 	public StartFormationAction (XMLNode aActionNode, GameManager aGameManager) {
 		super (aActionNode, aGameManager);
 		setName (NAME);
-		
-		String tTriggeringCorporationAbbrev;
-		
-		tTriggeringCorporationAbbrev = aActionNode.getThisAttribute (AN_NAME);
-		setTriggeringShareCompany (tTriggeringCorporationAbbrev, aGameManager);
 	}
 	
-	public void setTriggeringShareCompany (ShareCompany aTriggeringShareCompany) {
-		triggeringShareCompany = aTriggeringShareCompany;
-	}
-	
-	public ShareCompany getTriggeringShareCompany () {
-		return triggeringShareCompany;
-	}
-	
-	public void setTriggeringShareCompany (String aTriggeringShareCompanyAbbrev, GameManager aGameManager) {
-		ShareCompany tTriggeringShareCompany;
-		
-		tTriggeringShareCompany = aGameManager.getShareCompany (aTriggeringShareCompanyAbbrev);
-		setTriggeringShareCompany (tTriggeringShareCompany);
-	}
+//	public void setTriggeringShareCompany (ShareCompany aTriggeringShareCompany) {
+//		triggeringShareCompany = aTriggeringShareCompany;
+//	}
+//	
+//	public ShareCompany getTriggeringShareCompany () {
+//		return triggeringShareCompany;
+//	}
+//	
+//	public void setTriggeringShareCompany (String aTriggeringShareCompanyAbbrev, GameManager aGameManager) {
+//		ShareCompany tTriggeringShareCompany;
+//		
+//		tTriggeringShareCompany = aGameManager.getShareCompany (aTriggeringShareCompanyAbbrev);
+//		setTriggeringShareCompany (tTriggeringShareCompany);
+//	}
 	
 	public void addShowFormationPanelEffect (ActorI aFromActor) {
 		ShowFormationPanelEffect tShowFormationPanelEffect;
@@ -71,8 +57,7 @@ public class StartFormationAction extends ChangeRoundAction {
 					ShareCompany aTriggeringShareCompany) {
 		StartFormationEffect tStartFormationEffect;
 		
-		tStartFormationEffect = new StartFormationEffect (aActor, aFormingCorporation, 
-			aTriggeringShareCompany);
+		tStartFormationEffect = new StartFormationEffect (aActor, aFormingCorporation, aTriggeringShareCompany);
 		addEffect (tStartFormationEffect);
 	}
 	
