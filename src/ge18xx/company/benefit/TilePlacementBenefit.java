016bd95f6181b015631d365981f78ccbaa3b7298
Krazick <markjsmith60@gmail.com>
2025-01-05 16:48:43 -0500

    Comment out unused method

--- src/ge18xx/company/benefit/TilePlacementBenefit.java
+++ src/ge18xx/company/benefit/TilePlacementBenefit.java
@@ -100,15 +100,15 @@ public class TilePlacementBenefit extends MapBenefit {
 		}
 	}
 
-	private boolean ownerHasEnoughCash () {
-		boolean tOwnerHasEnoughCash;
-		TrainCompany tTrainCompany;
-		
-		tTrainCompany = getOwningCompany ();
-		tOwnerHasEnoughCash = companyHasEnoughCash (tTrainCompany);
-
-		return tOwnerHasEnoughCash;
-	}
+//	private boolean ownerHasEnoughCash () {
+//		boolean tOwnerHasEnoughCash;
+//		TrainCompany tTrainCompany;
+//		
+//		tTrainCompany = getOwningCompany ();
+//		tOwnerHasEnoughCash = companyHasEnoughCash (tTrainCompany);
+//
+//		return tOwnerHasEnoughCash;
+//	}
 	
 	private boolean operatingCompanyHasEnoughCash () {
 		boolean tOperatingCompanyHasEnoughCash;
