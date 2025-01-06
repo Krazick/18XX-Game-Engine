429894cd2145fce63327b00e24d717464115ce8b
Krazick <markjsmith60@gmail.com>
2025-01-06 16:19:42 -0500

    Add more tests

--- JUnitSource/ge18xx/company/benefit/TilePlacementBenefitTests.java
+++ JUnitSource/ge18xx/company/benefit/TilePlacementBenefitTests.java
@@ -1,10 +1,16 @@
 package ge18xx.company.benefit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import javax.swing.JPanel;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class TilePlacementBenefitTests extends MapBenefitTests {
 	TilePlacementBenefit tilePlacementBenefit1;
@@ -28,6 +34,45 @@ class TilePlacementBenefitTests extends MapBenefitTests {
 		assertEquals ("Tile Placement", tilePlacementBenefit3.getName ());
 		assertEquals ("Put Tile on TEST-C&A Home (M15)", tilePlacementBenefit2.getNewButtonLabel ());
 		assertFalse (tilePlacementBenefit2.hasButton ());
+		assertTrue (tilePlacementBenefit2.shouldConfigure ());
+	}
+
+	@Test
+	@DisplayName ("Tile Placement testing Map beneath")
+	void tilePlacementMapTests () {
+		JPanel tButtonRow;
+		
+		Mockito.when (mGameManager.getMapFrame ()).thenReturn (mMapFrame);
+		Mockito.when (mHexMap.getMapCellForID (anyString ())).thenReturn (mapCell1);
+		Mockito.when (mHexMap.getMapFrame ()).thenReturn (mMapFrame);
+		Mockito.when (mMapFrame.getMap ()).thenReturn (mHexMap);
+	
+		tButtonRow = new JPanel ();
+		tilePlacementBenefit3.configure (privateCSL, tButtonRow);
+		Mockito.when (mHexMap.isTileAvailableForMapCell (mapCell1)).thenReturn (true);
+
+		tilePlacementBenefit3.updateButton ();
+	}
+	
+	@Test
+	@DisplayName ("Tile Placement testing Tile beneath")
+	void tilePlacementTileTests () {
+		JPanel tButtonRow;
+
+		Mockito.when (mGameManager.getMapFrame ()).thenReturn (mMapFrame);
+		Mockito.when (mHexMap.getMapCellForID (anyString ())).thenReturn (mMapCell1);
+		Mockito.when (mHexMap.getMapFrame ()).thenReturn (mMapFrame);
+		Mockito.when (mMapFrame.getMap ()).thenReturn (mHexMap);
+		Mockito.when (mMapCell1.isTileOnCell ()).thenReturn (false);
+		
+		tButtonRow = new JPanel ();
+		tilePlacementBenefit2.configure (privateCSL, tButtonRow);
+		assertFalse (tilePlacementBenefit2.hasTile ());
+		
+		Mockito.when (mMapCell1.isTileOnCell ()).thenReturn (true);
+		Mockito.when (mHexMap.getMapCellForID (anyString ())).thenReturn (mMapCell1);
+		assertTrue (tilePlacementBenefit2.hasTile ());		
+		tilePlacementBenefit2.updateButton ();
 	}
 
 }
