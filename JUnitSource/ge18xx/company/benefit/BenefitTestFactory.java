c59a1950ce3c5bb77ed837b6c5ef959b1e9c1726
Krazick <markjsmith60@gmail.com>
2025-01-06 13:01:28 -0500

    Add XMLBenefitTest3

--- JUnitSource/ge18xx/company/benefit/BenefitTestFactory.java
+++ JUnitSource/ge18xx/company/benefit/BenefitTestFactory.java
@@ -45,4 +45,42 @@ public class BenefitTestFactory {
 		
 		return tMapBenefit;
 	}
+	
+	public TilePlacementBenefit buildTilePlacementBenefit (PrivateCompany aPrivateCompany, int aIndex) {
+		TilePlacementBenefit tTilePlacementBenefit;
+		String tXMLBenefitTest1;
+		String tXMLBenefitTest2;
+		String tXMLBenefitTest3;
+		XMLNode tBenefitXMLNode;
+
+		tXMLBenefitTest1 = "<Benefit actorType=\"Share Company\" "
+				+ "class=\"ge18xx.company.benefit.TilePlacementBenefit\" extra=\"true\" "
+				+ "mapCell=\"B20\" cost=\"0\" passive=\"false\"/>";
+		tXMLBenefitTest2 = "<Benefit actorType=\"Share Company\" ownerType=\"Player\" "
+				+ "				class=\"ge18xx.company.benefit.TilePlacementBenefit\" "
+				+ "				extra=\"false\" mapCell=\"M15\" tokenType=\"Port\" "
+				+ "				cost=\"0\" passive=\"false\" closeOnUse=\"true\" />";
+		tXMLBenefitTest3 = "<Benefit actorType=\"Share Company\" ownerType=\"Player\" "
+				+ "				class=\"ge18xx.company.benefit.TilePlacementBenefit\" "
+				+ "				extra=\"false\" mapCell=\"C15\" "
+				+ "				cost=\"50\" passive=\"false\" closeOnUse=\"false\" />";
+		if (aIndex == 1) {
+			tBenefitXMLNode = utilitiesTestFactory.buildXMLNode (tXMLBenefitTest1);
+		} else if (aIndex == 2) {
+			tBenefitXMLNode = utilitiesTestFactory.buildXMLNode (tXMLBenefitTest2);
+		} else if (aIndex == 3) {
+			tBenefitXMLNode = utilitiesTestFactory.buildXMLNode (tXMLBenefitTest3);
+		} else {
+			tBenefitXMLNode = XMLNode.NO_NODE;
+		}
+		if (tBenefitXMLNode == XMLNode.NO_NODE) {
+			tTilePlacementBenefit = TilePlacementBenefit.NO_TILE_PLACEMENT_BENEFIT;
+		} else {
+			tTilePlacementBenefit = new TilePlacementBenefit (tBenefitXMLNode);
+			tTilePlacementBenefit.setPrivateCompany (aPrivateCompany);
+		}
+		
+		return tTilePlacementBenefit;
+	}
+
 }
