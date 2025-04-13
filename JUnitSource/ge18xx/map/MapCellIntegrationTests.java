a1ef8fb59d60c74608a0fecc7fcdca1c69c99b53
Krazick <markjsmith60@gmail.com>
2025-04-13 16:01:32 -0400

    Change to Mock getTokenCompany not getShareCompany

--- JUnitSource/ge18xx/map/MapCellIntegrationTests.java
+++ JUnitSource/ge18xx/map/MapCellIntegrationTests.java
@@ -90,8 +90,8 @@ class MapCellIntegrationTests {
 		Mockito.when (mHexMap.getMapCellForID ("N11")).thenReturn (mapCell);
 		Mockito.when (mRoundManager.getTileSet ()).thenReturn (tileSet);
 		Mockito.when (mRoundManager.getGameMap ()).thenReturn (mHexMap);
-		Mockito.when (mRoundManager.getShareCompany ("TPRR")).thenReturn (alphaShareCompany);
-		Mockito.when (mGameManager.getShareCompany ("TPRR")).thenReturn (alphaShareCompany);
+		Mockito.when (mRoundManager.getTokenCompany ("TPRR")).thenReturn (alphaShareCompany);
+		Mockito.when (mGameManager.getTokenCompany ("TPRR")).thenReturn (alphaShareCompany);
 		Mockito.when (mRoundManager.getGameManager ()).thenReturn (mGameManager);
 		
 		layTileEffect.applyEffect (mRoundManager);
