346219aeb1ba41a60e54570f5853dc70dda8cffb
Krazick <markjsmith60@gmail.com>
2025-07-21 14:42:23 -0400

    Move buildAllPlayers to formCompany, new Method getPercentageExchange

--- src/ge18xx/company/formation/FormPrussian.java
+++ src/ge18xx/company/formation/FormPrussian.java
@@ -1,22 +1,14 @@
 package ge18xx.company.formation;
 
-import java.awt.Point;
 import java.util.List;
-
-import javax.swing.BoxLayout;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 
 import ge18xx.game.GameManager;
 import ge18xx.player.Player;
 import ge18xx.player.PlayerManager;
 import ge18xx.round.action.ActorI;
 import ge18xx.round.action.StartFormationAction;
-import geUtilities.xml.XMLFrame;
 
 public class FormPrussian extends FormCompany {
-	XMLFrame formationFrame;
 	int currentPlayerIndex;
 
 	public FormPrussian (GameManager aGameManager) {
@@ -36,12 +28,6 @@ public class FormPrussian extends FormCompany {
 	@Override
 	public void prepareFormation (StartFormationAction aStartFormationAction) {
 		showFormationFrame ();
-
-	}
-	
-	@Override
-	public void showFormationFrame () {
-		formationFrame.showFrame ();
 	}
 	
 	@Override
@@ -52,39 +38,8 @@ public class FormPrussian extends FormCompany {
 		if (tCurrentPlayerIndex >= 0) {
 			rebuildFormationPanel (tCurrentPlayerIndex);
 		}
-
 	}
-	public void buildAllPlayers (String aFrameName) {
-		Border tMargin;
-		Point tRoundFrameOffset;
-		int tHeight;
-		int tWidth;
-		List<Player> tPlayers;
-		PlayerManager tPlayerManager;
-
-		tPlayerManager = gameManager.getPlayerManager ();
-		tPlayers = tPlayerManager.getPlayers ();
-		formationFrame = new XMLFrame (aFrameName, gameManager);
-		formationFrame.setSize (800, 600);
 		
-		formationJPanel = new JPanel ();
-		tMargin = new EmptyBorder (10,10,10,10);
-		formationJPanel.setBorder (tMargin);
-		
-		formationJPanel.setLayout (new BoxLayout (formationJPanel, BoxLayout.Y_AXIS));
-
-		setupPlayers (tPlayerManager, tPlayers);
-		formationFrame.buildScrollPane (formationJPanel);
-
-		tRoundFrameOffset = gameManager.getOffsetRoundFrame ();
-		formationFrame.setLocation (tRoundFrameOffset);
-		gameManager.addNewFrame (formationFrame);
-		
-		tWidth = 1140;
-		tHeight = panelHeight ();
-		formationFrame.setSize (tWidth,  tHeight);
-	}
-	
 	@Override
 	public void setupPlayers (PlayerManager aPlayerManager, List<Player> aPlayers) {
 		int tCurrentPlayerIndex;
@@ -104,5 +59,13 @@ public class FormPrussian extends FormCompany {
 	@Override
 	public boolean isInterrupting () {
 		return true;
+	}
+	
+	public int getPercentageForExchange () {
+		int tPercentage;
+		
+		tPercentage = 10;
+		
+		return tPercentage;
 	}
 }
