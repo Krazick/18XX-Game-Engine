8b9696713c1bed1d57f4787041c518d27cee81eb
Krazick <markjsmith60@gmail.com>
2025-08-09 14:37:00 -0400

    Set TriggeringCompany before using it in buildAllPlayers method

--- src/ge18xx/company/formation/FormPrussian.java
+++ src/ge18xx/company/formation/FormPrussian.java
@@ -2,12 +2,17 @@ package ge18xx.company.formation;
 
 import java.util.List;
 
+import ge18xx.company.Corporation;
+import ge18xx.company.CorporationList;
+import ge18xx.company.MinorCompany;
 import ge18xx.game.GameManager;
 import ge18xx.player.Player;
 import ge18xx.player.PlayerManager;
+import ge18xx.player.PortfolioHolderI;
 import ge18xx.round.action.ActorI;
 import ge18xx.round.action.ChangeStateAction;
 import ge18xx.round.action.StartFormationAction;
+import geUtilities.xml.XMLNode;
 
 public class FormPrussian extends FormCompany {
 	int currentPlayerIndex;
@@ -20,12 +25,19 @@ public class FormPrussian extends FormCompany {
 		tFullFrameTitle = setFormationState (ActorI.ActionStates.Nationalization);
 		System.out.println ("Initiate Form Prussian, Frame Title: " + tFullFrameTitle +
 				" Game: " + gameManager.getActiveGameName ());
+		setTriggeringCompany ();
 		buildAllPlayers (tFullFrameTitle);
-		
+	
 		gameManager.setTriggerClass (this);
 		gameManager.setTriggerFormation (this);
 	}
 	
+	public FormPrussian (XMLNode aXMLNode, GameManager aGameManager) {
+		this (aGameManager);
+
+		parseXML (aXMLNode);
+	}
+
 	@Override
 	public void prepareFormation (StartFormationAction aStartFormationAction) {
 		showFormationFrame ();
@@ -40,7 +52,25 @@ public class FormPrussian extends FormCompany {
 			rebuildFormationPanel (tCurrentPlayerIndex);
 		}
 	}
+	
+	public void setTriggeringCompany () {
+		Corporation tTriggeringCompany;
+		CorporationList tMinorCompanies;
+		MinorCompany tMinorCompany;
+		int tMinorCompanyCount;
+		int tMinorCompanyIndex;
 		
+		tMinorCompanies = gameManager.getMinorsCompanies ();
+		tTriggeringCompany = MinorCompany.NO_MINOR_COMPANY;
+		tMinorCompanyCount = tMinorCompanies.getCorporationCount ();
+		for (tMinorCompanyIndex = 0; tMinorCompanyIndex < tMinorCompanyCount; tMinorCompanyIndex++) {
+			tMinorCompany = (MinorCompany) tMinorCompanies.getCorporation (tMinorCompanyIndex);
+			if (tMinorCompany.canFormUpgrade ()) {
+				tTriggeringCompany = tMinorCompany;
+			}
+		}
+		setTriggeringCompany (tTriggeringCompany);
+	}
 	@Override
 	public void setupPlayers (PlayerManager aPlayerManager, List<Player> aPlayers) {
 		int tCurrentPlayerIndex;
@@ -49,6 +79,29 @@ public class FormPrussian extends FormCompany {
 		tCurrentPlayerIndex = aPlayerManager.getPlayerIndex (actingPresident);
 		setCurrentPlayerIndex (tCurrentPlayerIndex);
 		updatePlayers (aPlayers, actingPresident);
+	}
+	
+	@Override
+	public Player findActingPresident () {
+		Corporation tTriggeringCorporation;
+		Player tActingPlayer;
+		PortfolioHolderI tPresident;
+		
+		if (actingPresident == Player.NO_PLAYER) {
+			tTriggeringCorporation = getTriggeringCompany ();
+			if (tTriggeringCorporation != Corporation.NO_CORPORATION) {
+				tPresident = tTriggeringCorporation.getPresident ();
+				if (tPresident.isAPlayer ()) {
+					tActingPlayer = (Player) tPresident;
+					setActingPresident (tActingPlayer);
+				} else {
+					setActingPresident (Player.NO_PLAYER);
+				}
+			}
+		}
+		tActingPlayer = actingPresident;
+	
+		return tActingPlayer;
 	}
 
 	@Override
