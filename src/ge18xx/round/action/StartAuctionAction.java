693e7a9425a43328a974c4eaab58f1582af38e3a
Krazick <markjsmith60@gmail.com>
2025-08-11 19:17:16 -0400

    Add new method addSetInterruptionRoundStartedEffect

--- src/ge18xx/round/action/StartAuctionAction.java
+++ src/ge18xx/round/action/StartAuctionAction.java
@@ -5,6 +5,7 @@ import ge18xx.game.GameManager;
 import ge18xx.round.action.ActorI.ActionStates;
 import ge18xx.round.action.effects.AddPrivateToAuctionEffect;
 import ge18xx.round.action.effects.AuctionStateChangeEffect;
+import ge18xx.round.action.effects.SetInterruptionStartedEffect;
 import geUtilities.xml.XMLNode;
 
 public class StartAuctionAction extends ChangeRoundAction {
@@ -37,10 +38,18 @@ public class StartAuctionAction extends ChangeRoundAction {
 		addEffect (tAuctionStateChangeEffect);
 	}
 
-	public void addAddPrivateToAuctionEffect (ActorI aActor, Certificate aCertificate, Certificate aFreeCertificate) {
+	public void addAddPrivateToAuctionEffect (ActorI aActor, Certificate aCertificate, 
+					Certificate aFreeCertificate) {
 		AddPrivateToAuctionEffect tAddPrivateToAuctionEffect;
 		
 		tAddPrivateToAuctionEffect = new AddPrivateToAuctionEffect (aActor, aCertificate, aFreeCertificate);
 		addEffect (tAddPrivateToAuctionEffect);
+	}
+	
+	public void addSetInterruptionRoundStartedEffect (ActorI aActor, boolean aSetInterruptionStarted) {
+		SetInterruptionStartedEffect tSetInterruptionStartedEffect;
+
+		tSetInterruptionStartedEffect = new SetInterruptionStartedEffect (aActor, aSetInterruptionStarted);
+		addEffect (tSetInterruptionStartedEffect);
 	}
 }
