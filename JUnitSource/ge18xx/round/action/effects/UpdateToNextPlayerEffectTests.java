782563d7a9d9f57d132c5665723387c8763b21c4
Krazick <markjsmith60@gmail.com>
2025-09-07 15:34:55 -0400

    Initial Version

---
+++ JUnitSource/ge18xx/round/action/effects/UpdateToNextPlayerEffectTests.java
@@ -0,0 +1,90 @@
+package ge18xx.round.action.effects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doNothing;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import ge18xx.company.formation.TriggerClass;
+import ge18xx.player.Player;
+import ge18xx.player.PlayerManager;
+
+class UpdateToNextPlayerEffectTests extends EffectTester {
+	UpdateToNextPlayerEffect updateToNextPlayerEffect;
+	List<Player> players;
+	TriggerClass mTriggerClass;
+	
+	@Override
+	@BeforeEach
+	void setUp () throws Exception {
+		super.setUp ();
+		players = new LinkedList<Player> ();
+		players.add (mPlayerActorAlpha);
+		players.add (mPlayerActorBeta);
+		
+		Mockito.when (mPlayerManager.getPlayers ()).thenReturn (players);
+
+		mTriggerClass = Mockito.mock (TriggerClass.class);
+		doNothing().when (mTriggerClass).setCurrentPlayerIndex (anyInt ());
+		Mockito.when (mTriggerClass.updateToNextPlayer (anyList(), anyBoolean ())).thenReturn (2);
+
+		Mockito.when (mGameManager.getTriggerClass ()).thenReturn (mTriggerClass);
+		
+		updateToNextPlayerEffect = new UpdateToNextPlayerEffect (mPlayerActorAlpha, mPlayerActorBeta);
+	}
+
+	@Test
+	void testGetEffectReport () {
+		assertEquals ("FromEffectTesterAlpha", mPlayerActorAlpha.getName ());
+		assertEquals ("ToEffectTesterBeta", mPlayerActorBeta.getName ());
+		assertEquals ("--Effect: Update to Next Player from FromEffectTesterAlpha to ToEffectTesterBeta.", 
+						updateToNextPlayerEffect.getEffectReport (mRoundManager));
+	}
+
+	@Test
+	void testUndoEffect () {
+//		fail ("Not yet implemented");
+	}
+
+	@Test
+	void testApplyEffect () {
+//		fail ("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateToNextPlayerEffectXMLNodeGameManager () {
+//		fail ("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateToNextPlayer () {
+		Player mPlayerActorDelta;
+
+		assertEquals (1, updateToNextPlayerEffect.updateToNextPlayer (mRoundManager, true));
+	
+		mPlayerActorDelta = playerTestFactory.buildPlayerMock ("ToEffectTesterDelta");
+		updateToNextPlayerEffect.setToActor (mPlayerActorDelta);
+		
+		assertEquals (2, updateToNextPlayerEffect.updateToNextPlayer (mRoundManager, true));
+
+		updateToNextPlayerEffect.setToActor (mPlayerActorBeta);
+		Mockito.when (mGameManager.getTriggerClass ()).thenReturn (null);
+		assertEquals (0, updateToNextPlayerEffect.updateToNextPlayer (mRoundManager, true));
+	}
+
+	@Test
+	void testGetPlayerIndexListOfPlayerString () {
+		assertEquals (2, players.size ());
+		assertEquals (1, updateToNextPlayerEffect.getPlayerIndex (players, mPlayerActorBeta.getName ()));
+		assertEquals (PlayerManager.NO_PLAYER_INDEX, updateToNextPlayerEffect.getPlayerIndex (players,
+						"FromEffectTesterGamma"));
+	}
+}
