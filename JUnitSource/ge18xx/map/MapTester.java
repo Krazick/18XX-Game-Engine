3d2e65baf5fa3e2d864e99b0bed7e693814b77c1
Krazick <markjsmith60@gmail.com>
2025-10-22 17:16:05 -0400

    New root class for MapCell Test Routines

---
+++ JUnitSource/ge18xx/map/MapTester.java
@@ -0,0 +1,28 @@
+package ge18xx.map;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import ge18xx.company.CompanyTestFactory;
+import ge18xx.game.GameTestFactory;
+import ge18xx.round.RoundTestFactory;
+import ge18xx.tiles.TilesTestFactory;
+
+@TestInstance (Lifecycle.PER_CLASS)
+public class MapTester {
+	protected CompanyTestFactory companyTestFactory;
+	protected GameTestFactory gameTestFactory;
+	protected MapTestFactory mapTestFactory;
+	protected RoundTestFactory roundTestFactory;
+	protected TilesTestFactory tilesTestFactory;
+
+	@BeforeAll
+	void factorySetup () {
+		gameTestFactory = new GameTestFactory ();
+		companyTestFactory = new CompanyTestFactory (gameTestFactory);
+		mapTestFactory = new MapTestFactory ();
+		roundTestFactory = new RoundTestFactory ();
+		tilesTestFactory = new TilesTestFactory (mapTestFactory);
+	}
+}
