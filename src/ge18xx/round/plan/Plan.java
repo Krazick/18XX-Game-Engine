6d255a4cc494c915a9037939322c055a71cd2d56
Krazick <markjsmith60@gmail.com>
2025-11-28 10:15:22 -0500

    Add ID Field, and constant of GREEK_ALPHABET Array

--- src/ge18xx/round/plan/Plan.java
+++ src/ge18xx/round/plan/Plan.java
@@ -1,8 +1,12 @@
 package ge18xx.round.plan;
 
 public class Plan {
+	public final String [] GREEK_ALPHABET = {"Alpha", "Beta", "Gamma", "Delta", "Epsilon",
+			"Zeta", "Eta", "Theta", "Iota", "Kappa", "Lambda", "Mu", "Nu", "Xi", "Omicron",
+			"Pi", "Rho", "Sigma", "Tau", "Upsilon"};
 	String gameName;
 	String name;
+	int id;
 	
 	public Plan (String aGameName, String aName) {
 		setGameName (aGameName);
@@ -17,11 +21,19 @@ public class Plan {
 		name = aName;
 	}
 
+	public void setID (int aID) {
+		id = aID;
+	}
+	
 	public String getGameName () {
 		return gameName;
 	}
 	
 	public String getName () {
 		return name;
+	}
+	
+	public int getID () {
+		return id;
 	}
 }
