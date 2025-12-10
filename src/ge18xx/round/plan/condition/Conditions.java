1e5b720dfa151a116e3aea05ed7b68ed0cfe43a4
Krazick <markjsmith60@gmail.com>
2025-12-09 19:24:05 -0500

    New methods getFailsReasons, getConditionAt, getConditionReport

--- src/ge18xx/round/plan/condition/Conditions.java
+++ src/ge18xx/round/plan/condition/Conditions.java
@@ -3,7 +3,11 @@ package ge18xx.round.plan.condition;
 import java.util.LinkedList;
 import java.util.List;
 
+import geUtilities.GUI;
+
 public class Conditions {
+	public static final Conditions NO_CONDITIONS = null;
+
 	List<Condition> conditions;
 
 	public Conditions () {
@@ -24,5 +28,40 @@ public class Conditions {
 		}
 		
 		return tMeetsAll;
+	}
+
+	public String getFailsReasons () {
+		String tFailsReasons;
+		
+		tFailsReasons = GUI.EMPTY_STRING;
+		for (Condition tCondition : conditions) {
+			tFailsReasons += tCondition.getFailsReason () + "\n";
+		}
+
+		return tFailsReasons;
+	}
+	
+	public Condition getConditionAt (int aIndex) {
+		return conditions.get (aIndex);
+	}
+
+	public String getConditionReport () {
+		String tConditionReport;
+		
+		if (conditions.isEmpty ()) {
+			tConditionReport = "No Conditions to report\n";
+		} else if (conditions.size () == 1) {
+			tConditionReport = "There is 1 Condition in this Plan\n";
+		} else {
+			tConditionReport = "There are " + conditions.size () + " Conditions in this Plan: \n";
+		}
+
+		if (! conditions.isEmpty ()) {
+			for (Condition tCondition : conditions) {
+				tConditionReport += tCondition.getReport () + "\n";
+			}
+		}
+		
+		return tConditionReport;
 	}
 }
