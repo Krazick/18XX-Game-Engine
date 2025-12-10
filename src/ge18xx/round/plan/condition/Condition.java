de9fe8e9b0511d82ca3420322730bdcaf9a9b56f
Krazick <markjsmith60@gmail.com>
2025-12-09 19:24:48 -0500

    Add methods getFailsReason, getReport, appendStatus, setFailsReason

--- src/ge18xx/round/plan/condition/Condition.java
+++ src/ge18xx/round/plan/condition/Condition.java
@@ -1,13 +1,19 @@
 package ge18xx.round.plan.condition;
 
+import geUtilities.GUI;
+
 public class Condition {
 	public static final boolean FAILS = false;
 	public static final boolean MEETS = true;
+	public static final String FAILS_STRING = "FAILS";
+	public static final String MEETS_STRING = "MEETS";
 	public static final String NAME = "Condition";
 	String name;
+	String failsReason;
 	
 	public Condition (String aName) {
 		setName (aName + " " + NAME);
+		setFailsReason (GUI.EMPTY_STRING);
 	}
 
 	public void setName (String aName) {
@@ -20,5 +26,31 @@ public class Condition {
 	
 	public boolean meets () {
 		return FAILS;
+	}
+	
+	public void setFailsReason (String aFailsReason) {
+		failsReason = aFailsReason;
+	}
+	
+	public String getFailsReason () {
+		return failsReason;
+	}
+	
+	public String getReport () {
+		String tReport;
+		
+		tReport = name;
+		
+		return tReport;
+	}
+
+	protected String appendStatus (String aReport) {
+		if (meets ()) {
+			aReport += " " + MEETS_STRING;
+		} else {
+			aReport += " " + FAILS_STRING + " " + failsReason;
+		}
+		
+		return aReport;
 	}
 }
