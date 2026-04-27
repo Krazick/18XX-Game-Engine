a790b5be3ef95857a01c21751d126a85994adef0
Krazick <markjsmith60@gmail.com>
2026-04-27 16:58:12 -0400

    Rename NewCurrentPlayerEffect

--- src/ge18xx/round/action/ChangePlayerAction.java
+++ src/ge18xx/round/action/ChangePlayerAction.java
@@ -4,7 +4,7 @@ import ge18xx.game.GameManager;
 import ge18xx.player.Player;
 import ge18xx.round.action.ActorI.ActionStates;
 import ge18xx.round.action.effects.Effect;
-import ge18xx.round.action.effects.NewCurrentPlayerEffect;
+import ge18xx.round.action.effects.ChangeCurrentPlayerEffect;
 import geUtilities.xml.XMLNode;
 
 public class ChangePlayerAction extends SetWaitStateAction {
@@ -28,22 +28,22 @@ public class ChangePlayerAction extends SetWaitStateAction {
 		setName (NAME);
 	}
 
-	public void addNewCurrentPlayerEffect (ActorI aPlayer, int aCurrentPlayerIndex, int aNextPlayerIndex) {
-		NewCurrentPlayerEffect tNewCurrentPlayerEffect;
+	public void addChangeCurrentPlayerEffect (ActorI aPlayer, int aCurrentPlayerIndex, int aNextPlayerIndex) {
+		ChangeCurrentPlayerEffect tChangeCurrentPlayerEffect;
 
-		tNewCurrentPlayerEffect = new NewCurrentPlayerEffect (aPlayer, aCurrentPlayerIndex, aNextPlayerIndex);
-		addEffect (tNewCurrentPlayerEffect);
+		tChangeCurrentPlayerEffect = new ChangeCurrentPlayerEffect (aPlayer, aCurrentPlayerIndex, aNextPlayerIndex);
+		addEffect (tChangeCurrentPlayerEffect);
 	}
 
 	public String getNewPlayerName () {
 		String tNewPlayerName = Player.NO_PLAYER_NAME;
 		int tNewPlayerIndex;
-		NewCurrentPlayerEffect tNewCurrentPlayerEffect;
+		ChangeCurrentPlayerEffect tNewCurrentPlayerEffect;
 
 		for (Effect tEffect : effects) {
 			if (tNewPlayerName.equals (Player.NO_PLAYER_NAME)) {
-				if (tEffect instanceof NewCurrentPlayerEffect) {
-					tNewCurrentPlayerEffect = (NewCurrentPlayerEffect) tEffect;
+				if (tEffect instanceof ChangeCurrentPlayerEffect) {
+					tNewCurrentPlayerEffect = (ChangeCurrentPlayerEffect) tEffect;
 					tNewPlayerIndex = tNewCurrentPlayerEffect.getNewPlayer ();
 					tNewPlayerName = "Player # " + tNewPlayerIndex;
 				}
