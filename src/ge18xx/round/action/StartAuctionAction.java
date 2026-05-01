1561777f98273cc28d37ce985e43390f8db6849c
Krazick <markjsmith60@gmail.com>
2026-05-01 16:00:00 -0400

    New method addSetInterruptedRoundEffect to support undo

--- src/ge18xx/round/action/StartAuctionAction.java
+++ src/ge18xx/round/action/StartAuctionAction.java
@@ -2,9 +2,11 @@ package ge18xx.round.action;
 
 import ge18xx.company.Certificate;
 import ge18xx.game.GameManager;
+import ge18xx.round.RoundManager;
 import ge18xx.round.action.ActorI.ActionStates;
 import ge18xx.round.action.effects.AddPrivateToAuctionEffect;
 import ge18xx.round.action.effects.AuctionStateChangeEffect;
+import ge18xx.round.action.effects.SetInterruptedNameEffect;
 import geUtilities.xml.XMLNode;
 
 public class StartAuctionAction extends ChangeRoundAction {
@@ -43,5 +45,29 @@ public class StartAuctionAction extends ChangeRoundAction {
 		
 		tAddPrivateToAuctionEffect = new AddPrivateToAuctionEffect (aActor, aCertificate, aFreeCertificate);
 		addEffect (tAddPrivateToAuctionEffect);
+	}
+	
+	public void addSetInterruptedRoundEffect (ActorI aActor, String aInterruptingRoundName, 
+							String aInterruptedName) {
+		SetInterruptedNameEffect tSetInterruptedNameEffect;
+		
+		tSetInterruptedNameEffect = new SetInterruptedNameEffect (aActor, aInterruptingRoundName, 
+				aInterruptedName);
+		addEffect (tSetInterruptedNameEffect);
+
+	}
+	
+	// TODO: Consider options:
+	// 1) Reverse order ALWAYS before undoing.
+	// 2) Use flag on the Action that if set will reverse before undoing. This is applied when Action is created
+	@Override
+	public boolean undoAction (RoundManager aRoundManager) {
+		boolean tActionUndone;
+		
+		reverseEffects ();
+		tActionUndone = super.undoAction (aRoundManager);
+		reverseEffects ();
+		
+		return tActionUndone;
 	}
 }
