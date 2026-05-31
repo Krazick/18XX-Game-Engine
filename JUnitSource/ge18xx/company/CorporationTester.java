cdc2cfaa01eb020d6c3b1262f14f205593e251b9
Krazick <markjsmith60@gmail.com>
2026-05-31 14:15:30 -0400

    Add roundTestFactory object to the factorySetup method

--- JUnitSource/ge18xx/company/CorporationTester.java
+++ JUnitSource/ge18xx/company/CorporationTester.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.BeforeAll;
 
 import ge18xx.bank.BankTestFactory;
 import ge18xx.game.GameTestFactory;
+import ge18xx.round.RoundTestFactory;
 
 class CorporationTester {
 	protected BankTestFactory bankTestFactory;
 	protected CompanyTestFactory companyTestFactory;
 	protected GameTestFactory gameTestFactory;
+	protected RoundTestFactory roundTestFactory;
 
 	@BeforeAll
 	void factorySetup () {
 		bankTestFactory = new BankTestFactory ();
 		gameTestFactory = new GameTestFactory ();
 		companyTestFactory = new CompanyTestFactory (gameTestFactory);
+		roundTestFactory = new RoundTestFactory ();
 	}
 }
