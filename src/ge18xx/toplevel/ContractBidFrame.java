30efc4071ff8a375535d4df2bb1ade1b315441d0
Krazick <markjsmith60@gmail.com>
2026-06-18 13:23:56 -0400

    Start Filling out Contract Bid Frame

--- src/ge18xx/toplevel/ContractBidFrame.java
+++ src/ge18xx/toplevel/ContractBidFrame.java
@@ -1,27 +1,103 @@
 package ge18xx.toplevel;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
 import ge18xx.game.GameManager;
+import ge18xx.round.ContractBidRound;
 import geUtilities.xml.XMLFrame;
 import geUtilities.xml.XMLSaveGameI;
+import swingTweaks.KButton;
 
 public class ContractBidFrame extends XMLFrame implements ActionListener, XMLSaveGameI {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "Contract Bid Frame";
 	public static final ContractBidFrame NO_CONTRACT_BID_FRAME = null;
-	
+	private final String DONE = "Done Contract Bid";
+	private final String UNDO = "Undo";
+	JPanel buttonJPanel;
+	JPanel fullPanel;
+	KButton doneButton;
+	KButton undoButton;
+	boolean isNetworkGame;
+	ContractBidRound contractBidRound;
+
 	public ContractBidFrame (String aFrameName, GameManager aGameManager) {
 		super (aFrameName, aGameManager);
+		
+		boolean tIsNetworkGame;
+		
+		tIsNetworkGame = aGameManager.isNetworkGame ();
+		isNetworkGame = tIsNetworkGame;
+		fullPanel = new JPanel ();
+		fullPanel.setLayout (new BoxLayout (fullPanel, BoxLayout.Y_AXIS));
+
+		buildButtonJPanel ();
+		
+		fullPanel.add (buttonJPanel);
+		add (fullPanel);
+
 		System.out.println (NAME + " Constructed");
 	}
 	
-	@Override
-	public void actionPerformed (ActionEvent e) {
-		System.out.println (NAME + " Action to be done");
+	public void setContractBidRound (ContractBidRound aContractBidRound) {
+		contractBidRound = aContractBidRound;
 	}
 	
+	public void buildButtonJPanel () {
+		buttonJPanel = new JPanel ();
+		buttonJPanel.setLayout (new BoxLayout (buttonJPanel, BoxLayout.Y_AXIS));
+		buttonJPanel.add (Box.createVerticalStrut (5));
+		doneButton = setupButton (DONE, DONE);
+		buttonJPanel.add (Box.createVerticalStrut (5));
+		undoButton = setupButton (UNDO, UNDO);
+		buttonJPanel.add (Box.createVerticalStrut (5));
+	}
+	
+	public KButton setupButton (String aButtonText, String aButtonCommand) {
+		KButton tKButton;
+
+		tKButton = new KButton (aButtonText);
+		tKButton.setActionCommand (aButtonCommand);
+		tKButton.addActionListener (this);
+		tKButton.setAlignmentX (Component.CENTER_ALIGNMENT);
+		buttonJPanel.add (tKButton);
+		if (isNetworkGame) {
+			if (aButtonText.equals (UNDO)) {
+				tKButton.setEnabled (false);
+			}
+		}
+
+		return tKButton;
+	}
+
+	@Override
+	public void actionPerformed (ActionEvent aEvent) {
+		String tTheAction;
+
+		tTheAction = aEvent.getActionCommand ();
+		if (DONE.equals (tTheAction)) {
+			completeContract ();
+		}
+		if (UNDO.equals (tTheAction)) {
+			undoLastAction ();
+		}
+	}
+	
+	private void undoLastAction () {
+		System.out.println (NAME + " Action is 'UNDO'");
+		
+	}
+
+	private void completeContract () {
+		System.out.println (NAME + " Action is 'DONE'");
+	}
+
 	@Override
 	public void showFrame () {
 		super.showFrame ();
