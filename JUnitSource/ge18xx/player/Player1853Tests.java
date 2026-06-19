cda223144ee74b54e89cd0078daa533bf47a1310
Krazick <markjsmith60@gmail.com>
2026-06-19 17:34:47 -0400

    New tests for 1853 Player Contract Bids

---
+++ JUnitSource/ge18xx/player/Player1853Tests.java
@@ -0,0 +1,212 @@
+package ge18xx.player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import ge18xx.bank.Bank;
+import ge18xx.bank.BankPool;
+import ge18xx.bank.BankTestFactory;
+import ge18xx.company.Certificate;
+import ge18xx.company.CertificateTestFactory;
+import ge18xx.game.GameInfo;
+import ge18xx.game.GameManager;
+import ge18xx.game.GameTestFactory;
+import ge18xx.round.action.ActorI;
+
+class Player1853Tests {
+	GameTestFactory gameTestFactory;
+	BankTestFactory bankTestFactory;
+	CertificateTestFactory certificateTestFactory;
+	PlayerTestFactory playerTestFactory;
+	Bank bank;
+	BankPool bankPool;
+	GameManager mGameManager;
+	PlayerManager mPlayerManager;
+
+	int playerCount;
+	int certificateLimit;
+	String playerName;
+	Player player;
+	Player player1853;
+	Portfolio playerPortfolio;
+	
+	@BeforeEach
+	void setUp () throws Exception {
+		gameTestFactory = new GameTestFactory ();
+
+		mGameManager = setupGameInfoAndManager (4);
+
+		bankTestFactory = new BankTestFactory ();
+		bank = bankTestFactory.buildBank ();
+		bankPool = bankTestFactory.buildBankPool (mGameManager);
+		Mockito.when (mGameManager.getBank ()).thenReturn (bank);
+		certificateTestFactory = new CertificateTestFactory ();
+		playerTestFactory = new PlayerTestFactory (mGameManager);
+		
+		playerCount = 4;
+		certificateLimit = 16;
+		playerName = "BusterPlayer";
+		mPlayerManager = playerTestFactory.buildPlayerManagerMock (playerCount);
+		Mockito.when (mPlayerManager.getBank ()).thenReturn (bank);
+		Mockito.when (mPlayerManager.getBankPool ()).thenReturn (bankPool);
+		player = playerTestFactory.buildPlayer (playerName, mPlayerManager, certificateLimit);
+		playerPortfolio = new Portfolio (player);
+		player.setPortfolio (playerPortfolio);
+	}
+
+	protected GameManager setupGameInfoAndManager (int aGameInfoIndex) {
+		GameInfo tGameInfo;
+		GameManager tmGameManager;
+		
+		tGameInfo = gameTestFactory.buildGameInfo (aGameInfoIndex);
+		tmGameManager = gameTestFactory.buildGameManagerMock ();
+		Mockito.when (tmGameManager.getMaxRounds ()).thenReturn (1);
+		Mockito.when (tmGameManager.getActiveGame ()).thenReturn (tGameInfo);
+		Mockito.when (tmGameManager.gameHasRoundType ("Contract Bid Round")).thenReturn (true);
+		
+		return tmGameManager;
+	}
+
+	@Test
+	@DisplayName ("Player get routine Tests")
+	void playerGetRoutineTests () {
+		Bank tBank;
+		BankPool tBankPool;
+		GameManager tGameManager;
+		Portfolio tPortfolio;
+		PortfolioHolderI tPortfolioHolder;
+		
+		tGameManager = player.getGameManager ();
+		assertEquals (mGameManager, tGameManager);
+		
+		tBank = player.getBank ();
+		assertEquals (bank, tBank);
+		
+		tBankPool = player.getBankPool ();
+		assertEquals (bankPool, tBankPool);
+		
+		tPortfolio = player.getPortfolio ();
+		assertEquals (playerPortfolio, tPortfolio);
+		
+		tPortfolioHolder = player.getPortfolioHolder ();
+		assertEquals (player, tPortfolioHolder);
+		
+		assertFalse (player.gameHasMinors ());
+		player.setGameHasMinors (true);
+		assertTrue (player.gameHasMinors ());
+		
+		assertFalse (player.gameHasPrivates ());
+		player.setGameHasPrivates (true);
+		assertTrue (player.gameHasPrivates ());
+		
+		assertFalse (player.gameHasShares ());
+		player.setGameHasShares (true);
+		assertTrue (player.gameHasShares ());
+		
+		assertEquals (ActorI.ActionStates.NoAction, player.getAuctionActionState ());
+		player.setAuctionActionState (ActorI.ActionStates.AuctionRaised);
+		assertEquals (ActorI.ActionStates.AuctionRaised, player.getAuctionActionState ());
+	}
+	
+	@Test
+	@DisplayName ("Player Certificate Tests")
+	void playerCertificateTests () {
+		Certificate mCertificate1;
+		Certificate mCertificate2;
+		
+		assertEquals (16, player.getCertificateLimit ());
+		
+		mCertificate1 = certificateTestFactory.buildCertificateMock ();
+		Mockito.when (mCertificate1.countsAgainstCertificateLimit ()).thenReturn (true);
+		player.addCertificate (mCertificate1);
+		assertEquals (1, player.getCertificateCount ());
+		assertEquals (1, player.getCertificateTotalCount ());
+		
+		mCertificate2 = certificateTestFactory.buildCertificateMock ();
+		Mockito.when (mCertificate2.countsAgainstCertificateLimit ()).thenReturn (false);
+		player.addCertificate (mCertificate2);
+		assertEquals (1, player.getCertificateCount ());
+		assertEquals (2, player.getCertificateTotalCount ());
+		
+		player.setPortfolio (Portfolio.NO_PORTFOLIO);
+		assertEquals (0, player.getCertificateCount ());
+		assertEquals (0, player.getCertificateTotalCount ());
+	}
+	
+	@Test
+	@DisplayName ("Player at Certificate Limit Tests")
+	void playerAtCertLimitTests () {
+		Certificate mCertificate1;
+		Certificate mCertificate2;
+		Certificate mCertificate3;
+		Certificate mCertificate4;
+		
+		player.setCertificateLimit (3);
+		assertEquals (3, player.getCertificateLimit ());
+		
+		mCertificate1 = certificateTestFactory.buildCertificateMock ();
+		Mockito.when (mCertificate1.countsAgainstCertificateLimit ()).thenReturn (true);
+		player.addCertificate (mCertificate1);
+		
+		mCertificate2 = certificateTestFactory.buildCertificateMock ();
+		Mockito.when (mCertificate2.countsAgainstCertificateLimit ()).thenReturn (true);
+		player.addCertificate (mCertificate2);
+
+		assertEquals (2, player.getCertificateTotalCount ());
+		assertFalse (player.atCertLimit ());
+		assertEquals (-1, player.exceedsCertificateLimitBy ());
+		
+		mCertificate3 = certificateTestFactory.buildCertificateMock ();
+		Mockito.when (mCertificate3.countsAgainstCertificateLimit ()).thenReturn (true);
+		player.addCertificate (mCertificate3);
+
+		assertEquals (3, player.getCertificateTotalCount ());
+		assertTrue (player.atCertLimit ());
+		assertEquals (0, player.exceedsCertificateLimitBy ());
+
+		mCertificate4 = certificateTestFactory.buildCertificateMock ();
+		Mockito.when (mCertificate4.countsAgainstCertificateLimit ()).thenReturn (true);
+		player.addCertificate (mCertificate4);
+
+		assertEquals (4, player.getCertificateTotalCount ());
+		assertTrue (player.atCertLimit ());
+		assertEquals (1, player.exceedsCertificateLimitBy ());
+	}
+	
+	@Test
+	@DisplayName ("Player Actors Bank Tests")
+	void playerActorsBankTests () {
+		Bank tActorsBank;
+		
+		tActorsBank = player.getActorsBank ();
+		assertEquals ("Bank", tActorsBank.getName ());
+	}
+	
+	@Test
+	@DisplayName ("Player Contract Bid Tests")
+	void playerContractBidTests () {
+		assertTrue (player.hasContractBid ());
+		assertFalse (player.hasCompletedContractBid ());
+		assertFalse (player.hasFulfilledContractBid ());
+			
+		player.setHasCompletedContractBid (true);
+		assertTrue (player.hasCompletedContractBid ());
+		player.setHasCompletedContractBid (false);
+		assertFalse (player.hasCompletedContractBid ());
+		
+		player.setHasFullfilledContractBid (true);
+		assertFalse (player.hasFulfilledContractBid ());
+		player.setHasCompletedContractBid (true);
+
+		player.setHasFullfilledContractBid (true);
+		assertTrue (player.hasFulfilledContractBid ());
+		player.setHasFullfilledContractBid (false);
+		assertFalse (player.hasFulfilledContractBid ());
+
+	}
+
+}
