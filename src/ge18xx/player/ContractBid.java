376b6967645f8af92bfa0cb1a0e456ab6aa4a330
Krazick <markjsmith60@gmail.com>
2026-06-19 20:18:58 -0400

    Add in support for the Extra Bid over the minimum

--- src/ge18xx/player/ContractBid.java
+++ src/ge18xx/player/ContractBid.java
@@ -2,17 +2,20 @@ package ge18xx.player;
 
 public class ContractBid {
 	public static final ContractBid NO_CONTRACT_BID = null;
+	public static final int NO_EXTRA_BID = 0;
 	Player player;
 	boolean completed;
 	boolean fullfilled;
+	int extraForBid;
 	
 	public ContractBid (Player aPlayer) {
 		setPlayer (aPlayer);
-		setContractBidCompleted (false);
+		setCompleted (false);
 		setFullfilled (false);
+		setExtraForBid (NO_EXTRA_BID);
 	}
 
-	public void setContractBidCompleted (boolean aCompleted) {
+	public void setCompleted (boolean aCompleted) {
 		completed = aCompleted;
 	}
 
@@ -30,5 +33,13 @@ public class ContractBid {
 	
 	public boolean isFullfilled () {
 		return fullfilled;
+	}
+	
+	public void setExtraForBid (int aExtraForBid) {
+		extraForBid = aExtraForBid;
+	}
+	
+	public int getExtraForBid () {
+		return extraForBid;
 	}
 }
