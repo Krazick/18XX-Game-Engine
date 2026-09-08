68b72eaae1b1a5e230f4cde4ef540c06eb6a537b
Krazick <markjsmith60@gmail.com>
2026-09-08 19:05:29 -0400

    Added comments to Package-info

--- src/ge18xx/game/variants/package-info.java
+++ src/ge18xx/game/variants/package-info.java
@@ -1,1 +1,13 @@
+/**
+ * 
+ * This module supports have game variants that need to be loaded before a game is selected so 
+ * that a player can specify when optional rules, or game variants are activated or de-activated
+ * before the game is initialized. It also supports the storage of the variant status when the
+ * game is saved, or loading a previously saved game to allow the game to continue.
+ * 
+ * @since 1.0
+ * @author Mark J Smith
+ * @version
+ * 
+ */
 package ge18xx.game.variants;
