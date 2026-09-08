6c162e7851ee46c63391f9297ae52a4537e2d454
Krazick <markjsmith60@gmail.com>
2026-09-08 19:05:51 -0400

    Correct typo

--- src/ge18xx/game/variants/EnableBenefitVEffect.java
+++ src/ge18xx/game/variants/EnableBenefitVEffect.java
@@ -49,7 +49,7 @@ public class EnableBenefitVEffect extends VariantEffect {
 	 * Given an XMLDocument, this will create the XMLElement by using the super-class and then stores
 	 * the CompanyID and the VariantEffect Class
 	 *
-	 * @param aXMLDocument The XMLDocumdnt to use to create the XMLElement
+	 * @param aXMLDocument The XMLDocument to use to create the XMLElement
 	 *
 	 * @return the filled out XMLElement
 	 *
