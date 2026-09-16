ec27a25b54ca20d604d4e9404b8be41e13c0172e
Krazick <markjsmith60@gmail.com>
2026-09-16 08:32:48 -0400

    Update to the Package Info files for these Packages

--- src/ge18xx/game/userPreferences/package-info.java
+++ src/ge18xx/game/userPreferences/package-info.java
@@ -1,1 +1,13 @@
+/**
+ * 
+ * This module supports the User Preferences that allow for certain generic features for 18XX Games
+ * that can be turned on and off during the Game as desired. The preferences are presented in the
+ * User Preferences Dialog, Page 1. Page 2 has the various Frames that can be resized as needed.
+ * Page 3 will eventually show the User Color Choices.
+ * 
+ * @since 1.0
+ * @author Mark J Smith
+ * @version 1.0
+ * 
+ */
 package ge18xx.game.userPreferences;
